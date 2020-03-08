a74709019016f14c6bd7f9a8e82dde75e5711fc3
chadi <joseph.lemaitre@epfl.ch>
2020-03-08 14:43:51 -0400

    Model work from California scenario

---
+++ SEIR/results.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:59c6a53bfd758ffb6fc675c485700fe078d608182c2aa4a21effcc77f8ec27f5
+size 9132
---
+++ SEIR/setup.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d99fd885b372fbe52920a123baaeab35900af00dbe4a42e2d8540280aca35961
+size 3602
