6f737eadede065d4485b3286ff3384a82d847a6a
chadi <joseph.lemaitre@epfl.ch>
2020-03-10 15:27:01 +0100

    plot fixes

--- SEIR/results.py
+++ SEIR/results.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a53129533fed32d49b2a5661c7a0ec5994fc6b01f00ff1d1b3fa009bedb76ff
-size 9139
+oid sha256:e62a31e3c95c3b735dbe124805bda37f79d890bd394ff1553eef121d9363d75f
+size 9119
