f813f25ca5c61497a1c977698848ea49d88c2525
hrmeredith12 <hrmeredith12@gmail.com>
2020-03-11 11:16:07 -0400

    Update results.py filing
    Changed output filing system: model_output/set-up/figures/file_name.pdf OR  model_output/set-up/set-up_time/file_name.csv

--- SEIR/results.py
+++ SEIR/results.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e62a31e3c95c3b735dbe124805bda37f79d890bd394ff1553eef121d9363d75f
-size 9119
+oid sha256:6ea65e339d74a8e88fa30e87d60511141be4b2ef1d7894f5c900388ea434dd78
+size 9199
