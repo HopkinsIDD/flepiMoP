7dc204a426a2672429534995d9447cd0add2ec22
chadi <joseph.lemaitre@epfl.ch>
2020-03-15 16:20:10 +0100

    more setup

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ab75a8afb19731ecdfb0c5ebd47ce7f101fc9c57eda9bd27a6cb32c20f6cc80
-size 4759
+oid sha256:6b46d5f8c4ff5a8c75f9b2ab142d08b184cbd77517550406c8ad6e730c241384
+size 4666
