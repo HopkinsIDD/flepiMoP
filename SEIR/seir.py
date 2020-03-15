86b40aaac055ea74010c0939ec4e545b5393217f
chadi <joseph.lemaitre@epfl.ch>
2020-03-15 06:05:03 +0100

    bug fix

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fba02d6103b3ed97268411e3d43c644d6acfbcf78ffd1cc35405e363314e5c5d
-size 4685
+oid sha256:b4ef16d0d77f71ea375f5dd4f2e2c3af1da0f63f096f9d57efe5451ffd55a6b5
+size 4687
