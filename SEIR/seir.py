679f0bc8538711481c592c8dafe15e163fd3d620
chadi <joseph.lemaitre@epfl.ch>
2020-03-15 05:04:09 +0100

    Drawing from NPI distribution

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b483c3fa3b0727a2119995e44a21b7d7d15a857124f420e0ab014350d6c133c
-size 3243
+oid sha256:bf00ad8b17fb19c1c406a6838cff5bf5ed3bdca47b9dbd5421a5e484c98bb83f
+size 3796
