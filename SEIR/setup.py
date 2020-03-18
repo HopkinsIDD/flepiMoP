9b886510a3cc25a1c3db82b77b41a42a2fefe878
chadi <joseph.lemaitre@epfl.ch>
2020-03-18 19:48:48 +0100

    sampling of serial interval

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22427d1471039fa386185506f78573dcbf7a5c78b2de534cbfec4eafaabdbd80
-size 4364
+oid sha256:c67bd8eaa46a85ae83e108aead2de7d28554a4f13502e9fddc6eb65309ce8db5
+size 4437
