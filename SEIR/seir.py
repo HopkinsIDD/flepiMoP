748324925d74e73f87c973f1ecef29b14c2952ad
chadi <joseph.lemaitre@epfl.ch>
2020-03-18 21:55:23 +0100

    no more seed

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55de296875e2d35264405770d6836ba5216b69deff2dcbf4019a6a75701debfa
-size 4927
+oid sha256:2e5f07b931954bcb2b459430824b344972d3db868a22ab62b12d5b095002afaa
+size 4928
