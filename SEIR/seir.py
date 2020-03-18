3138829e971916ce4f022aa156ba3bf07ac8bffc
chadi <joseph.lemaitre@epfl.ch>
2020-03-18 23:00:53 +0100

    seeding with data

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e5f07b931954bcb2b459430824b344972d3db868a22ab62b12d5b095002afaa
-size 4928
+oid sha256:2eddb65a5bcd27532f6ef6cc291481d9e5ba71fcaefd8680d4c95a5606ab8c90
+size 5051
