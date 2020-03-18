eb5cf5f4d8782c823912b1c0cf403ee6d085f9ca
chadi <joseph.lemaitre@epfl.ch>
2020-03-18 21:27:04 +0100

    fixed NA from pivot

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8546491370868b9f6de1714561dfafdd7961405fe708e41448bdf4de418b177e
-size 4879
+oid sha256:55de296875e2d35264405770d6836ba5216b69deff2dcbf4019a6a75701debfa
+size 4927
