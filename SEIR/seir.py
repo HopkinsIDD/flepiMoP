345d2d55b3f61f2a6304fb31755a2684d4e49d51
chadi <joseph.lemaitre@epfl.ch>
2020-03-19 19:32:22 +0100

    stupidest way of wasting 10 people time

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2eddb65a5bcd27532f6ef6cc291481d9e5ba71fcaefd8680d4c95a5606ab8c90
+oid sha256:4ea95f7fe5f0fdc871c4b17c1ec0d3f4d6771b2d6015cc3d06a31fccd42a0ab6
 size 5051
