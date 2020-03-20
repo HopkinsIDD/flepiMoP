40edbb6d319921d7af5039a78fdd3f541700980f
epic.farmer <magicgathering2001@gmail.com>
2020-03-20 16:04:43 -0400

    Revert "Manually reverting to commit https://github.com/HopkinsIDD/COVIDScenarioPipeline/commit/396d240b8e7409a0af324980fd91121c76b8b53e"
    This reverts commit aa9fb7536d79326b0a27fad0284d366f2f9c3f22.

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b279e309ff55a97cababd49fdbb08b1b0ce7a18614fe519cb1991cd2a95d8714
-size 4869
+oid sha256:c67bd8eaa46a85ae83e108aead2de7d28554a4f13502e9fddc6eb65309ce8db5
+size 4437
