cba6f2e843dc3b4cd5ef61e8cfba18f49e40a07f
chadi <joseph.lemaitre@epfl.ch>
2020-03-20 15:24:31 +0100

    better folder structure

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:caaa25940f0d8d28f40d26d4e2d4660f532f94a477ef9956582d55ae5b950e16
-size 6449
+oid sha256:878a236aa7756c64754491b2119aeef9c2e1b6ff1c79aef56ba28a094d8acb6b
+size 6459
