f1269cc024f06d4d1dc2fa0e0aac5abe66881573
Sam Shah <sam@skipflag.com>
2020-03-21 22:48:04 -0700

    Code cleanup: Make Python code close to PEP8 compliant

--- SEIR/results.py
+++ SEIR/results.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:adcc9ec82c9ce9bbb13584d44a02dfa0de0229609a3e106792188c9a33087122
-size 9203
+oid sha256:fffbe01b6fc044cfe37f75e91af785b12d5e0a972af34a23f198dcffd2af7367
+size 10095
