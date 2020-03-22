7ebd493baec81e4e78fc5abbe78881e3786cae5a
Sam Shah <sam@skipflag.com>
2020-03-21 22:48:04 -0700

    Code cleanup: Make Python code close to PEP8 compliant

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:652aa32c39a0d4866e683b3c539dafe41e492d7861f846e80fa895dc5065469e
-size 6099
+oid sha256:4adb1b8505bcc2370fcfc58d9a696a02fe76ebc2fe1ce70dee057fea3594e2d1
+size 5577
