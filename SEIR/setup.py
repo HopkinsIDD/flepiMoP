53fd511e3637905c172161c15bd407c0ef42b2be
Sam Shah <sam@skipflag.com>
2020-03-23 08:46:54 -0700

    Rename parameters -> parameters_seir in config as per feedback

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b748bd8301793052bd6ccb59f4a7c1836269fbebfa612bce175113609f80d55a
-size 3629
+oid sha256:656c4647593cf591fd3a66528b2fa27bb80265f98fc46b9f0c7a59c1a95931cd
+size 3644
