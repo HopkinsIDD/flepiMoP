0aa211d036b444294e829c79f596f25ca569e9a6
Sam Shah <sam@skipflag.com>
2020-03-25 15:42:25 -0700

    SEIR: Moved to Python NPIs specified in config
    - NPIs are now specified in config versus code
    - Use a plugin system where NPI treatments are simply Python classes
      with config

---
+++ SEIR/NPI/ReduceR0.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:422b06f9beb2d5c5d3999d362e85a766cbe56dd96eec77c98fb7374aab84873f
+size 791
--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a986ccd7cdc3f10ccd0a6424f2aa59851fdacb9b4f698a15e96eaa7c349200a
-size 6423
+oid sha256:ca46d3f8cd233776f042268ff4a2e4764d3370136b9e9ca6ba4c9e5d11b7fc7c
+size 5176
