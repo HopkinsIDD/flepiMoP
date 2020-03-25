5eafe5e5129f02e2f7bf64922bafb1bbfad1a6d6
Sam Shah <sam@skipflag.com>
2020-03-25 12:12:35 -0700

    SEIR: support NPI-specific settings that are injected into NPI R scripts

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:974a59c173be06f8a3ff5cc5c9562e082987b735c253fc1123d7c657d3d86da8
-size 6144
+oid sha256:1118a6b2f077c47ebd097c458898b08ece3976a68c32acc7bb1d271bc4a081b0
+size 6254
