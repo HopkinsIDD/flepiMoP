73a447db844d288e00d2b6d545c1b3b328278d22
chadi <joseph.lemaitre@epfl.ch>
2020-03-27 00:24:41 +0100

    Fix of stacked NPIs

--- SEIR/NPI/Stacked.py
+++ SEIR/NPI/Stacked.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a5071d29749d1b23604a80c47e1d9bccf2f55f375927b9464b8a495e1c68219
-size 955
+oid sha256:ffb5dfad05aac8965db082eb7c72a3f5aeb3dfefbaffcbcdc6d5060f50fdd23c
+size 1029
