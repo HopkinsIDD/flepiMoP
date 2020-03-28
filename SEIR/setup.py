e0e1f8572a43a94ab11ee20794196c0ac5e84d87
Josh Wills <jwills@apache.org>
2020-03-28 12:31:23 -0700

    Use a COO sparse matrix format for the mobility data, but pass the underlying numpy arrays directly to the core loop to maintain JIT-compatibility.

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3656f46020f91cfc1c783c9c00bed5a9713cc42b956f493ea08c2302010b65e4
-size 5113
+oid sha256:f001fd62c09c085d41e81e878e15c5a44122a40e9c8445fd2298acb55b077d44
+size 5158
