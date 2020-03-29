09877a5009d5d3563bd7f456381b762b666bb172
kkintaro <katkintaro@gmail.com>
2020-03-29 14:13:35 -0400

    Added some R comments (for practice?)

--- R/pkgs/covidcommon/R/config.R
+++ R/pkgs/covidcommon/R/config.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2c08499365ac3cca821ea15083915606d0bf4a6a04f6664e2c5527ec14a4c0e
-size 1859
+oid sha256:a2d3803a69b35a98e09475ffa0b46ae3797414a6a26a6d167e2f59757f873103
+size 2037
