2db960e965f05b3aa177e7e95c14c556069be02a
kkintaro <katkintaro@gmail.com>
2020-03-31 19:46:56 -0400

    Add more input validation to setup

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0d495b8dcf68072dec478ccdf54a59f24dd96b85d85f1f14ab0a790dafd54e31
-size 4320
+oid sha256:c45c7ca2cdb179490ffacc5f3f5efdb7e185855d7d361891800d72ff8f60a616
+size 4685
