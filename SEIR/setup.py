cf700393aa134d49948252f61aefc2f94e7a6ba4
kkintaro <katkintaro@gmail.com>
2020-04-01 10:17:48 -0400

    Adding more validation

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c45c7ca2cdb179490ffacc5f3f5efdb7e185855d7d361891800d72ff8f60a616
-size 4685
+oid sha256:921ff5b6c195f5c75c4addd4aaaba46ce25edd150e93f51a4e7fbb34129e34df
+size 4922
