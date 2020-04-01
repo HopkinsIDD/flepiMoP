fee999d26e17209ce5f6ea770f2f422faab1b357
jkamins7 <jkaminsky@jhu.edu>
2020-03-31 23:48:06 -0400

    Adding some testing for the config loading functions.  This is just based on my personal expectation so feel free to change

--- R/pkgs/covidcommon/R/config.R
+++ R/pkgs/covidcommon/R/config.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2c08499365ac3cca821ea15083915606d0bf4a6a04f6664e2c5527ec14a4c0e
-size 1859
+oid sha256:52f6622d94e354bdd4ed8d0cd43275ae174ab2286086363ce25ecc8f4c051541
+size 1878
