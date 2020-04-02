59d607e162b98f3a6a13eb0d40c40ece3299b8f2
kkintaro <katkintaro@gmail.com>
2020-04-02 10:32:03 -0400

    3AM fixes

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b0b0199d54b5c1349080bb5b5dc3f8b8e2b6ac99f9fe3ba67a1665df8bb8aef
-size 5238
+oid sha256:7356d04f16d3a5f0947453b5da164c6db10bbf782acce520ea2a67a08615dc77
+size 5451
