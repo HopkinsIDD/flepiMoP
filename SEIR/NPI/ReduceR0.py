f7684ada742903b3b4cdd5d4169397829bf47fd9
kkintaro <katkintaro@gmail.com>
2020-04-02 14:59:02 -0400

    Fancy pandas .loc

--- SEIR/NPI/ReduceR0.py
+++ SEIR/NPI/ReduceR0.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:456d3ca9900774a59fd37318e942b0d59b5c8dbae563c9dc8bacdae3e5dce276
-size 2439
+oid sha256:19a91321749b60a977b610579096f8882de0a16957d7b2291519e04af0d98f31
+size 2401
