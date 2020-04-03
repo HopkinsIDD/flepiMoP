dd5be017b7778fd8311e9e2eb2fb81e3429d0f5c
kkintaro <katkintaro@gmail.com>
2020-04-03 13:49:46 -0400

    undoing some practice R comments

--- R/pkgs/covidcommon/R/config.R
+++ R/pkgs/covidcommon/R/config.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d81304b6bd71bd3cb1bdfaad2f9d7bc40e67ab052da9e6678f1dc491d5ce335
-size 2129
+oid sha256:4ed2457c9e2892e8a99581ada294d2fd2a3b2958c13a01cc252d951e5e1287d0
+size 1951
