d0319c98aced7a019d6ca39ad8abc6b32759156e
kkintaro <katkintaro@gmail.com>
2020-04-03 13:49:46 -0400

    undoing some practice R comments

--- R/pkgs/covidcommon/R/safe_eval.R
+++ R/pkgs/covidcommon/R/safe_eval.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14671cfb4856df402dab71d22b888b38cc4aa83df88ef99971b75b1a414f3dea
-size 869
+oid sha256:989888ba5dc5557bf0ec2f116476baaa0df0cda37abe6e8f022a9bc0b32cda68
+size 553
