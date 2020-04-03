2cf7ea422cb9106a56bde3c83e8fdfcffb65b718
kkintaro <katkintaro@gmail.com>
2020-04-03 12:45:31 -0400

    Write a couple pytests

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4cccda61be481b2c228c9f22c5fe8c3729dc3e0f9ba309a0444835161d600aa1
-size 7755
+oid sha256:2e5212890b8f17fcc01de3e7b5341ee59f0381998fd1f5ef6e3eb19d387a0050
+size 7750
