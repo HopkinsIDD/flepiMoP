44330ad6950309303166954b34d7806607347295
jkamins7 <jkaminsky@jhu.edu>
2020-04-04 09:13:10 -0400

    Exporting config load

--- R/pkgs/covidcommon/R/config.R
+++ R/pkgs/covidcommon/R/config.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0df1232327f123769e01b199ec0c5c7853fdaf7abbc2aaaa0effe57cee0165d2
-size 2374
+oid sha256:1825dd682cca2c44bb444d255db0494570e080b4cf5ae911b27d3ae65f6311ab
+size 2385
