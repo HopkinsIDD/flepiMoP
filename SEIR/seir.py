3312e956e1019f3a3e5f3cdfc66527ed6ca3f680
jkamins7 <jkaminsky@jhu.edu>
2020-04-04 12:21:07 -0400

    This will at least prevent the super movement issue.  Might do it by breaking things but at least it will be explicit about it

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbd2339f988ba8f1dd7e7641e913d225d6dce03c3e6eb2b1de471fad2dff6999
-size 5325
+oid sha256:36dede8e404f53ed7dd0d737b2b5e09841c30d03d9f90760c9bd7fc07855137b
+size 5343
