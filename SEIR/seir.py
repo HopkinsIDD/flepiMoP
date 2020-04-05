669bc09b21120c5b36bf8c9b5c532d8c313f8450
Josh Wills <jwills@apache.org>
2020-04-04 17:17:31 -0700

    bug fix

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c1fbdd3e1b7790ee8e4de0427527a6ee047cd094053b48fa443626735100471
-size 5580
+oid sha256:94849ef7073576fcec8c9f5bfc3fb9c89730d391ada59a9fce13ac9f4747e333
+size 5583
