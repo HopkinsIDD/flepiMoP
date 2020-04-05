6d5b464f7aa54702e664f7a2257644c90bcb132d
Josh Wills <jwills@apache.org>
2020-04-04 17:16:46 -0700

    Integrate a mv matrix and the is_check_loop into the core loop

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29dec62c810cd652bf39c3c6185eb22f0434c9f478413aac258f435c5cdd5f76
-size 5250
+oid sha256:6c1fbdd3e1b7790ee8e4de0427527a6ee047cd094053b48fa443626735100471
+size 5580
