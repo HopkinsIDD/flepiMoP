192de27764f122a9140392d5b1aee8dae99efdf5
kkintaro <katkintaro@gmail.com>
2020-04-05 22:40:54 -0400

    Refactor arguments to parameters_quick_draw()

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8aba22a9321b9eb7ce1ec208667157bfbfeb37dc448d437a541ad5ec79621f76
-size 5987
+oid sha256:b20a859a5db5d4519dd5e208abc837869defc828fedaed00b7f3c5b214cb267d
+size 6073
--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:635ec4b3a1cab7dfa53adce71ef432cc36628a6070eeefb64b645ebe17f33d62
-size 6793
+oid sha256:c8d38175f781dfb24f2017da616432da4b12f0301bc06eefde402b5972b3319c
+size 6713
