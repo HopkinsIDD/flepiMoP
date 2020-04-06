347ecf8c2e82948b4cf04a70d2b57ffda82f2978
kkintaro <katkintaro@gmail.com>
2020-04-05 22:50:23 -0400

    Change gamma and sigma to scalars in parameters_quick_draw

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8d38175f781dfb24f2017da616432da4b12f0301bc06eefde402b5972b3319c
-size 6713
+oid sha256:507ab8cafb6ca7cf185dbc6aa34b8b7ea938ce5ce5f25caa53be3d89310dbb3c
+size 6734
