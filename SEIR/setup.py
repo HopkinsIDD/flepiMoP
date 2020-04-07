f7b829e7cfad1bc4277909aecfc3c72534620236
Joshua Kaminsky <jkaminsky@jhu.edu>
2020-04-07 12:57:47 -0400

    Updated setup.py and test cases to match

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75013714f24a3d3291ae58f1b052bf431c7ef3061d3e4913d4c4be16356eb0ee
-size 6797
+oid sha256:677eaabde5b42e286080056e56bf47eb42198eaafa8319a203939d4da1bf6258
+size 6872
