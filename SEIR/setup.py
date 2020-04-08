df9cb3548fefc6ff19832240ed4430caccf471c0
kkintaro <katkintaro@gmail.com>
2020-04-07 21:23:26 -0400

    Update setup.py

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9bfd17ca8c751b02f9a8c10a706e4cec909849d6a2c79c4d2e7c37a603b0c7cc
-size 7010
+oid sha256:1d6ac9a1218e4cbcbea727973f87a2d132c3a34a7d5937173e9788b2c38c707d
+size 6908
