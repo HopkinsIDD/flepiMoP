25665d1a8a201e67ba0386ed283c99b8e3e9b3d5
jkamins7 <jkaminsky@jhu.edu>
2020-04-15 16:45:14 -0400

    Final fix to avoid numba

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44b5136f120159b5d4540aebbc19ba4359f29ece7a6f40a7ad3a2151103aeee3
-size 6793
+oid sha256:45a63b32ea9713d4875b3dc53b9c224a07804dba467a521d7d9d5b3c9d752db9
+size 6823
