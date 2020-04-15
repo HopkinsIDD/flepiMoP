0a02722fbe197b1f967322d40457d4db02092ece
jkamins7 <jkaminsky@jhu.edu>
2020-04-15 16:19:29 -0400

    Updated model code to use the new covidImportation package, and also seed to E instead of I (and keep population fixed

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c67b865967e1d12a1426f013d8533d74a518b8b3ee2c481b7294dfe95839c2f8
-size 6794
+oid sha256:16bc1fca880c085b6041ccf7431a74017203e1ea0c4e317f0c2c0b9516e998c0
+size 6888
