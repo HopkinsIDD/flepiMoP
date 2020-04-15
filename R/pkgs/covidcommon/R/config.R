a2d02b220c21b4c7f8045eb1c29491508c5fef45
jkamins7 <jkaminsky@jhu.edu>
2020-04-14 21:04:29 -0400

    Updated test cases since `n` is reserved in yml

--- R/pkgs/covidcommon/R/config.R
+++ R/pkgs/covidcommon/R/config.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0fc5db62350d7ea6b7f10edfe5826ab1a455ddfd4f89ae76e295b25b7b0b698a
-size 2584
+oid sha256:f6ad0b5ffb323425d43bc3e948a1fb98e0a3a382adb8db82f277fac0cca814b2
+size 2509
