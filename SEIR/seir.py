da4f72fd1a9cb8f6fc2245748c0200bfcd301c8c
jkamins7 <jkaminsky@jhu.edu>
2020-04-15 16:37:19 -0400

    Fixed typo

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16bc1fca880c085b6041ccf7431a74017203e1ea0c4e317f0c2c0b9516e998c0
-size 6888
+oid sha256:44b5136f120159b5d4540aebbc19ba4359f29ece7a6f40a7ad3a2151103aeee3
+size 6793
