6ed1b7e23f888d035bf094df165f2bb796cd1cc4
chadi <joseph.lemaitre@epfl.ch>
2020-04-16 17:32:33 +0200

    long form mobility

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85aa40fc8d3a28a8e3c377855d8c52e6a1613695879a359e113d2c874272b6ef
-size 7683
+oid sha256:ba274ce0576b8748547e5490afb56f21d67c69c0a70342d5d45480f6cc39d70e
+size 8731
