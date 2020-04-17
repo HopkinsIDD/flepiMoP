6bbb78d0e3461b03b8eca5f44e71ab245a613148
Josh Wills <jwills@apache.org>
2020-04-17 16:39:31 -0700

    Fix broken tests, though I recommend we eliminate the mean and var checks since they'll be flaky

--- R/pkgs/covidcommon/tests/testthat/test-as_random_distribution.R
+++ R/pkgs/covidcommon/tests/testthat/test-as_random_distribution.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de6b0be003f41cfad6114117c6afeec2b6ad38098456c82a6c0c46f1c5f4e514
-size 2359
+oid sha256:cda84fcba607b861a49ceaf931b7c8c94662f2403566a8c603f60d36ab1f3665
+size 2419
