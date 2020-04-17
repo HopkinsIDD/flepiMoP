b8ee75492caf6c1e354f83bc6a18461ef8cf0abf
jkamins7 <jkaminsky@jhu.edu>
2020-04-17 09:51:15 -0400

    Updated build_US_setup.R to account for the move

--- R/scripts/build_US_setup.R
+++ R/scripts/build_US_setup.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea0ab4d7c6c1eb90dc24367c8b601ba38fa95d03af64684bf6f05b88ba1286d5
-size 2132
+oid sha256:c195997a8d1e0384965836310764559b4692c51d980a6c4dae11524c3af543f0
+size 2146
