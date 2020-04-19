9adc23380d2a13c2bf3994be6437793221c10042
jkamins7 <jkaminsky@jhu.edu>
2020-04-18 20:04:15 -0400

    Report devel (#208)
    * fix unit test code
    
    * fix unit test for real
    
    * fix unit tests
    
    * adding ability to filter geoids in relative heatmap function
    
    * adding template for county-specific report for a given state
    
    * lower tolerance for distribution tests
    
    * planning_models chunk
    
    * planning scenario chunk
    
    * add names to dev team
    
    Co-authored-by: eclee25 <eclee25@gmail.com>
    Co-authored-by: Kyra Grantz <kyragrantz@gmail.com>
    Co-authored-by: hrmeredith12 <hrmeredith12@gmail.com>

--- R/pkgs/covidcommon/tests/testthat/test-as_random_distribution.R
+++ R/pkgs/covidcommon/tests/testthat/test-as_random_distribution.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cda84fcba607b861a49ceaf931b7c8c94662f2403566a8c603f60d36ab1f3665
+oid sha256:ed4bc0ba8f22327c86eeb0a05226fa72d2fe0122a0f7a578a12ba0e199edebc4
 size 2419
