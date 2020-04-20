d05e53c2702eecf8fbba37d76c1244206a139086
jkamins7 <jkaminsky@jhu.edu>
2020-04-19 20:57:58 -0400

    Removing config.yml and changing the variable name in create_seeding to be truthful. (#219)

--- R/scripts/create_seeding.R
+++ R/scripts/create_seeding.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6ea816260a7a864bd26ec8ce3ef4195358ae1f38d31e02f5f1b8613e49ab052
-size 2701
+oid sha256:2ae84ab2e0262ee1f0c433b35a5778a5325623d50a1f9147838b4ca92854c23c
+size 2683
