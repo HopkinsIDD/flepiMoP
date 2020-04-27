e22743b99fe418fd2807c3b004a1f7e18eca7ffc
kkintaro <katkintaro@gmail.com>
2020-04-27 12:54:11 -0400

    Add ability to reduce alpha, sigma, and gamma (#241)
    * Add the ability to reduce multiple parameters
    
    * Add Reduce scenario template to test_simple and documentation
    
    * minor bug test fix
    
    * Minor bugs
    
    Co-authored-by: Joseph Lemaitre <joseph.lemaitre@epfl.ch>

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a510b7aaa584497a87e92d0378d08fdb9187756b668f2c9694f058f6863fe74c
-size 8746
+oid sha256:5dcda1b6522fb6b85c78cb3e18c5681071c7fa2ecdebaa0b41e376523cb3fb69
+size 9959
