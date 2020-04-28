dbccc44897b7b6c03568de0d092e2d2429e98d3f
kkintaro <katkintaro@gmail.com>
2020-04-28 16:00:04 -0400

    Remove debug print statements

--- SEIR/NPI/Reduce.py
+++ SEIR/NPI/Reduce.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6588b9e19966be56a0195c580fc7db6444a3835f3cdfd67bf4e667618da78878
-size 3585
+oid sha256:448de5f4a01b8a6e1959e2855eefd3847b58b869c6e9e5da0e5f9488dfc281f0
+size 3567
