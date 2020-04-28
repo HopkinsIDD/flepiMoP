2e8cb2ea2b64cc31838376d9b6bd326b2f9204c8
kkintaro <katkintaro@gmail.com>
2020-04-28 15:22:02 -0400

    Add npi_name column to snpi output

--- SEIR/NPI/Reduce.py
+++ SEIR/NPI/Reduce.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42c7deb1aef5b0e66cd273c1d20e39c685788a742c5705aa5da7f44f9fae0c16
-size 3919
+oid sha256:6588b9e19966be56a0195c580fc7db6444a3835f3cdfd67bf4e667618da78878
+size 3585
