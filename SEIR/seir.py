40e468978213495ff3c76dd67fbd17c21ca983e8
chadi <joseph.lemaitre@epfl.ch>
2020-04-29 02:57:21 +0200

    typo

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b37877a030867e2b57f8440067d8ccff5adcf9593b65263e6697b5aed79cac73
-size 8119
+oid sha256:021822ea2114c1ec953a64523698767cdc56a638b55e12d35f3ae1178d869f85
+size 8116
