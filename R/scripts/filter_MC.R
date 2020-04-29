87d62385faecd8ebbdcd083f2169b34f4704a5f5
jkamins7 <jkaminsky@jhu.edu>
2020-04-29 13:46:13 -0400

    Updated to fix a hardcoded filepath

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1d8aa9d52761e5849a6020a60c5baa7ea987784a0f65afba628bd4b1e0bbc2e2
-size 20140
+oid sha256:07fd572afc455c2def63947ddec408221a41706f9719c39ee719831c98bb40af
+size 20146
