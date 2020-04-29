ba0817fe238dc75cabd5c858023f94752185e826
jkamins7 <jkaminsky@jhu.edu>
2020-04-28 23:25:35 -0400

    Removing a browser

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bf4387b1a3ad75a6896fa32c16bce482dff7df5dcc3dd2b84e82dd8f2ef9041
-size 16737
+oid sha256:de2650286dec6c9d7d4b9cc06c73fbecd8083c9ece91fe7fac80e76326765d5e
+size 16723
