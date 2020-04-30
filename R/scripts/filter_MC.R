8d72db59b4056220a79b87a977c246da0a18af36
jkamins7 <jkaminsky@jhu.edu>
2020-04-30 01:53:15 -0400

    First take at updated npi treatment in likelihood

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c9bb3f9d46aa4642cf5a03bc1343856a7d291c59d81ce36bbe7b563b6e8e8a2
-size 20291
+oid sha256:f4412dc2f92426cb5c6042d022cee85d8ec316fe6ab0e64939e729cbf1325409
+size 20650
