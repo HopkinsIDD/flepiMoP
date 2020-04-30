d43a77579f5f5508f261a553fdff2f96e87289eb
chadi <joseph.lemaitre@epfl.ch>
2020-04-30 11:06:01 +0200

    support ignore missing

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb67b5af2c2e93fce064326be2ccf76da3df5fe315dc3bf5c92f86504234d1c6
-size 15355
+oid sha256:ca134ab00cb57ebfa5fbc2d8625ff4d4b13bfe3274ecabea4e41353574da1602
+size 16344
