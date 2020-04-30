787a056e76455d90f0a715c6cc6c1ae1a0e96351
chadi <joseph.lemaitre@epfl.ch>
2020-04-30 17:56:09 +0200

    optimization

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a9f5b7ba6156f1aae89f13bae6b5d5b4c4462d7b5586c05b70cee24cce72581
-size 17251
+oid sha256:7ae4b493b5a5cf7906bc878b57dc656919b5074b79c0637690d19cc217e42c94
+size 16820
