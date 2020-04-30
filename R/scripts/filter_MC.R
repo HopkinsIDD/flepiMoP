6deeb1fdb6c4e7b7f099c2ffd7c58e75e56c53e0
jkamins7 <jkaminsky@jhu.edu>
2020-04-29 23:00:07 -0400

    Fixed ll vs likelihood issue again

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eebbcb8460cc466cecbd63c9c132f0a052d1a14a4d399a8d09e657ec67f6ea10
-size 20299
+oid sha256:4c9bb3f9d46aa4642cf5a03bc1343856a7d291c59d81ce36bbe7b563b6e8e8a2
+size 20291
