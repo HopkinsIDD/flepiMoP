8044615573773b8f1c05103a7e5b38af54d9221a
chadi <joseph.lemaitre@epfl.ch>
2020-04-30 17:09:50 +0200

    better interface for initial conditions

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca134ab00cb57ebfa5fbc2d8625ff4d4b13bfe3274ecabea4e41353574da1602
-size 16344
+oid sha256:7c3c810bf8cceff4ad7f5e8bf22c8f8e7b4b8e16e2cb32a74b29ec52c30f995c
+size 16861
