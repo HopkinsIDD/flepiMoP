c064bbfb93180014bf8caf255c47b18ebd51591a
jkamins7 <jkaminsky@jhu.edu>
2020-05-03 14:26:10 -0400

    Updated Reduce interventions to be more object oriented

--- SEIR/NPI/Reduce.py
+++ SEIR/NPI/Reduce.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ecf1af1b862c98d50456b5217dee423c7814d5f14d63aff6e97329af3444c8f
-size 4096
+oid sha256:41b24e874ea2b1ed83c8303a2ad7d9a4d75ed9957fdf9fafbfe59f42b926ca27
+size 4170
