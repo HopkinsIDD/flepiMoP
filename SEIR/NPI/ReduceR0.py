fb2a7785596f2b86ed52d4b9579c707c96ded3f0
jkamins7 <jkaminsky@jhu.edu>
2020-05-03 15:59:40 -0400

    Made ReduceR0 explicitly extend Reduce.  Restructured to make editing easier.

--- SEIR/NPI/ReduceR0.py
+++ SEIR/NPI/ReduceR0.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7926d1f7538e3e2f9781d6478aa77e5908cfead3127c130053c7b00c1ef6dbfe
-size 3847
+oid sha256:f76b009b08b94c7cddc4d557eed47b60caef9f2e1be4c778bdbcaffc2333af27
+size 360
