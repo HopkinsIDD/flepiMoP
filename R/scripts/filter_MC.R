e9d01da5414c2b67b89e114287ea5244f146bcb5
jkamins7 <jkaminsky@jhu.edu>
2020-05-04 10:11:13 -0400

    Squashing a bug where it doesn't detect the scenarios

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:732d538d7ab5c30e852732ad73f95f51af60c6583e90b2739d345ea1ed818281
-size 20612
+oid sha256:5043ffcfbd6bda088c195b7e8e1ac9f86c2c71dfb3a9d790ad22c327f4e85eb9
+size 20630
