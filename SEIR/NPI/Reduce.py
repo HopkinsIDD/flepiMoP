668250be75feeb01538b63cc8c644940ed109afa
jkamins7 <jkaminsky@jhu.edu>
2020-05-04 10:10:21 -0400

    Fixed date issue

--- SEIR/NPI/Reduce.py
+++ SEIR/NPI/Reduce.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05b5643e7eb10d8480a2a9b4c5e4749b87d2f5953bac33d2f9ec2d705711b96f
+oid sha256:c241e11a03c056926a82f11147ce0d5f2aa91ead947fbaf4dfff20024690c674
 size 4886
