eb59733e220344e1521f6065b5a0869666966170
jkamins7 <jkaminsky@jhu.edu>
2020-05-03 23:16:22 -0400

    Updated to get something running with the new npi parameterization

--- SEIR/NPI/Reduce.py
+++ SEIR/NPI/Reduce.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2214e9e1bffe03830a610906a66f2558311e9d7fa42ad69944389b83b1e78bdc
-size 4171
+oid sha256:05b5643e7eb10d8480a2a9b4c5e4749b87d2f5953bac33d2f9ec2d705711b96f
+size 4886
