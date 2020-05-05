fe52d8363375c4b0fb55aa9b95b391f5d9ccc039
Josh Wills <josh.wills@gmail.com>
2020-05-05 12:24:49 -0700

    Merge inference_batch into inference (#287)
    * WIP for a batch runner for the inference-based pipeline
    
    * Add some minimal logic for testing out copying output from one job to another
    
    * Fixes based on testing out the dependent array jobs
    
    * Update default bucket for the job
    
    * Wire this bit up correctly
    
    * Add an ls here so I can see the outputs in the logs
    
    * Add a utility for converting csv to csr format for fast matrix loading
    
    * Do the local_install.R step as part of the batch runs
    
    * Fix myriad bugs in this script
    
    * Copy over my legacy batch runners here so I can use them with other inference code
    
    * Helper script for running correct hosp
    
    * add support for parallelizing along death scenarios and rotating jobs across multiple job queues
    
    * add in p_hosp_inf as well
    
    * more intelligent pasting
    
    * oasting work
    
    * Update the inference job to assign work to queues based on how lightly loaded they are
    
    * fixes
    *
    
    * batch with retries
    
    * fix that bug
    
    * oh bash
    
    * WIP for a batch runner for the inference-based pipeline
    
    * Add some minimal logic for testing out copying output from one job to another
    
    * Fixes based on testing out the dependent array jobs
    
    * Update default bucket for the job
    
    * Wire this bit up correctly
    
    * Add an ls here so I can see the outputs in the logs
    
    * Add a utility for converting csv to csr format for fast matrix loading
    
    * Do the local_install.R step as part of the batch runs
    
    * Fix myriad bugs in this script
    
    * Copy over my legacy batch runners here so I can use them with other inference code
    
    * Helper script for running correct hosp
    
    * add support for parallelizing along death scenarios and rotating jobs across multiple job queues
    
    * add in p_hosp_inf as well
    
    * more intelligent pasting
    
    * oasting work
    
    * Update the inference job to assign work to queues based on how lightly loaded they are
    
    * fixes
    *
    
    * batch with retries
    
    * fix that bug
    
    * oh bash
    
    * Add in my helper script for tracking the status of batch jobs as they run
    
    * Add a check to ensure that the filtering.data_path exists before we launch a job
    
    Co-authored-by: chadi <joseph.lemaitre@epfl.ch>

---
+++ batch/inference_runner.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:564f23731d35e0e859b516b48cfdd81c096eae2b0faf9205f2c5108c51db9eba
+size 2077
---
+++ batch/launch_job.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:083c0e3003826710a3cf87d4c3944d18e169c89ab408d9106fdf960f33ca700a
+size 6871
