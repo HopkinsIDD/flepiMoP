353398a526c606b247fc187e37392510a0cae7bb
kkintaro <katkintaro@gmail.com>
2020-05-06 17:39:58 -0400

    Removed covidImportation references

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2bca3c30eb90d218fb59240de12d4976eec10b3c002952bfa7f84ff63edb030
-size 21954
+oid sha256:d64ce86f71b3e9107b3042a7c0aed7eeb63abe903760593e542b760e9025baa6
+size 21598
