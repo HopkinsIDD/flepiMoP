adbc86b73e4398d636af59f19e03d6d7bf35af8d
jkamins7 <jkaminsky@jhu.edu>
2020-05-07 00:37:26 -0400

    Updated filter_MC.R

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d64ce86f71b3e9107b3042a7c0aed7eeb63abe903760593e542b760e9025baa6
-size 21598
+oid sha256:3638300d5d4c6f62cf751ab8d6ccbb9b9874253a107254e7b76ce310eb080b19
+size 26821
