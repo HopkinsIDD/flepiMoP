682911802fce83bde9cef4fd7a3ab1128618035b
Josh Wills <josh.wills@gmail.com>
2020-05-07 10:05:24 -0700

    Use a local file named  for all runs

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67515fb767e358c372d2399b0991abd8333683c01146fb286ed902c5dcd92220
-size 9403
+oid sha256:8f60d6046235ec7edfa6284aa97853a3f181ae1d2bfa4eb70e59c058ca647a5a
+size 9378
