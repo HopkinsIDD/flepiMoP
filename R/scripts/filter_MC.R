a964062c602baf9bc18290ed9e1df9efc0b544b2
jkamins7 <jkaminsky@jhu.edu>
2020-05-07 01:31:16 -0400

    Updated to fix differential reporting

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3638300d5d4c6f62cf751ab8d6ccbb9b9874253a107254e7b76ce310eb080b19
-size 26821
+oid sha256:7f5fcc2ea0370981f1c515ab8ed36b68e6c28a9c3db805ff2310d1b01eb83adf
+size 26993
