94491e2c00982d1e82537dd103c8fb11622b077b
Sam Shah <shahsam@umich.edu>
2020-05-06 18:52:49 -0700

    Batch runner job now uses temporary config files instead of rewriting existing config (#292)
    Safer as the config isn't modified in-place: if the job is killed/fails, we aren't left in an intermediate state with a modified config.

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f739a13d963f2b3de73b4a1b149bbfea0bb3675d70644709898dc9d73d6bb7c
-size 9359
+oid sha256:67515fb767e358c372d2399b0991abd8333683c01146fb286ed902c5dcd92220
+size 9403
