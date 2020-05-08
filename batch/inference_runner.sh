fc053f079ebc4c8a1fc6eac40f79e4080f8e04cf
jkamins7 <jkaminsky@jhu.edu>
2020-05-07 23:50:11 -0400

    updated inference runner to print contents of working directorywq

--- batch/inference_runner.sh
+++ batch/inference_runner.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:564f23731d35e0e859b516b48cfdd81c096eae2b0faf9205f2c5108c51db9eba
-size 2077
+oid sha256:4d4488508e24b2a1746259ec2760e9e08d3efdbdb0e5621e45f235cc15816e53
+size 2288
