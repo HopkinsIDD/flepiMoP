891beddf20c6d93c69fc40019688a9cfb6c89241
kkintaro <katkintaro@gmail.com>
2020-05-07 21:21:32 -0400

    Change timestamp of output filenames

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f60d6046235ec7edfa6284aa97853a3f181ae1d2bfa4eb70e59c058ca647a5a
-size 9378
+oid sha256:eb89cb4471fe333cc3abaa2f19f3587d7f682403dbf049f8f93060322b983aa1
+size 9466
