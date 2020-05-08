49b46f61294dd4cabd7fa5b915f9f4119f69eca5
Sam Shah <sam@skipflag.com>
2020-05-07 18:26:49 -0700

    Fix missing type information during merge

--- SEIR/steps_source.py
+++ SEIR/steps_source.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12452aabbfa47db75cd93dd20b3f8b0c3ec504023b9cd8237cf816b0db00e259
-size 3285
+oid sha256:6530735577f2b4185ab752b65f801d269b7614ed61628675e618cda27a451e52
+size 3032
