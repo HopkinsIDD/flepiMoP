5de2b5e1b0f5bdb4935578114c7b2cf1e6b8bd4f
jkamins7 <jkaminsky@jhu.edu>
2020-05-08 08:37:54 -0400

    Updated filter to not allow seeding to walk off the edge

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab479c601392452dc5f6b923fa6e8cddbfd739b6e25a0e42d2bf0bcbbcd033c8
-size 27360
+oid sha256:adb0fe3d36b3432c190cd587b35ea19b4b19412eee8259e822b823d4a09b1697
+size 27461
