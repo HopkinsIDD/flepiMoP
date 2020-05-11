e87cd43bd3cd126d66c90572f06e22af6fdd5d1b
jlessler <jlessler@jhsph.edu>
2020-05-11 16:53:37 -0400

    Create test cases for inference code and move critical bits to the inference package.

--- R/pkgs/inference/R/functions.R
+++ R/pkgs/inference/R/functions.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e2f3a3257a43be21f3c38b5ae13c50c6b4dbcf2cdc9c09a6d5410fd57087553
-size 9105
+oid sha256:5efedb320920558294209a583d1a24dca23cac6639b2cf4260bb2049ddb6a40b
+size 9707
