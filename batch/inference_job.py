88a5eccd04af0a91990dceac16efc7f85add82ab
Josh Wills <josh.wills@gmail.com>
2020-05-12 08:05:33 -0700

    bump default RAM

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:67f5152d46aa08919107b49b213e7677636ad22226baa38e0ca303b88e055b12
+oid sha256:1d5a75b3fbad19d84e288da14bc7e658af3c4b7c6a134ed8b413370f988d5542
 size 12478
