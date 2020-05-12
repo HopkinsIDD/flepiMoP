ed6b188be9a032711f7d1461390c43324bd72d5b
jkamins7 <jkaminsky@jhu.edu>
2020-05-12 15:20:28 -0400

    fixed export issue

--- R/pkgs/inference/R/functions.R
+++ R/pkgs/inference/R/functions.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:173dc0494bc6a492e2beb1fa33ec094d0941d877c8d15443d4c73a5c9ed068a0
-size 7823
+oid sha256:f1a0ffbf6fe29e1f0a9b010d4f1db7ef5590f68a7ae95ca1c067bca95811b6f5
+size 7835
