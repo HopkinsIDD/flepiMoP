1856f8bdd85df7b75565ed463a6ee2fce49936b4
Josh Wills <josh.wills@gmail.com>
2020-05-12 08:01:59 -0700

    Add the manifest info and smarter handling for the CSP code to inference_batch_org

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f13602bc12863b281db1805551c78a58fe35d2e0221986ebd4760b4e3604af8b
-size 11692
+oid sha256:63029c36b6dbd679c609938409d1ea98d5dd7ae559b330322a018cbce94c38ec
+size 12467
