3cc81f0fe852ed68083ee92998aaa67caa10738f
jkamins7 <jkaminsky@jhu.edu>
2020-05-12 23:34:49 -0400

    Updated local install

--- local_install.R
+++ local_install.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9b24f5fdf7e55ff1a8fa0ec6876aceb70cbbd6b2b67d91f93e6b6c49884c70b
-size 485
+oid sha256:1a7d10b2b0b77adc54fcfb7d6fed552a19605636d52184d77ad3e0684e6409d9
+size 326
