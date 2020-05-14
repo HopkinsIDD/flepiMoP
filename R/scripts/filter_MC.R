d870b3f444961adff900a4b2006a86d2b5e7226b
jkamins7 <jkaminsky@jhu.edu>
2020-05-14 17:04:52 -0400

    Updated to a consistent initial/current/first/previous naming scheme. initial : the accepted, current : the proposed, first : there is a first_X_filename for every X we save/load.  Changed to always write initial to file at the end (except hospitalizations)

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4bed0a742ccc70ea1c43dba56f61d586f9925e1d28e8402b413f729e00921d7
-size 19651
+oid sha256:da3298ad0bc3377bb7ebb169871f8b0272f6004b70eb5ebae478953d274a0f7a
+size 19860
