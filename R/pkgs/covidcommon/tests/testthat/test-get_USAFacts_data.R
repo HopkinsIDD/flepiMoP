6de773baffb886f57d29f21c392e60fe2f261472
kkintaro <katkintaro@gmail.com>
2020-05-22 21:23:11 -0400

    Calculate incidence data for USAFacts (#308)
    * Calculate incidents and fix negative incident counts, missing dates, and NA values
    
    * Add a test.

---
+++ R/pkgs/covidcommon/tests/testthat/test-get_USAFacts_data.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1e4fe9f4899436b8a8e5f46cf744ed02dfea9d98e8603b78915d3b7ec813cbfa
+size 925
--- R/pkgs/inference/R/groundtruth.R
+++ R/pkgs/inference/R/groundtruth.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1299e1810c323ea14f2bb7fa47a93a5a9cc4013276f38e6ef051d7e9d71ef93a
-size 2290
+oid sha256:192dc14fcb7a7057f63d8bad6cf297787edbfbffc2ac0c58a0c8831fc28ed91c
+size 1968
