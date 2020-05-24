3b75045e242b213b936afbf9718325959d0eca04
Josh Wills <josh.wills@gmail.com>
2020-05-24 16:53:22 -0700

    error handling logic for inference runs

--- batch/inference_runner.sh
+++ batch/inference_runner.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f2acbd9f1e1e52fd2313bea16c7fa80d31eab6beb7aecf3d8a89e05c0985638
-size 2571
+oid sha256:9ffcf8fb904e98099246832bbc1295e16bdee6b6ccbc8a9ad8d7b90925be2c84
+size 3085
