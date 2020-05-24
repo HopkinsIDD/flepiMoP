d47c88bd987abc03711c25dffb5a5542d9258aa3
jkamins7 <jkaminsky@jhu.edu>
2020-05-24 18:15:39 -0400

    Updated to fix week aggregation issue

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40741a8293c3444aa82b9158358b0111f45b594695e78cf4c82f75ba9514c69a
-size 19922
+oid sha256:3adf47cf2a80841ebfc9209c1dac0eafea8e655cf30069c3f70c651a29ac3f5b
+size 19954
