e52b6b473c3124281ebd91a3669493c8bb6315a9
chadi <joseph.lemaitre@epfl.ch>
2020-05-28 12:12:37 +0200

    pre-compute probablities

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1ca80ea1a8f0f5c8a4f78850b36578dcbdcfa12bab4130157af5db9c9cf5382
-size 5481
+oid sha256:da2a0ab6c31691f6b1c786553ddeaeeb77a45ac9ecb7e256690f1113e962992e
+size 5904
