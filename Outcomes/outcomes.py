4a9eea6896350f894d0e50bd7f875018f9bf148d
chadi <joseph.lemaitre@epfl.ch>
2020-05-31 10:23:54 +0200

    reusing code from the SEIR module

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc5f79020fdef0ff9cfde597777c943d5fef529ea0311fa0a5be017039f0108d
-size 7566
+oid sha256:2bab8f7972fb5138e3c9ca07a1d6d293127ded73abb8b0b3946899c2039d183c
+size 7572
