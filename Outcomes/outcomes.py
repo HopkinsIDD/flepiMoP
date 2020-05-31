ecfb89165666721df519cb9306afad39a5e93938
chadi <joseph.lemaitre@epfl.ch>
2020-05-31 08:52:39 +0200

    parmeters from files

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fa4b005a06be8abceb3057ceb16ea7a76249d21360703d59f1e773abbcb4a3d
-size 6030
+oid sha256:82b50a791842e7a34645ef5b99d589d765ed32cb474f1d9389a70e409999bff8
+size 7389
