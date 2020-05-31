868fdd9c37f70cfcc0b4b59882e201d1493f4a3a
jlessler <jlessler@jhsph.edu>
2020-05-30 23:09:07 -0400

    Added functions to provide basic support for priors and corresponding basic unit test.

--- R/pkgs/inference/R/functions.R
+++ R/pkgs/inference/R/functions.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9d659f1e2419312ee189184cdd13ef5f42cce5b08a2071b49c67d1f87ccd96c6
-size 8829
+oid sha256:c6512ba76cfac7c15b3c2973f7de83625c8e3e592a8e583e118acf1868d13f0e
+size 11151
---
+++ R/pkgs/inference/tests/testthat/test-calc_hierarchical_likadj.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:aeb152f27af359e1ba98dfb611bacbb9d3bb01376827b9db78dbabcc027b65df
+size 4296
