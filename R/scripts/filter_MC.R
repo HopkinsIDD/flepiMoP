6b54822d4700ae5feb6187a995ce6c6d92d26b2f
jkamins7 <jkaminsky@jhu.edu>
2020-06-02 01:49:51 -0400

    First pass at getting inference working (and broke out a function for testing (though I can't get tests to work)

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3adf47cf2a80841ebfc9209c1dac0eafea8e655cf30069c3f70c651a29ac3f5b
-size 19954
+oid sha256:4a04a7a7a2e4fc5f30cf497b2cac3fcbd82ac03ca3636069f807199699bfeb2e
+size 19833
