c6f2a937c10aeeb2f88ac3230785dbd1d101aec1
Justin Lessler <jlessler@jhsph.edu>
2020-06-02 03:52:51 +0000

    initial pass at moving aggregation and ll calc to a function and adding hierarchy

---
+++ R/pkgs/inference/R/filter_MC_runner_funcs.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ac53772301c88c29f095a75afea76380d2ebc585fb07cee97470269eafe509ac
+size 4588
--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d56b5b34d2204bcb08959e35f4916dadd48bc00f0149f73e9d3a6ab4c65eef0
-size 20212
+oid sha256:fd2df02b2dcc987d31f3e5d51da3f769a7a1d377e096bff4057f2dc7078ddf33
+size 25246
