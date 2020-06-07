5b5a559ffe759423b2cb333b557a0cdd20079b56
Justin Lessler <jlessler@jhsph.edu>
2020-06-07 02:53:45 +0000

    Ability to have hierarchical structure in parameters added

---
+++ R/pkgs/inference/tests/testthat/test-aggregate_and_calc_loc_likelihoods.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e8ce3c011818fcc6c102e00a6b0b82f7b6095bf0f5aea8a8c5ab8e8a437709fe
+size 21041
--- R/pkgs/inference/tests/testthat/test-calc_hierarchical_likadj.R
+++ R/pkgs/inference/tests/testthat/test-calc_hierarchical_likadj.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aeb152f27af359e1ba98dfb611bacbb9d3bb01376827b9db78dbabcc027b65df
-size 4296
+oid sha256:6bc0dfb0a16be2d39cd5b90b6f9d5c3db924fea79761f7d813bf208c4f4cb9da
+size 5712
