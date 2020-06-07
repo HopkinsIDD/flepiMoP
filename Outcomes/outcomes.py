dd23310063e58a4dc1e41a1925670c9c56bec327
jkamins7 <jkaminsky@jhu.edu>
2020-06-07 02:21:09 -0400

    Updated outcomes to use a file format compatible with current inference

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a6fb6835ca0a28ee3567451909318eef7afb1d718624c75f2724ed95ae05e88
-size 8826
+oid sha256:0bca75dfee1365c22bf63f8db67a8af679b59b5d40981b62ba20e6e1b887606f
+size 9090
