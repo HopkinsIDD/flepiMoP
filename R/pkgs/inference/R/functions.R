5fe90506c7bee5001f4626a623e9e9e2daceed42
Justin Lessler <jlessler@jhsph.edu>
2020-06-08 04:06:39 +0000

    Fixed bug with logit transfored 0s

--- R/pkgs/inference/R/functions.R
+++ R/pkgs/inference/R/functions.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:665aaf3a9f7ffc0c650752060937cd9aa0aa3e88e0172f6bbfdb5a184f99bef6
-size 11481
+oid sha256:439a74b7bbd33bac0b74a93afaa07a0d47ca366422acf5c9a5b766c037f45c66
+size 11943
