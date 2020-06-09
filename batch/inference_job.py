e1bf2a71fb93750a5d14f993b7efe85ce1284354
jkamins7 <jkaminsky@jhu.edu>
2020-06-09 15:45:29 -0400

    First working batch job submitted

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:418a56b4bdcb091542aa850256f3350b45baa6886b4f16d3d9d75b97dc587686
-size 14220
+oid sha256:f439436c863eaa99c5b2343c4df8512506f961c6ed5e77b6593f45ee41339c8b
+size 14638
