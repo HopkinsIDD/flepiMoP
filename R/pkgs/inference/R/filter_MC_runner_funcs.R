b3ac31a49fc01756548081c86ec3b987f43bea4e
jkamins7 <jkaminsky@jhu.edu>
2020-06-17 19:11:14 +0000

    Fixed some namespace issues

--- R/pkgs/inference/R/filter_MC_runner_funcs.R
+++ R/pkgs/inference/R/filter_MC_runner_funcs.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:128b6d428c113715846b582a75f2c6d319612fec1d6c6ae34f783f225939722d
-size 7339
+oid sha256:0ba51e5d0292562cab9e576bdcb43dd72756672de8c4d01cf7bb1ac23bae8107
+size 7423
