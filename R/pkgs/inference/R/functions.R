53e67b897edec9c13c44f92288725b42b681eb11
jkamins7 <jkaminsky@jhu.edu>
2020-06-25 22:17:04 -0400

    Updated to fix crash bugs, and get around a wsl bug involving dates.

--- R/pkgs/inference/R/functions.R
+++ R/pkgs/inference/R/functions.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:796e51e2013a90c6cd8cbb3e5dd006c025751c134f1f3d955bdcb8e4ed1b346e
-size 12853
+oid sha256:4a643784832de952b1f6630dec450c7a695408d527239f4f61ffa8b471bc4b7b
+size 12939
