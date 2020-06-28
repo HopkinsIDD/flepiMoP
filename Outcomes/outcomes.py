9ad4ab996ce717e802f83118a188c8952791e6be
chadi <joseph.lemaitre@epfl.ch>
2020-06-28 11:57:45 +0200

    cosmetic stuff misclicked on PEP8 compliance...

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afbf292aac7442e00a62767c4aedf38b65f4457f785dc5344b4141c1a31f68d8
-size 8787
+oid sha256:5c0b2b37240c5e7492da2a0871b76de60f135048c42fe02a39ef94bad79669c6
+size 8723
