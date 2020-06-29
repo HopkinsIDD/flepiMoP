6b30a5176f1c03b17f1ccf3b9ba81c759cf6e21b
chadi <joseph.lemaitre@epfl.ch>
2020-06-29 03:05:36 +0200

    TODO: test & comment

--- minimal_interface.py
+++ minimal_interface.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c92baf925d2053e3d9f96d3790a775e205ae580dafd653c945e8a2276f308e8
-size 3464
+oid sha256:67b365d0fd3670e3747883a25d35a9fa135e2f2c4dc09395c32227b4cb9439ae
+size 3576
