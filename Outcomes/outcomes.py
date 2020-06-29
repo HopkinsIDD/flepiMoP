82331493d56b3f1167ee4de4bac2907109eec493
chadi <joseph.lemaitre@epfl.ch>
2020-06-29 02:46:12 +0200

    interesting bug: all sim where the same if same instance of random distribution, whatever seed. Fixed

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a853af85672194f2dde9ec74e0e5ef92208e228234aa4188a0a5fa2cd02f11fa
-size 12604
+oid sha256:66d7d45396a3524f9f12b00c91023dd4fc0cee07a4437cba3ece23787bee5dc1
+size 12521
