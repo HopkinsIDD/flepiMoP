5ae31f9b6afceeeca26baa0e18f58b4851e29e0c
chadi <joseph.lemaitre@epfl.ch>
2020-06-29 02:50:57 +0200

    update simulate

--- minimal_interface.py
+++ minimal_interface.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d98d38f28242755d124bdec7b083a673ddf6547c5d54c85480c5f40733a64ee5
-size 3212
+oid sha256:3c92baf925d2053e3d9f96d3790a775e205ae580dafd653c945e8a2276f308e8
+size 3464
