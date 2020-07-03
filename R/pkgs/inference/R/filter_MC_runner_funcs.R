96dd33484ded97e3878bf3f8fbba487f68e818f8
jkamins7 <jkaminsky@jhu.edu>
2020-07-02 20:07:30 -0400

    Updated inference to properly resume seeding and seir files, and test to reflect that

--- R/pkgs/inference/R/filter_MC_runner_funcs.R
+++ R/pkgs/inference/R/filter_MC_runner_funcs.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e002185736b874238c8fbfc7dd251d121578aa153e5ddbe9e9bebb30da28e92
-size 16561
+oid sha256:3317b264a88cf89f789bbbd520c2d0f3aab42af5f788bb7fa8280499741ecd09
+size 17531
