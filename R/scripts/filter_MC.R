57c40463bd640c8bb6bae2f2157d726340e20efe
jkamins7 <jkaminsky@jhu.edu>
2020-07-04 02:26:26 -0400

    Updated multiblock inference to use hierarchy and prior, and fixed filter_MC to have final likelihoods definitely part of chimeric likelihoods

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:718756d62b11d24dbe914acd9c3887bcfc84f66a45430b753745015cb7df91ca
-size 16147
+oid sha256:d63edb8391551c06b3d023007dea0549b9e551281360a13864edd24d039ffe98
+size 16149
