7ce2eb524cd3564d659c3ac93364c20eff6ddf3d
jkamins7 <jkaminsky@jhu.edu>
2020-07-13 17:10:11 -0400

    Merged inference

--- R/pkgs/inference/R/functions.R
+++ R/pkgs/inference/R/functions.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a643784832de952b1f6630dec450c7a695408d527239f4f61ffa8b471bc4b7b
-size 12939
+oid sha256:51f16bfd62b0fb5c3daf3ec36f522b65aa30ac19e90441d83c96fb965f280933
+size 12942
