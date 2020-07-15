9df59c5af22efa0dd726d154a974e2d8542efe92
chadi <joseph.lemaitre@epfl.ch>
2020-07-15 23:28:42 +0200

    work with new format from launching

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15df241e091933ad074d45c2a00914e87636a088c8da107e1108adf73b8c9a7b
-size 12899
+oid sha256:894dd333a19ca6e0b69f6186d92b276ee950ec73b6bf779abe12f26beb9b0c67
+size 13507
