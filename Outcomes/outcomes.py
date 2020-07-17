3789a79ff1a0f6699858ed46dc186ad639ce84fc
chadi <joseph.lemaitre@epfl.ch>
2020-07-17 17:58:07 +0200

    fixed bug filtering of relative prob file not used

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3c2143ec55b8ba2c72d7fca6f81cf374ace2021137a100a6e22cd7cd69c34961
-size 13308
+oid sha256:5d8c57b1afdc700e9f0998a0f4a1f459f9bac3fc3d3f4ae9cbc2b11afe2aa105
+size 13533
