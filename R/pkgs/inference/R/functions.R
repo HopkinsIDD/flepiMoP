e607c3ee60bdd3602d693eeeaf19fb84603ca610
javierps <javier.perezsaez@gmail.com>
2020-08-04 14:42:03 +0200

    functions to compute cumulates and national-level counts

--- R/pkgs/inference/R/functions.R
+++ R/pkgs/inference/R/functions.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84e552634dacf7fbcce76cd762159a7a280b81ef9968892d9c32e28f56756b27
-size 14056
+oid sha256:bd62d1307a3ee9ee754e3bd4ea8b9f11dcca29803a5c5f0dd8c8d0af83fb0b91
+size 14559
