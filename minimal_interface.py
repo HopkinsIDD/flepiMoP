fc93f1ad84b4eda0cae3178a677cccd71f6d28f4
chadi <joseph.lemaitre@epfl.ch>
2020-08-05 16:11:20 +0200

    interface

--- minimal_interface.py
+++ minimal_interface.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e7c8c1600e58d02e53d113da67a4c3867794195bff7d77a10b5c5d93a8e26e2
-size 3343
+oid sha256:3f1373eed9b6dd30cb111094648cf9ca2270ec28fea9a6d31bc699ad63393277
+size 3515
