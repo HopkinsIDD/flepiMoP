8ff92dbde024190e70fe928fb73b49223ca6b785
chadi <joseph.lemaitre@epfl.ch>
2020-08-26 14:52:20 +0200

    Smooth non-integer non stochastic simulations

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55f647a267648e7d40769d6ec1f4a19038b2efa97baee0f24faeae193090e673
-size 9321
+oid sha256:79941fa5b61790eb3f24d4bc8a7fefc84aa854f41b73277f59922121b33091e6
+size 9376
