7a6954f486fa6ba94cbac9817ebfce8063cd9eaf
chadi <joseph.lemaitre@epfl.ch>
2020-08-28 16:43:15 +0200

    First try

--- SEIR/steps_source.py
+++ SEIR/steps_source.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5abd8bda28bb19c08061d8579a48878269d18e8f8cc6147876cf313bef41cfb
-size 3460
+oid sha256:a6752555e51631238b696c1f1fe4051bb4ff32d1996981c53e6d73eca11b675a
+size 3410
