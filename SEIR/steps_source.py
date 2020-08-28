edc5ee913ba484c9e0a1afd2ba4bfac4cda0450f
jkamins7 <jkaminsky@jhu.edu>
2020-08-28 00:06:13 -0400

    Changed some things to support the test I wanted to run

--- SEIR/steps_source.py
+++ SEIR/steps_source.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b5abd8bda28bb19c08061d8579a48878269d18e8f8cc6147876cf313bef41cfb
-size 3460
+oid sha256:689ac436fe303e6d301b2c7b60f22f521dd6f18da0bc72635db94e295a0015b6
+size 3505
