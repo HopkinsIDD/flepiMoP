dda5c719fbec7bb9ef2b70f0d4c05f47444452de
chadi <joseph.lemaitre@epfl.ch>
2020-08-28 16:43:15 +0200

    First try

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79941fa5b61790eb3f24d4bc8a7fefc84aa854f41b73277f59922121b33091e6
-size 9376
+oid sha256:21484acf7a9a075213e077ceb3f6e59d3fb3ee4002e557ed41ef97b9cf98b7b4
+size 9301
