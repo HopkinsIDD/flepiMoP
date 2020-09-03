91f8e27abe245872e4695abb66aef1b59d3d91af
jkamins7 <jkaminsky@jhu.edu>
2020-09-03 09:27:26 -0400

    Revert "Merge branch 'inference_testing_no_deaths' into inference"
    This reverts commit c814549b91f005b0e92bda59c701885fb445c3c8, reversing
    changes made to e3b6808dc866b47d54cac93f1111cdb5db8b6e48.

--- SEIR/steps_source.py
+++ SEIR/steps_source.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3f24f4cd620e405110db14d5342ab226e1abac789e5017add45546cedde008b
-size 3455
+oid sha256:a6752555e51631238b696c1f1fe4051bb4ff32d1996981c53e6d73eca11b675a
+size 3410
