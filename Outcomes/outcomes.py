f32da48f996fa74e4a98dc630958e3554eb1b83c
chadi <joseph.lemaitre@epfl.ch>
2020-09-10 14:24:52 +0200

    keep name for script compatilibty

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:84b94b18038a905b3f8d6c789400255cced1b833d803213ae7a03bd2ac79cf2b
-size 10101
+oid sha256:e6be5c4160704efb593f7a7e1d35cb74acd16d3a4cd9668d6824ea29ad83d529
+size 10047
