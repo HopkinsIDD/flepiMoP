512629f5880e4a87036c7475e07f499c40a1e262
chadi <joseph.lemaitre@epfl.ch>
2020-09-10 14:14:53 +0200

    MVP of age division

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:787a8f4f0842ee7aaad2fc53f4d0882f1867927d8fe790c9394a0c7ab739dfc7
-size 9318
+oid sha256:84b94b18038a905b3f8d6c789400255cced1b833d803213ae7a03bd2ac79cf2b
+size 10101
