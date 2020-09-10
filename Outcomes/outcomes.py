8fc60d1d523191fd58b5320c4749e27d7ee7fa55
chadi <joseph.lemaitre@epfl.ch>
2020-09-10 14:44:52 +0200

    default are more sane (saner ?)

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48617f20a7e59e78971f817b0215116be7b8e0ede8b277f999883b68a831ec3e
-size 10597
+oid sha256:7d5661a363000828ae20ed98407c7d9c8d6cc7373e33aed27e040f79e5f8cf97
+size 10613
