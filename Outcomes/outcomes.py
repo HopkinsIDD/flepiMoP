ee835cc41151523eff13c801aca679985527ff89
chadi <joseph.lemaitre@epfl.ch>
2020-09-10 15:42:37 +0200

    default only when from incidI to match non subclassed sims

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d5661a363000828ae20ed98407c7d9c8d6cc7373e33aed27e040f79e5f8cf97
-size 10613
+oid sha256:fc857982da68ae4ec62f25496d7d635d0d5170e53bc31dbfadb88a2b950e36cf
+size 10772
