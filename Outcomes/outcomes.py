faf734187dd0334135a57a273e1bb4b8079dd8f2
chadi <joseph.lemaitre@epfl.ch>
2020-09-14 15:14:15 +0200

    removing default value

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1efbf4d167b8b5a5eeeca6a15af6ddb7d3579e90d21fcfbdd6c77191fd88f354
-size 15613
+oid sha256:2ca329ad7a6aeb734a721f6400664899e8c4b153476d6f9ca0045ab1d89f8949
+size 15428
