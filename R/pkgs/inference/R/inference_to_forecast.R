c65f5c7544ee27622c362d71b954e39324f21060
Justin Lessler <jlessler@jhsph.edu>
2020-09-14 20:03:33 +0000

    accommodation for csse update time in inference pkg

--- R/pkgs/inference/R/inference_to_forecast.R
+++ R/pkgs/inference/R/inference_to_forecast.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:150e2d270f04812ef18d4767ec2e8ec0de59f7532feec2e6f072e0a236e5f8e6
-size 3426
+oid sha256:e2553b648ba8272d34a76561ce9028d09674416c8f365180280824d14ec6a3c4
+size 4336
