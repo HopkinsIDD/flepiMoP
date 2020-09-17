0635c6399de349cd8bd3717ae8558c18a5513ffb
chadi <joseph.lemaitre@epfl.ch>
2020-09-17 12:03:01 +0200

    raise error on file non-conversion

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ef60303444b99b7028d90b5043aeca595adb3ba6981f46cccf007877ee2def3
-size 15438
+oid sha256:498ef637bbb2822a28292a2822ae87a2437686d32b4f9032461875b1749dc8f8
+size 15524
