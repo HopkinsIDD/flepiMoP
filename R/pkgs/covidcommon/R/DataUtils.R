540ddaa0f79f5110b2c618d7394e300cbbdd0acc
eclee25 <eclee25@gmail.com>
2020-09-27 00:14:05 -0400

    add NAMESPACE, fix print typos

--- R/pkgs/covidcommon/R/DataUtils.R
+++ R/pkgs/covidcommon/R/DataUtils.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c44939a0e6b23bca3016d7e24ac1d138c0de61936fd4a7ba6c25e10b94addd38
-size 21756
+oid sha256:7e147970e160286e5445298fd3410e9ed06e1817480ddadae50ae1621ffa3d99
+size 21759
