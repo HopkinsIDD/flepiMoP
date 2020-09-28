fc367694c65e761c7da4b2b3e44315cfe668eb8b
eclee25 <eclee25@gmail.com>
2020-09-28 16:32:03 -0400

    update processing for incidI/D to add 0 as 1st val

--- R/pkgs/covidcommon/R/DataUtils.R
+++ R/pkgs/covidcommon/R/DataUtils.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dec15d174170c8e2b1d95565128ec0143aea92851a1d894cbcd1ca94e38932e5
-size 31509
+oid sha256:e5116c87a5a6cda5bd255ec3ac152b182851437961b3d828bc878a6bc9069714
+size 31785
