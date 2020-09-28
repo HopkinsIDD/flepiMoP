971e62a0e9783933e7359da180efbebad2ec88da
eclee25 <eclee25@gmail.com>
2020-09-27 22:53:06 -0400

    update reichlab functions

--- R/pkgs/covidcommon/R/DataUtils.R
+++ R/pkgs/covidcommon/R/DataUtils.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e147970e160286e5445298fd3410e9ed06e1817480ddadae50ae1621ffa3d99
-size 21759
+oid sha256:0f38a31514b970dd12cdb2b7336c85d7bb2e8ba0dc4e99c5d8240138dc8b81d8
+size 28110
