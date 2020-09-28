7ea7420908d269e35bc17691c34f2e3f675b754b
eclee25 <eclee25@gmail.com>
2020-09-28 00:00:09 -0400

    covidcommon with wrapper, needs matchGlobal &tests

--- R/pkgs/covidcommon/R/DataUtils.R
+++ R/pkgs/covidcommon/R/DataUtils.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f38a31514b970dd12cdb2b7336c85d7bb2e8ba0dc4e99c5d8240138dc8b81d8
-size 28110
+oid sha256:dec15d174170c8e2b1d95565128ec0143aea92851a1d894cbcd1ca94e38932e5
+size 31509
