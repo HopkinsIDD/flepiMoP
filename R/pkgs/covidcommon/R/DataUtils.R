eeba966c40d3d463322842f18175c3b133fcd1ce
eclee25 <eclee25@gmail.com>
2020-09-28 23:34:34 -0400

    rm US from reichlab st data pull

--- R/pkgs/covidcommon/R/DataUtils.R
+++ R/pkgs/covidcommon/R/DataUtils.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5116c87a5a6cda5bd255ec3ac152b182851437961b3d828bc878a6bc9069714
-size 31785
+oid sha256:ac5f5110dcc6a2602da5da8589a3c3b655133b8743955bfcb44afc7e241adbbc
+size 31825
