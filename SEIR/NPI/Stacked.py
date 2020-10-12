396a9b7f0524300613ca6ab3b9a2f21ad86dc3b0
jkamins7 <jkaminsky@jhu.edu>
2020-10-12 00:17:18 +0000

    Updated max # of interventions

--- SEIR/NPI/Stacked.py
+++ SEIR/NPI/Stacked.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5f64a51139930cc5344b19012add4ab7c9d2ab2e33c0358860b54d6865383a0
+oid sha256:7f748db23e144886fe611d932914dd6cc2a390e2b666fef4a94d5e7e3f0657c1
 size 3680
