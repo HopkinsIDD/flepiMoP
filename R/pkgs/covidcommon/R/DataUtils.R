1f1a310a692ab0235417f597f6349a71b3ed7ce0
eclee25 <eclee25@gmail.com>
2020-10-19 13:31:33 -0400

    fix type and csse complete test

--- R/pkgs/covidcommon/R/DataUtils.R
+++ R/pkgs/covidcommon/R/DataUtils.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4fdfd168eed7a1acd930b3df522c03586f37eb59aa6110578a9e4b4c0b7d021
-size 32591
+oid sha256:958d1bdcab933c1fdb842656132c30f69104f4aff87a8b3da6ddcf5e3d87ba52
+size 32745
