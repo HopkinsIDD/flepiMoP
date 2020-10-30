af243ef3f25ac0f134eb2b9f525646af4f736b89
jkamins7 <jkaminsky@jhu.edu>
2020-10-30 07:06:50 -0400

    Initial commit

--- SEIR/NPI/Reduce.py
+++ SEIR/NPI/Reduce.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5aa327f67e598efa9a8cac459146376e881f86d624e3fc9980c1f35574355199
-size 5299
+oid sha256:31c2ae8b99bd999ff32155ea33dbd374f43ca3f899d7078ed20999cff47dcacd
+size 5411
