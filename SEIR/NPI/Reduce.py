77505bbfe5d7adab86e35794522f6d981ca7e69a
jkamins7 <jkaminsky@jhu.edu>
2020-10-30 08:00:43 -0400

    Updated reduce to properly load in files when dates are strings

--- SEIR/NPI/Reduce.py
+++ SEIR/NPI/Reduce.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31c2ae8b99bd999ff32155ea33dbd374f43ca3f899d7078ed20999cff47dcacd
-size 5411
+oid sha256:488d333444b3b6d20b29f94b6d18ef90ec88592144754f38367ce649f0e1b3b8
+size 5659
