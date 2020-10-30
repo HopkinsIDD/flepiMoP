99fb56f01930e27a84589c7e8e32a858a3f09432
jkamins7 <jkaminsky@jhu.edu>
2020-10-30 09:06:03 -0400

    Got the MultiTimeReduce npi working with inference

--- SEIR/NPI/MultiTimeReduce.py
+++ SEIR/NPI/MultiTimeReduce.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4e60120c61e03aed6e531cdbf9cf3aec5164639aca863f10d2469e18fd93ab8
-size 6555
+oid sha256:273864c41124e0a1020f9a519580ab9662e335adf8ca31fec995debde9e1511e
+size 7309
