a996dbf25703385a7c20cee4d6ba3f5285eb7807
jkamins7 <jkaminsky@jhu.edu>
2020-11-11 23:40:19 -0500

    Updated to pass flag for stochastic submissions.  Totally untested

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fbac38b2306917ffa75044eeb6de4ae0a4fc8f93f36f410f7c5dc508fc82680d
-size 15080
+oid sha256:2f423a41d28457d41bc9723e4a7ea7e007d97efa6fe8748fec965715db75f05a
+size 15433
