e7b834d3b83a020c9046468a571cb7e74fee31c8
eclee25 <eclee25@gmail.com>
2020-11-24 15:30:35 -0500

    enable inclusion of unallocated data in USAF/CSSE

--- R/pkgs/covidcommon/R/DataUtils.R
+++ R/pkgs/covidcommon/R/DataUtils.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:faabef03355cb781f3cf8e942db22e5c2fb2674f1b706117d3c1f46243335d02
-size 33305
+oid sha256:2fa17305ea5ae8570bfa3db8ccd766c0e259062cb3eb68b3b06e08ec783450f3
+size 34178
