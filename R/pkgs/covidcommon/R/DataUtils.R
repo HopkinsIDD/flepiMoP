97a1bcc8b96c9abdf8403cbd5b8077abb171a34f
eclee25 <eclee25@gmail.com>
2020-12-01 00:34:23 -0500

    pull hosp data from CMU Delphi API

--- R/pkgs/covidcommon/R/DataUtils.R
+++ R/pkgs/covidcommon/R/DataUtils.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fa17305ea5ae8570bfa3db8ccd766c0e259062cb3eb68b3b06e08ec783450f3
-size 34178
+oid sha256:a1de5b3def9cfafb1478352c4552695658df7f1643ad6f4b69458415e04e59b5
+size 44122
