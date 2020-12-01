12ef30dca2f08a871a512d2288fb335f75dacfb5
eclee25 <eclee25@gmail.com>
2020-12-01 12:22:13 -0500

    add incl_unass to ground truth wrapper function

--- R/pkgs/covidcommon/R/DataUtils.R
+++ R/pkgs/covidcommon/R/DataUtils.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1de5b3def9cfafb1478352c4552695658df7f1643ad6f4b69458415e04e59b5
-size 44122
+oid sha256:acc8c0340a4bd10bcab40985eb0995039d51bcc16935afb7c967b68541e4e794
+size 44254
