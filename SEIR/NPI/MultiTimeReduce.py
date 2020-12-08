959119c72f9275336b2ddd08a5d6a4abcb04375b
chadi <joseph.lemaitre@epfl.ch>
2020-12-08 21:48:26 +0100

    hotfix for old MultiTime bug when geoids are specified

--- SEIR/NPI/MultiTimeReduce.py
+++ SEIR/NPI/MultiTimeReduce.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0702164a07d08eade34dd74e0ef252a2cf972e3be6c074f75bca6e7d92f7a8b9
-size 6798
+oid sha256:28b5ac007fd5c46f0e3c85804c34808c5be5de0778a16adc2771e149f975c2e9
+size 6820
