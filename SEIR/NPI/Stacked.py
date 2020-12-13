72904f5e069c2b4faa980c19ac375d6338a920a5
eclee25 <eclee25@gmail.com>
2020-12-13 17:46:00 -0500

    add option to choose USAF or CSSE for groundtruth

--- SEIR/NPI/Stacked.py
+++ SEIR/NPI/Stacked.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2ddc08584b5c283ef39975a3589bca7519de61c2d45121384b7c2ed84983e421
+oid sha256:b6df504fd3d1d41f3d5f5a586adf5c92ea7c8bff616b30c3eeb73c9c576d182a
 size 3680
