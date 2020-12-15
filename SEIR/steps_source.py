679c9ed3bfce71b68402618c3b9e02d558060ccb
jkamins7 <jkaminsky@jhu.edu>
2020-12-15 11:27:42 -0500

    First pass at adding vaccination.  No clue if it works or not

--- SEIR/steps_source.py
+++ SEIR/steps_source.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6752555e51631238b696c1f1fe4051bb4ff32d1996981c53e6d73eca11b675a
-size 3410
+oid sha256:69ee4c65c470784e46cd4bb7458dff18c772b8a024d0afc53bca2d07dfe6c741
+size 3901
