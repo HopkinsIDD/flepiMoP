b6311e2e2be641dfdb4049e28c620ab1d714b249
jkamins7 <jkaminsky@jhu.edu>
2020-12-15 14:03:46 -0500

    Updated to get numba compilcation to work

--- SEIR/steps_source.py
+++ SEIR/steps_source.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69ee4c65c470784e46cd4bb7458dff18c772b8a024d0afc53bca2d07dfe6c741
-size 3901
+oid sha256:a9b8233200bf1909cf8a2ed57834496c64bde5524a6e23248321766c90a48d5a
+size 4515
