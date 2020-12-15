347dc63bab728fe71689d23844b046951c20f247
jkamins7 <jkaminsky@jhu.edu>
2020-12-15 11:27:42 -0500

    First pass at adding vaccination.  No clue if it works or not

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55d17752e7ca717c02cfca6be16baab4873278c140305eb82002f66e6d291bef
-size 6671
+oid sha256:e9635c29765dd4ab5419e2e555a5bcc2c57183b2e8b4e098e9d2845ad34aa224
+size 6781
--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8b9719ad14c52e00df28ef7ceeb1f35dce530ace3600e36e2bc3b14303abc75
-size 17745
+oid sha256:5d4e9f53e46370858fc37aec373ede41e60d03f416295ec238db3a8eee43d533
+size 18646
