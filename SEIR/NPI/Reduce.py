a00fb1e680e993d645f34f8cd2605eb74d368701
jkamins7 <jkaminsky@jhu.edu>
2020-12-16 16:33:03 -0500

    Added first pass at moving between vaccination levels

--- SEIR/NPI/Reduce.py
+++ SEIR/NPI/Reduce.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4495ad5863291eb73e2a863f5572b714d0e84ab7fbb529c9910cd751261b0451
-size 5666
+oid sha256:12cb295b7a22d9f31261f187deeb2c200f6c47d591ca2205894f14afd66c7596
+size 5717
--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e5712a8f491477ed39fda73cb89341fd327d53604ec6dac19554f48e211967b
-size 19919
+oid sha256:c9a2ac6c433523d34f795fdc6b111cf10e1d2b212f2696d5d6b60661e914aae7
+size 21174
