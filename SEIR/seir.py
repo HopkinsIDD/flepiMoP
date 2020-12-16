e07775a2913e6148aa9b3bad22967c215172a60d
jkamins7 <jkaminsky@jhu.edu>
2020-12-16 17:14:43 -0500

    Theoretically fully working vaccination (but evidence suggests problems)

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b5154c32042f8179c39bb4a0dc8da5511ccb551c727580d4988ef8b700e6741
-size 8747
+oid sha256:c43d478ec99297d952a18444cddd73442e734dd2bf790d1bdd61424004c8ed43
+size 8898
