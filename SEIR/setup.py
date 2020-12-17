5aaccca90257f6b87b7286be83b1971aefcdb6eb
jkamins7 <jkaminsky@jhu.edu>
2020-12-17 15:45:56 -0500

    Reformatted things to use the transition graph approach for the parallel configs

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a47cc66451438b91337ec1516530cea993aa6a6a20ffb22dc1d8a4007f45403b
-size 21508
+oid sha256:5f4867f7c364138fa1241bb9af8cc2c81910f495c457fe775e7d85989b13b457
+size 24944
