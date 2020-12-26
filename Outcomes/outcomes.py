ef8db9740e92d3be1375a7cc4125f7cf14a18f75
chadi <joseph.lemaitre@epfl.ch>
2020-12-26 19:15:53 +0100

    MVP of outcomes vacc

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ad1f59ddca971e48c12ae9970a2797f38988bcade7b6691cbc3dba35b0c6a43
-size 27921
+oid sha256:7372c7c6c52a36964ce6a66991ad718d97212546cda2447577b183e47aa0422a
+size 27788
