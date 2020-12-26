f742444485c5d8b169621471dfc3fafcedf2fae6
chadi <joseph.lemaitre@epfl.ch>
2020-12-26 18:38:27 +0100

    p_comp for outcomes

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59f96315f7177434a1bd13dd8267cf2309ba0f96b1b959513a9ba9af88e80b7c
-size 27082
+oid sha256:5ad1f59ddca971e48c12ae9970a2797f38988bcade7b6691cbc3dba35b0c6a43
+size 27921
