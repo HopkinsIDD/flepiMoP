04806b08820b0bff0beb191c88f306b522a1a146
chadi <joseph.lemaitre@epfl.ch>
2020-12-26 22:22:07 +0100

    Fix for test: dates were not ok

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0407520052e70d13f6c6a2284f3c74e3e186e39e3c7cdb164dff875527691f96
-size 27744
+oid sha256:4046fe349eb863065b654cb6f67c661b383ac1dfde42aa0dd7cab3a99dd1d501
+size 27792
