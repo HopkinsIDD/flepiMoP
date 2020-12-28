806fe3dc4e0170ebd940e5c908e569af28dee8f4
eclee25 <eclee25@gmail.com>
2020-12-28 16:04:45 -0500

    fix buildUSSetup bug

--- R/scripts/build_US_setup.R
+++ R/scripts/build_US_setup.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1330561e04db22c13b41dd8f857a210a89dd4669ea34910ea52a8e0b29261269
-size 7955
+oid sha256:03ade14883b736ca7d83cdb9b97a0203c506749d45981c091a03c9f40fc7f72e
+size 8292
