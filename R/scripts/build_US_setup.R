1829cf9e28964d21c75541c09ce89b615c9786c5
eclee25 <eclee25@gmail.com>
2020-12-28 17:29:11 -0500

    fix typo

--- R/scripts/build_US_setup.R
+++ R/scripts/build_US_setup.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2158dfd14b726426941e34149ff62a80631c470867e4fa7312ccc261f8ec6709
-size 8363
+oid sha256:716b8dc5b2a0cf5f5f2a4d987fb9cb74809b44dc5f835b7dbe4a7c865e927308
+size 8364
