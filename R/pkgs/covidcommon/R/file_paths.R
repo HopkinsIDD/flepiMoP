4f88869c70b325269526c4744752e592d025e80b
eclee25 <eclee25@gmail.com>
2020-12-28 19:57:54 -0500

    fix roxygen docs

--- R/pkgs/covidcommon/R/file_paths.R
+++ R/pkgs/covidcommon/R/file_paths.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd8729f7d45e9e202c0a18626e09ffd363c563ca548f55484554ca04e157d07a
-size 5180
+oid sha256:b119dfe7fc04fbba120f8c4ca29efdd97a54f8fc344f9745405a25a7c04c13fa
+size 5296
