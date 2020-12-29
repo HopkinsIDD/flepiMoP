8815088fc8608e4a1381463f6be55008c57fab43
eclee25 <eclee25@gmail.com>
2020-12-28 20:01:15 -0500

    incl_unassigned TRUE for seeding at US state scale

--- R/scripts/create_seeding.R
+++ R/scripts/create_seeding.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5624bbd6e12eb88170baf7effc7daa02d4d2a72dd9b87875889f9fe3791f864b
-size 3319
+oid sha256:539ffd33733537f1b34a4333ed60d8e1f0364801b36321de512bc2b55acd2662
+size 3437
