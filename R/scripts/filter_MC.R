d7a17cf8a33f6c9fd40999c9ce1dbac68bef4383
eclee25 <eclee25@gmail.com>
2020-12-28 20:12:21 -0500

    clean up state control flow and scale arg

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:991dc557b01425b60c0417596144c6c0f79a6d8ceccf5f15ad3f433941d8c3d0
-size 17328
+oid sha256:872e0d4269378a8d39f0dc0a95ffb3a2820bea1f34ef8dfbfe026a8db0ced992
+size 17156
