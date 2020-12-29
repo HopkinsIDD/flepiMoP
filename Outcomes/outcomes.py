7cf43d6641707450d7fede3c6762bff801792602
chadi <joseph.lemaitre@epfl.ch>
2020-12-29 15:07:50 +0100

    test for NPI on outcomes

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4046fe349eb863065b654cb6f67c661b383ac1dfde42aa0dd7cab3a99dd1d501
-size 27792
+oid sha256:1e124f11cb0bbf6cc68edf625b7a659b609906647029cc6a58ba2d9c38d8f018
+size 27795
