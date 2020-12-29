456fb2034879aec2ac57460db5ca788f83d73ba6
eclee25 <eclee25@gmail.com>
2020-12-28 20:06:12 -0500

    fix gt_scale arg name

--- R/pkgs/inference/R/groundtruth.R
+++ R/pkgs/inference/R/groundtruth.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1af9f18e9f053bc70ba3684e888f78eb7eb7c59904d90306c6fde3094af64a2a
-size 2549
+oid sha256:a1740d8be4635109351bf5b1600cb17c656a9dc49fd7638cdedb94caf77ca045
+size 2558
