89b3a854b66f4b3898617c4f7ed6de96615f5ae4
chadi <joseph.lemaitre@epfl.ch>
2020-12-29 15:09:39 +0100

    remove some dead code

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e124f11cb0bbf6cc68edf625b7a659b609906647029cc6a58ba2d9c38d8f018
-size 27795
+oid sha256:f9e9c027c7969fc6aed292652ae8d052971b86d526668d6eaa92db708f6e4e16
+size 22453
