56725da158d49f8ec2cf535a166e1c4f9fbfb205
eclee25 <eclee25@gmail.com>
2020-12-28 19:58:57 -0500

    inf groundtruth defaults to incl_unas T for states

--- R/pkgs/inference/R/groundtruth.R
+++ R/pkgs/inference/R/groundtruth.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3868ad78c351dbf88fc92a951702211b98eea22170b9e493033337ffdcf99977
-size 2502
+oid sha256:1af9f18e9f053bc70ba3684e888f78eb7eb7c59904d90306c6fde3094af64a2a
+size 2549
