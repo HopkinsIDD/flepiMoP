f237bbc50e88bc400b126c07340cdddec17dff13
jkamins7 <jkaminsky@jhu.edu>
2021-01-05 12:26:59 -0500

    partial fix to vaccination + inference

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad3cf49035af137ac410dab1211608f827a23d5b6011453eb19258037368dfe0
-size 25753
+oid sha256:e262046fc80396bdcb85c1cc4e6c6f70621237b7fe67a94080b15ab0fd9657c4
+size 26032
