d5d1c9a4d6e3338a9391eaa1b693c6906fa95b54
jkamins7 <jkaminsky@jhu.edu>
2021-01-06 14:39:22 -0500

    Fixed problem with p_draw double loading

--- SEIR/steps_source.py
+++ SEIR/steps_source.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:584f5af040abae0c2935a01b5a801eacd73c2dc32c50246564d6bcf168396fc8
-size 7693
+oid sha256:1c96e3d7d66c410013781a8f134d7dd00c6ff3d947d2fe45cc9fcc43639b4ba3
+size 8263
