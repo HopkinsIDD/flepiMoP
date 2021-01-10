b671174e696fe4387b3c500e0da601d28a0b4584
jkamins7 <jkaminsky@jhu.edu>
2021-01-10 17:03:14 -0500

    Ability to modify # stacked interventions via environment variable

--- SEIR/NPI/Stacked.py
+++ SEIR/NPI/Stacked.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ca5db585ba3f6c7aade786570183b742960ad9aab76330b0f3a596025cc278f
-size 6246
+oid sha256:bd20eda3a5cb6a2a2566fd8809c6f2ec98231f08a0a00970134b23c42722b8d1
+size 6426
--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9f6ea432194c519013c613f9517d4436558b596afd47a982913c505c4b74b41
-size 15434
+oid sha256:d379bb9575d9f93a9a72b4f70195fa6d0563f23a4b7a3e7edf35af69b8f7481a
+size 15729
