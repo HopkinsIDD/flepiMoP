a4339540109cd8ad460b02f01a5b0fd3a6e75919
jkamins7 <jkaminsky@jhu.edu>
2021-01-10 22:58:56 -0500

    Reorganized to optimize main use case

--- SEIR/NPI/Stacked.py
+++ SEIR/NPI/Stacked.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6419d362532fd65651fd9aba636070e93c96cd8f6d1587e7b1d37fa23c55c014
-size 6302
+oid sha256:073233a6d63cbbac34e6a99d70d34e440c5d1990a567dfdb7d5da02f04567e08
+size 6345
