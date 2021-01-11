600ac3d889521c991fb0f844fb0f593d98b0d5fb
jkamins7 <jkaminsky@jhu.edu>
2021-01-10 23:51:02 -0500

    Forgot to localize the object from command line arg

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c57c7840cec335ad7ffb9ee29f11944369ee0ddb9eedf56798bdfe0480e7628d
-size 15912
+oid sha256:584cb802dfdf81440432a01c6bda377547a87bc44d4b8d2a0b4813097a871b00
+size 15917
