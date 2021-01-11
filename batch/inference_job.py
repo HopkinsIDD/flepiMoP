e303d96117c2ab600647dc7ad2e86612a8114b6c
jkamins7 <jkaminsky@jhu.edu>
2021-01-10 23:22:49 -0500

    Typo

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37de4e50631b1f960e1dc0517d062f1a6b687f5c15326ef4e7804371491ccca4
+oid sha256:8e8ad411eb0ddc54b19396f43409275b7c770e8906d351fc42b85a5285e7feee
 size 15796
