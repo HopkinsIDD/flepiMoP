5df20fe7d045b960975d97a10f5f89fe1e447cbc
jkamins7 <jkaminsky@jhu.edu>
2021-01-14 21:41:30 -0500

    Fixed time aggregation code

--- R/pkgs/inference/R/functions.R
+++ R/pkgs/inference/R/functions.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6e63fa88965bccc443a2e06ca5bfc0210f4bf3f80053a7a094dc45f7fb8e90d
-size 16266
+oid sha256:26e6765fb0c0cbd67c2533dceb3c97cb353ca624cd8ca0b688936529fa0d7a51
+size 17182
