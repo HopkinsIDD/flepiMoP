e08e1ad05c5ab6c7a9f0903c4a74ffc349823470
jkamins7 <jkaminsky@jhu.edu>
2021-01-14 21:41:30 -0500

    Fixed time aggregation code

--- SEIR/steps_source.py
+++ SEIR/steps_source.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c52a8982ad3154501026d5eab511bd4522947cff6ab937aa257efbbbc31e0ba9
-size 8540
+oid sha256:60aafd2090c20806d1e386bfd66f358adb3fbc04bf2dada404b5e868ca6504aa
+size 8626
