a558611eb976349779b2fa59200c0bc229e46169
jkamins7 <jkaminsky@jhu.edu>
2021-01-18 10:35:34 -0500

    Added validation end date to aws submission

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:584cb802dfdf81440432a01c6bda377547a87bc44d4b8d2a0b4813097a871b00
-size 15917
+oid sha256:3e81f828069e9f57bdfef21c9d3933d6ddad1e6a7aad34f99a998332210e969a
+size 16328
