47c838dc7a9d6a634d1ed7c6352976f20247bdd6
shauntruelove <satruelove@gmail.com>
2021-01-18 22:11:11 -0500

    fixing last_validation_date error

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a721dfcf68227d4d7aa6a5c5fa1f09fdea812ff06cdec5edfae9f2956f6d731f
-size 16355
+oid sha256:93c077cd1712a331ca8115e13e53685878e993b71e87fb34fcc4d34b30bc4eba
+size 16377
