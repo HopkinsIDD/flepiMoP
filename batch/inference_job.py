48967b49479e8d18cb1bbcca356694ac695b3da2
jkamins7 <jkaminsky@jhu.edu>
2021-01-18 21:54:06 -0500

    forgot to add date to argument list

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1363ed802cfb9645c7e440ee060f0db6ce60f0cd688a22888cb56023b0cf374b
-size 16333
+oid sha256:a721dfcf68227d4d7aa6a5c5fa1f09fdea812ff06cdec5edfae9f2956f6d731f
+size 16355
