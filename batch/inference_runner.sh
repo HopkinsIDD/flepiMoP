9635ebac7fca39e809a01cb7095bce2b2e93f443
jkamins7 <jkaminsky@jhu.edu>
2021-01-22 10:29:45 -0500

    Added copy of final seeding files

--- batch/inference_runner.sh
+++ batch/inference_runner.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79493e4b8a0f88b951f0afdf47197f740d3469901b8b60c0e6a6372255caad5d
-size 5754
+oid sha256:fe2133de214989df59b9cc8d71d504620ad492b52fb87ca3065ee7deb5bf6354
+size 6034
