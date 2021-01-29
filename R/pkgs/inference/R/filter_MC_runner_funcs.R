e90d21d170d829f1edc051a33c38510bbf18112e
jkamins7 <jkaminsky@jhu.edu>
2021-01-29 10:22:37 -0500

    Adding some other date formats

--- R/pkgs/inference/R/filter_MC_runner_funcs.R
+++ R/pkgs/inference/R/filter_MC_runner_funcs.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0dd95f8371bcab1aac31999bab992259a632acd8fd45f0d1d07a5d327aee7dd3
+oid sha256:5da13da428bc7734d4bb987e9130adac08bfb75d2a526d6b169bebfdcf0b6aba
 size 17935
