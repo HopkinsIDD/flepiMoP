7227701eb505f4d3d66f3257f5bb42c1fc58275d
jkamins7 <jkaminsky@jhu.edu>
2021-01-29 10:29:47 -0500

    Updated setup.py to include the modifications to seeding_load in addition to seeding_draw

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb39d91270b780ae30b6d27b9076ad577013807d1e5bd902a5152a236d6ff13b
-size 27526
+oid sha256:44660b40c3ca17d70eb7d231faa5adf2e16957fefe759f618c6993bf4659b201
+size 28963
