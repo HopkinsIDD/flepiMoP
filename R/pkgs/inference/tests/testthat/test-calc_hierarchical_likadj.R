326047618e67bd39594b3658d2fd2960223c84b5
jkamins7 <jkaminsky@jhu.edu>
2021-02-02 13:55:00 -0500

    FINALLY fixed the tests that randomly failed

--- R/pkgs/inference/tests/testthat/test-calc_hierarchical_likadj.R
+++ R/pkgs/inference/tests/testthat/test-calc_hierarchical_likadj.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d4cc00c1172d625eddcb9736849ccbcd7553cac72a7cdbd0a6088e70de8fb7c
-size 7464
+oid sha256:fd0458c4347566e1b0fd1c4517ed028a216c3e2b3822776f061248e01287a804
+size 7506
