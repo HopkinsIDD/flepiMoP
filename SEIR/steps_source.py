b42a2d7885d7128f7b49c66d8adbe3841437c109
jkamins7 <jkaminsky@jhu.edu>
2021-02-04 01:25:42 -0500

    Changed from daily vaccination to vaccination rate

--- SEIR/steps_source.py
+++ SEIR/steps_source.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b54d9eaf677ef5ee30e1ecaca457ca00685b9ab139522b68d592a186407d1ff9
-size 8738
+oid sha256:f2f37e6f43a7b6946668e0837062ddad9a17bd1588816922aa0d996cb6729d92
+size 8641
