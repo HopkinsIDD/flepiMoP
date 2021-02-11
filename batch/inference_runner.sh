be290fe93ccebfdf4bda59ad6b5ab1f906d38788
jkamins7 <jkaminsky@jhu.edu>
2021-02-11 14:16:49 -0500

    Forgot to not import llik

--- batch/inference_runner.sh
+++ batch/inference_runner.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eda41f1786d9aa70590a988bab1ffe2d9916d2e8baefafb61e9107ae203aa7c1
-size 5910
+oid sha256:add5e5939ca011a4f2efdb01a4f0f7a30d30b9548dfc0114fc8c41e207fba06e
+size 5896
