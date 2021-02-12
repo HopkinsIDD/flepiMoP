50ce1b436ea8c45ba4db6cfedb81d6583fa6fd37
jkamins7 <jkaminsky@jhu.edu>
2021-02-12 15:13:31 -0500

    Destination filename not correctt

--- batch/inference_runner.sh
+++ batch/inference_runner.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32c039414bac983ba602a42b0966618fefa392ee69cc2c965018f5ba1daae563
-size 6462
+oid sha256:fa2f99229dd07ef82644b2708794c8c637d47335d6652a019bb3a62ab1dc75db
+size 7478
