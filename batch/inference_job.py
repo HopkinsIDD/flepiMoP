071ecba4afd4ff63f13ec8acfd01cfa90181f521
jkamins7 <jkaminsky@jhu.edu>
2021-02-12 11:03:07 -0500

    Forgot if statements

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f877c40ac64b2ab3fafe26a2253f23f865266ab87e9c73c906e76fb324d6866f
-size 17513
+oid sha256:5be2d89b513da96a5afca7aa742d522bb22e4f19c66267470f3ba6831099fe3f
+size 17334
