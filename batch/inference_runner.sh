45a76c5b6c582bc92d3c1fe9d3cc6d0ead2f5bd7
jkamins7 <jkaminsky@jhu.edu>
2021-02-16 11:31:44 -0500

    Fixed typo with types

--- batch/inference_runner.sh
+++ batch/inference_runner.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4282b0cbec9716ee1851f3e9b53cb8b33a6b6fd29d937d078a80d93dd11abc1d
+oid sha256:a295627cca0c8af58001ffe20eb9b8d5e22dfbb5f424a480629d16e43208bc79
 size 6125
