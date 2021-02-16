4c94660f8677ce1ebf13e052c7798d28296fb001
jkamins7 <jkaminsky@jhu.edu>
2021-02-16 11:10:46 -0500

    Added force resume option to cause failure when not resuming

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1709dcbe1b232344ce90511627b3ac2dbc2cbe2feff6076b74c97c8ad49968dd
-size 17312
+oid sha256:41c73447ee70f98cc5a50a4a3a56408423e150a0456d272f0eafce50e38bca17
+size 17395
