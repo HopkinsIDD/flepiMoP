4b3ac265ab039fd7abde7bd5f7ab1ab79653d9e1
jkamins7 <jkaminsky@jhu.edu>
2021-02-17 10:31:59 -0500

    Fixed problem with opt name on use

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b98868a0c5fa489b49d7ccc9279f458bbadc2d461f9aa15971945bd38bc827e
-size 18181
+oid sha256:6e8272792760deb73faad953771cfeae0e0d16de68b27c4a292da05654436195
+size 18186
