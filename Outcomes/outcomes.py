0308d6edc3a9f0c825ee48154b6a5fb3aa4579d8
chadi <joseph.lemaitre@epfl.ch>
2021-03-14 20:52:23 +0100

    tqdm should be able to infer lenght now

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e917d10f6252998bfcddb9413ffbd1ad5caf00bc7b1db6f5860b4a4cced13af7
-size 23701
+oid sha256:05ac491c6aee99ea7b1be4283a29c0b96bcb7272bcfd224071198948148b2d8f
+size 23772
