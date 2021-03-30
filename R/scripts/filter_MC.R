c3c8eb3e32a66a25b0140b964b66a3391eb71a92
jkamins7 <jkaminsky@jhu.edu>
2021-03-30 10:05:38 -0400

    Updated ground truth date processing

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5dea00a2093ec9202a80ac703e0d0e57e12002da415dd8a2617e9b9ecf3ecfe5
-size 18425
+oid sha256:f9dd2b2a7ad0d5a2a98282f8a98f4f6460d22af00dcd5ffb11db3d2aec74e4f7
+size 19310
