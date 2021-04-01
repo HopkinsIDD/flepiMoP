255439e699063fc682a3e77dc30d6721d0ef03f1
Alison Hill <34223923+alsnhll@users.noreply.github.com>
2021-04-01 03:36:56 -0400

    Cleaned up all new features added, moved to functions

--- R/pkgs/inference/R/filter_MC_runner_funcs.R
+++ R/pkgs/inference/R/filter_MC_runner_funcs.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:42242f1236756ec2c44cf7760ad378ef79e733e271a82ebeab19a12fa2d14a8a
-size 19528
+oid sha256:e877ccf0c2dcd955a8afbbba899847cf12f8972751417bda8f0c8cb94a20c249
+size 20313
--- R/pkgs/inference/R/functions.R
+++ R/pkgs/inference/R/functions.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2dc8aac27f5721c72dd6e478af31043f03da322b282559acdcf7448104a3645b
-size 18712
+oid sha256:41cfe2c7ffbb2f4e2a0e65893429ef55700761f851f279f510f7b184d1e051fc
+size 28421
