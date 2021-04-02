346db556698b3e21407eef1ca3356c2874df54f8
Alison Hill <34223923+alsnhll@users.noreply.github.com>
2021-04-01 23:28:49 -0400

    cleaned up a few functions

--- R/pkgs/inference/R/functions.R
+++ R/pkgs/inference/R/functions.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41cfe2c7ffbb2f4e2a0e65893429ef55700761f851f279f510f7b184d1e051fc
+oid sha256:8a754ae2f6c0396df240d0ec8f167e6c96a4bc7c1b4f2cd6a4886626005c02e3
 size 28421
