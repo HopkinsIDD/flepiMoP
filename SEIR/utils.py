0a2236a224418e0229fb1bf9ad8355eb361213fd
chadi <joseph.lemaitre@epfl.ch>
2021-04-22 15:35:18 +0200

    proper logging and timer class

--- SEIR/utils.py
+++ SEIR/utils.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afc898dd7594a91959a12e8b7d211730b99ef2ff9bed589d255a1251f0fff4d4
-size 3370
+oid sha256:d9eea17ad3152500730b9bcd2bbc7cdd25e3ab421d1d3ef64d94311cc8dd5712
+size 3778
