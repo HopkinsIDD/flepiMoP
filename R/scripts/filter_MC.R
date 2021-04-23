a5602088e5a2d295f57ab971b303a2dc0fca08ec
chadi <joseph.lemaitre@epfl.ch>
2021-04-23 12:32:00 +0200

    final form of reduce intervention, tested

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d35f375bcef668133126221bde8586f1d16a4aa0929b6f131ea406b91cc96eee
-size 19378
+oid sha256:32a67ba1ec9f38247eaa4711555d0763cf2872015f1b2b0dd165501c75cbfd86
+size 19443
