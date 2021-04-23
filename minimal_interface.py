84479b39eaabf4bf4535ea45a1266e4bad78c292
chadi <joseph.lemaitre@epfl.ch>
2021-04-23 13:15:55 +0200

    retaining possibility to run several files

--- minimal_interface.py
+++ minimal_interface.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4ec7c486ced974e25e8527f5663d8bb3c27967a491e9aab5f096a8ec44c3034
-size 6508
+oid sha256:76dddfed736d71f4cc889e983dd9c28878608c504eac79393442347dff664638
+size 6560
