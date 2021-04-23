468ac13ead18f2d8309bd9444f6ecd331846ccd1
chadi <joseph.lemaitre@epfl.ch>
2021-04-23 10:56:01 +0200

    change environement variable to COVID_LOGLEVEL

--- minimal_interface.py
+++ minimal_interface.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97e7134dfece11afc4ff9b644516e856aff16461016fdd261b40802168a72372
-size 7107
+oid sha256:e4ec7c486ced974e25e8527f5663d8bb3c27967a491e9aab5f096a8ec44c3034
+size 6508
