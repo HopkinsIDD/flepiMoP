bd760d6ab80fd939cb55cfadeb9242f8e11ac63e
chadi <joseph.lemaitre@epfl.ch>
2021-04-23 10:46:01 +0200

    pretty print for options in covidcommons

--- R/scripts/full_filter.R
+++ R/scripts/full_filter.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1336339b03c426b5d4aa52afbe50353df613bb917f72afb675c3f94e1003574c
-size 4152
+oid sha256:9fa5303c1d4304c5f6d4247851008789911fb1b4c5ef5c6630a7378c98aa97d5
+size 4103
