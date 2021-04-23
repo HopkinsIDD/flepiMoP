30616c422295e7ea91b7273653b8c104d4fdb2ab
chadi <joseph.lemaitre@epfl.ch>
2021-04-23 13:15:55 +0200

    retaining possibility to run several files

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:155c039941ab3f8a9ee7b2ecc58aede1183857ba5dafb4b02521ccd63018b4f4
-size 8668
+oid sha256:aa9352139e8eee62c1c10e2da601704af73ab1ac2fbc099fe5b0b84c51c922b7
+size 8665
