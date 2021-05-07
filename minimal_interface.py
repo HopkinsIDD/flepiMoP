d17976ea265a3808c8bcf2845a387c27f0b2e76a
jkamins7 <jkaminsky@jhu.edu>
2021-05-07 12:00:54 -0400

    Removed merge residue

--- minimal_interface.py
+++ minimal_interface.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2338b1ad0fddc4791bdd31da0fcb6a8d565ec347c97284a3751fd26890d4a9e
-size 6625
+oid sha256:2da7740e60cd9d1f797644e2e081d0a409fce639780106d169f8d636713c0a78
+size 6612
