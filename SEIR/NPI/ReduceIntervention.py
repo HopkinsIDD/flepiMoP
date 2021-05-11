aea5f70baca31036602cb1bf28532165e7be26c1
jkamins7 <jkaminsky@jhu.edu>
2021-05-11 15:59:06 -0400

    Fixed a bug in ReduceIntervention

--- SEIR/NPI/ReduceIntervention.py
+++ SEIR/NPI/ReduceIntervention.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:79fe88b987e9393ba5e445b0d51634983d70d9bb6a307d0505ba7e54349a440e
-size 10807
+oid sha256:a9ff6157ec4559aa4d2f795b51cacb1ef2045e18c5133700663154a0e44c7046
+size 9437
