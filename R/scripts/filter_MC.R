3acc90d941feb8ab26bb80b1fe14f39b00ebddab
Alison Hill <34223923+alsnhll@users.noreply.github.com>
2021-05-28 17:44:59 -0400

    minor changes to report functions and printed output

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0f9bedfe8c9d7ac52d0de0a59c4b9a75d2b2e6f6926932718a8e63ef00324c2
-size 30928
+oid sha256:7a22169aff4f72cca185ca329996141a6e2826ca574017b289b8a0e3abc4ade9
+size 30848
