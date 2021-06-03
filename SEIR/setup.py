b7fab3d2cba12fbad19223af029b380e78c416e6
jkamins7 <jkaminsky@jhu.edu>
2021-06-03 11:44:30 -0400

    Deleted old code

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4f0f526680312a2cdfbf8ca315f211d2a639cdaf6968144277cdad1568e8045
-size 45303
+oid sha256:ecf71104c1226b906a025bd2e1c643fc2720ce573ec412f5896013c0da1ee998
+size 43796
