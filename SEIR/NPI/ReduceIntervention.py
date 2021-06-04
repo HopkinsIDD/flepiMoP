ad59c4ae10d3aa96557d25d068f66af9e8657c3b
chadi <joseph.lemaitre@epfl.ch>
2021-06-04 15:21:40 +0200

    Specify to NPI which operation applies to which parmeters

--- SEIR/NPI/ReduceIntervention.py
+++ SEIR/NPI/ReduceIntervention.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37be679425923bf25e70561f7abc48048d910509bc0263e190f396932ee4d3cc
-size 9384
+oid sha256:d1c8f5c0989bd010465b2c67fcbfdfe1bc44ec9090e56365ae90c719b442a33f
+size 9500
--- SEIR/NPI/Stacked.py
+++ SEIR/NPI/Stacked.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95f28135eca4baaa99e0caf503072dec355c44b090c298dcb3303572fb065381
-size 6706
+oid sha256:4cd722151e24d99b6f47a01416601be3f7463755dc1ebcd1627512dafc43a9a5
+size 5189
