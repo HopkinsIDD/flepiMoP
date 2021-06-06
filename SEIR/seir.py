956876d711c6492774ac5a12d4ad552b8b2d41e8
chadi <joseph.lemaitre@epfl.ch>
2021-06-06 11:12:13 +0200

    updated to the new parameter object while breaking things

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ec1e764ad3d7407339148fedac613b31d77238db3782efe663e8746e79a0612
-size 8982
+oid sha256:e5a2ee85e22ed2c65d117f0fa787f3be20e44dd85eb51c3e55377ada3c740711
+size 9173
--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f448abbca95dfb212bcea26139251f84da68e4cdcd289dea244e2926225ab443
-size 33146
+oid sha256:62995b5c5b2d2d7e626669d14e6466bdb413ddc4fee7435a4d756a58431b87a8
+size 33256
