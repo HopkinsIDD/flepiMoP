41cb0a1bd597f935e69ef702339ec1cd04ae7145
jkamins7 <jkaminsky@jhu.edu>
2021-06-08 17:13:01 -0400

    Rate parsing and indexing should now be working (and exponents too)

--- SEIR/compartments.py
+++ SEIR/compartments.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52b85fa67e62f3013d89d5c0201c30059c350963b2e20105dd5f2aa11388e1ba
-size 15799
+oid sha256:e54bbb44642ab1f8ed9d9e351384f0521400d0a31fd1861f13b77b75a8b4973e
+size 16115
