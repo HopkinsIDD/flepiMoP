b34f4e8d6a97461a0539d730543c9a772e4c8b76
jkamins7 <jkaminsky@jhu.edu>
2021-06-08 00:02:57 -0400

    Last changes before bed.  I *think* that steps_source is almost working.  I need to reorganize the proportions data, so that I can properly sum over compartments, but otherwise should be working.  I set up a new test case mimicking test_seir.py (test_new_seir.py) and have the code to get things running there. It doesn't use a proper seeding or rates, since that's not working yet, and it also needs the proportion change, but otherwise is working (overflow error is from going negative on account of stupid values

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58856de394f0324317bde1dea0aa586404f50fef0d5ea7a2b60823bb6ada7c6d
-size 21732
+oid sha256:d9b4bec9072e200920437dbc6125864432361a5bf3cddf86a70612518641b631
+size 28132
--- SEIR/steps_source.py
+++ SEIR/steps_source.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:431e1f6ef2a082d30d8d4be72b0766cca90e68a7ec230172fc1f1a0778b4eb51
-size 8654
+oid sha256:6b7f18b8749fd7f889ce5101229d7fdfa4aa08b332d1ec1f449aa3531f9146f5
+size 8357
