af0ffd329198ac51521e6b1339281e0c6a232c69
chadi <joseph.lemaitre@epfl.ch>
2021-06-09 20:41:41 +0200

    switched to numba dictionary,hopefully no bad side effect ts 🏻

--- SEIR/steps_source.py
+++ SEIR/steps_source.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0fbcff781edbf917c2c294fd339366e6e2a2d1495eb776da8e12898298143d5
-size 8678
+oid sha256:f68c66e8fcaf266938c712b02fbd68df29382316b0f9bd586b3fe8f7684ab5b5
+size 9317
