2874ccd87e4ae6c732390ee3bfc78e955fee1a1a
chadi <joseph.lemaitre@epfl.ch>
2021-06-09 15:30:47 +0200

    seeding wip

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62e5212df7b394950ada861029d2695a9576e2d1294db8103ba0108c165aa392
-size 9294
+oid sha256:0eabddac08241a2ef0616ab62ab9ff1e4e8b9e089a2a576fc109ce40a4cb550f
+size 9314
