991f5f61a002dafd46daada05b540c2f3b156412
chadi <joseph.lemaitre@epfl.ch>
2021-06-09 18:33:57 +0200

    method to access compartiment from dictionary

--- SEIR/compartments.py
+++ SEIR/compartments.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d56102d889b126bc44d2f77f158257f0e803023a2d7d0f2a30d2966d3329c2f2
-size 25295
+oid sha256:d42690da15ada87dc3e77eab749c90f900b572f59994bab06d68cd1b405cb08a
+size 25704
