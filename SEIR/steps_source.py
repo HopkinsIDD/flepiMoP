4fa13b8b5fa3a328564d11ba682fbb711f09b973
chadi <joseph.lemaitre@epfl.ch>
2021-06-10 11:44:26 +0200

    setup.py edit for us to be able to mix imports from site-packages or relative imports, what a headache.

--- SEIR/steps_source.py
+++ SEIR/steps_source.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f68c66e8fcaf266938c712b02fbd68df29382316b0f9bd586b3fe8f7684ab5b5
-size 9317
+oid sha256:1790af33aebb69129c6f0924dfd38841dd2933e2d14e09f1fab986693ac4d091
+size 9326
