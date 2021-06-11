1fe83952315bbe174ebea95b47251d5f41fab597
chadi <joseph.lemaitre@epfl.ch>
2021-06-11 18:30:54 +0200

    clarify t_span into ndays, solve off-by-one error causing segfault

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:322acd07444e4e406ffd51ee7a2a309a81f04d65723bf6d3f5ba8cc3cdf4598a
-size 11241
+oid sha256:5c19c5e7b2db4d8ada5a49c48cb7680d9e0c000c3ea79d649855edd3a4a70f71
+size 11338
--- SEIR/steps_source.py
+++ SEIR/steps_source.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a586014220b6ccd5d0cc1e273a2f57808e80bca9130f3cba5694618a8f8feb1b
-size 9521
+oid sha256:924fba22cff9f9a766614f4ddc73b4446e82537529ab2fe142e989a20be17a96
+size 9735
