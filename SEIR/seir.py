15ca8ca571cfad5091351502454c931ab190fd5f
chadi <joseph.lemaitre@epfl.ch>
2021-06-13 22:24:25 +0200

    ability to write new results to disk in a fancy dataframe

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:857a6f7572620e6e2538bb5350b764f4cb191377f7d03caf7922cfb0a5af134e
-size 10576
+oid sha256:b3bdb3dade0b042d9dae1bc00d8e63d85dae7d12da95945ae1adeec63af895c4
+size 14204
