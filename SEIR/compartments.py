39680bf75d787245ba2cbba2ab43f67f106f8e1b
chadi <joseph.lemaitre@epfl.ch>
2021-06-13 22:24:25 +0200

    ability to write new results to disk in a fancy dataframe

--- SEIR/compartments.py
+++ SEIR/compartments.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eecbbebada35b9f8f07ca507f573514fa6533ac956a122348c63dcd35474eceb
-size 26217
+oid sha256:bd56b1c2532ffeb1025e0e5c2c7e4bbb301bf8937f2f813e6c261db4647ef40f
+size 26452
--- SEIR/steps_source.py
+++ SEIR/steps_source.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd641d6a3cf242b106a4f7abdd2b1c06568328c45e0f209b40c3dbcc106aff7d
-size 9675
+oid sha256:1528a41b2aaa1fb7ad962c97ebbd1d6b0324eb1611701a3ba09086ba18a67a11
+size 9673
