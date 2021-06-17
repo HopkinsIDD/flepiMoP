3b6025975c3b78e7e09776351e65aae013fdd04f
jkamins7 <jkaminsky@jhu.edu>
2021-06-17 09:11:11 -0400

    Fixed stochastic v non-stochastic  bug

--- SEIR/steps_source.py
+++ SEIR/steps_source.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:250dead22dffb15f49c88ed2e8e230f9bb39c7aae299696e2973072ab52a81ab
-size 10464
+oid sha256:d62cffcab0696d778809839b9da0447fc87b44b4cf82ee0156811c3de506bb28
+size 10561
