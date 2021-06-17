5260c87f0a1b44199f6aea6c52b5a88e195e0e69
jkamins7 <jkaminsky@jhu.edu>
2021-06-17 08:37:52 -0400

    Fixed an issue

--- SEIR/steps_source.py
+++ SEIR/steps_source.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9213f191fa7f4e580f15305607e2420e31298a4838b6863e95b01cf1351f201
-size 9878
+oid sha256:ec87e21420618d3e1362ec841acdbb1b91f659f8d97ded05704130bdc9cd1fed
+size 10077
