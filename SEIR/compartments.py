0123b6cf2838f9845a942717d2cb7f7da54f8b09
chadi <joseph.lemaitre@epfl.ch>
2021-06-20 18:29:24 +0200

    All SEIR test passes 🤩

--- SEIR/compartments.py
+++ SEIR/compartments.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b23146d77c4bf82611db0ea6a3fb837b4d1f1f532c9b34b421071245ddd8f159
-size 26916
+oid sha256:3a5b3a5f1e251c362c0a6bb1f12016bbad77b2df12a78d78d23f1e6c0bfa801c
+size 26922
