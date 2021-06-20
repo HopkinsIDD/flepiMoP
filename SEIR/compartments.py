f867ff2c79c411c55f9ec8417624e96f2b0ba4cd
chadi <joseph.lemaitre@epfl.ch>
2021-06-20 17:46:48 +0200

    test_seir_spread and no_spread passes

--- SEIR/compartments.py
+++ SEIR/compartments.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abcbcd2b23a882cf6275d365693cb58343b0b93446c21a694be601619fce0a8f
-size 26905
+oid sha256:b23146d77c4bf82611db0ea6a3fb837b4d1f1f532c9b34b421071245ddd8f159
+size 26916
