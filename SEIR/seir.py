9ec9e012026ed3d3a31364aa8b703b76a62e2792
jkamins7 <jkaminsky@jhu.edu>
2021-07-01 07:53:31 -0400

    Got test_compartmental_specification working through Outcomes

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edd4592901c52b9102accd23190c3dfb2cd9c8152c96f5ec7ee03fb181992f0a
-size 12504
+oid sha256:7404cf81f835ef5842e70beb305349a07ced2005d04a2dc5ea951219f2373b1e
+size 12503
