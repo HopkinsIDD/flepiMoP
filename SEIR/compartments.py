696d5d6a24f890044bcf5dca210cd8c4884542a2
chadi <joseph.lemaitre@epfl.ch>
2021-07-05 15:51:18 +0200

    fix: math and herd immunity

--- SEIR/compartments.py
+++ SEIR/compartments.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7bdfddd6a9d78f8276c0ee7b5bffb5f7ec0b8d130915b2d748a0aadccf2a882e
+oid sha256:3adf8cc874cf800b024c148b047d5a89ed6132b67b0ba0ab193ec8f3a05622bf
 size 29081
