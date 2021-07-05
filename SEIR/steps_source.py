b157f18de2eb82c7100851c9e8cdcb34e2b4e37a
chadi <joseph.lemaitre@epfl.ch>
2021-07-05 15:17:02 +0200

    fix: seeding could make susceptible go negative

--- SEIR/steps_source.py
+++ SEIR/steps_source.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64e72a45f1cc0c1fb9588ecd5f47020e913fcad0ff40e7fffb8cf1e2b4b3a8b3
-size 10283
+oid sha256:3239bedc63a32276723c72d3f6ecf6d5e9433c38070dd24b7178601a4b65edb7
+size 10908
