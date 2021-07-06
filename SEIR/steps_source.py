0468b2f7197ba0bb1c3e2278bd54c32982beefef
chadi <joseph.lemaitre@epfl.ch>
2021-07-06 16:30:16 +0200

    save prevalence at the start of the day instead of at end, makes SEIR tests pass

--- SEIR/steps_source.py
+++ SEIR/steps_source.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3239bedc63a32276723c72d3f6ecf6d5e9433c38070dd24b7178601a4b65edb7
-size 10908
+oid sha256:b6cb6872409c0305eb02606d9de89449d0c8e16094d1449fd8d05899a2ef4b27
+size 10980
