23deb8c8c4ceb5627a8fd3c1212ae579f5da65a8
chadi <joseph.lemaitre@epfl.ch>
2021-07-06 17:11:42 +0200

    no space in pnames, + uses pnames in config

--- SEIR/compartments.py
+++ SEIR/compartments.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5a9a3b369eb176d91671928757911c547cbdc17efcdbe1b4dd31be9f20db382
-size 29698
+oid sha256:0a876a959cf3e39f5f46f53974866fe4af1352ffb3981a81a95782f952ee6456
+size 29690
