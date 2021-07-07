30e3887d6907cf1e49b3c50e2ffe5dead133dff2
chadi <joseph.lemaitre@epfl.ch>
2021-07-07 15:57:39 +0200

    npi do not support space in name for compatibility reasons with old config

--- SEIR/steps_source.py
+++ SEIR/steps_source.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b14c838ccd01f287f24c5321cfec6f0d147d16851ce1124fbc26ef3b528200b6
-size 11314
+oid sha256:eac321e3bb6ac97ff648fc16dfc670cd86d868dd6c3e613d7153d9a93b13e044
+size 11315
