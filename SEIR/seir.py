fc576a73ae2cd977302cda90aba7c4502ac43020
jkamins7 <jkaminsky@jhu.edu>
2021-07-15 12:39:32 -0400

    Fixed seeding things

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e06eaa677af303f346e670ce6ea4bbe598c33d9e573a34b098baf31119576a2
-size 12977
+oid sha256:f9a611608aa92e1ecd007022567a7e32cd37c4eb190c5c27e9b2bf19bfe8d788
+size 13023
