dff779566264e9fefa6a6b9fbcd7402da39c8db1
chadi <joseph.lemaitre@epfl.ch>
2021-08-08 01:21:25 +0200

    remove p_comp

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:36db7948e59b60f44767f79b00c60c7ab8c33048fae6fd647af8f33acfffde8a
-size 25777
+oid sha256:8778cfae9fb027594801bfb49af503c4724ba80fb30b749b21c56571c2c7ae1e
+size 25595
