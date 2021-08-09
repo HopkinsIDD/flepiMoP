ea1c67e7a86e47b5f972655d92a939587b9d2223
chadi <joseph.lemaitre@epfl.ch>
2021-08-09 19:16:13 +0200

    remove source from everyfile now that it's not properly defined

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a47c7b3f2169c318538164edcb6d1d5109d6fbd0bc3ff3373630ec195b621b6c
-size 26724
+oid sha256:6ae516e6854b79f611e082583b48930a79e053e11ac4e3d6796c38ca76b4ee6d
+size 26725
