86faa5edf6fafe6531231dcccc0e167d7d5f5a06
chadi <joseph.lemaitre@epfl.ch>
2021-08-09 17:23:51 +0200

    detailled config + new better way of doing the test

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30e9fb1c22a20de88ceaebe389b6df38955402fe4c31261b1a1c236fe1723b4d
-size 26708
+oid sha256:a47c7b3f2169c318538164edcb6d1d5109d6fbd0bc3ff3373630ec195b621b6c
+size 26724
