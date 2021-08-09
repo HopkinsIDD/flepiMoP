34518c7afca1294f881abcb3e17698c21850e217
chadi <joseph.lemaitre@epfl.ch>
2021-08-09 16:53:08 +0200

    outcomes work with filtering seir

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95f9a20446d3c667c09f4da3b15533d5a1fc7fd92aa818f6fbdf1d7d8b096730
-size 25597
+oid sha256:30e9fb1c22a20de88ceaebe389b6df38955402fe4c31261b1a1c236fe1723b4d
+size 26708
