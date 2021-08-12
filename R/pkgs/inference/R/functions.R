61624f37c64b7b4b473c4fa68fdf07cc893cff3f
jkamins7 <jkaminsky@jhu.edu>
2021-08-12 08:04:45 -0400

    Rounding in poisson inference

--- R/pkgs/inference/R/functions.R
+++ R/pkgs/inference/R/functions.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52794a5bc8b3becd5e35f03c8bea7da4ab8392e02e5d4770163fdc923f52f336
-size 19065
+oid sha256:6382ef1ed1b99d4599ee273cc097494f8da57b98ea606dfa85992020ac9de988
+size 19072
