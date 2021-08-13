b7db2f75095277cae3c6b500744565a676cfec6d
chadi <joseph.lemaitre@epfl.ch>
2021-08-14 00:41:47 +0200

    fix bug where sum could no be used as source in outcome

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61e401f712131a01bc119d2e13ec5b62efe7a9cf5f12d2707b4e547c95e9bbf8
-size 26728
+oid sha256:8864b9a00a50b4d4c8c61d5f966ac36059686e4b44481e1630c9a46e993f44d1
+size 27235
