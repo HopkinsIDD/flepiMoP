e1133e3551dc924daa883b600cf0d869072a31e9
jkamins7 <jkaminsky@jhu.edu>
2021-08-20 09:17:42 -0400

    Forgot to push this

--- SEIR/compartments.py
+++ SEIR/compartments.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:edbd1709af1151be1fbbd57e02491296aed47c0eeed0c6b2f8bcad0d6b0f1ab6
-size 30250
+oid sha256:1075f82924a3bf209cb10a9ee74fc24a1f02ad6c5972e2972957349a95da1178
+size 29506
