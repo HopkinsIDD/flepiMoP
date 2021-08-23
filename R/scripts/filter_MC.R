1efe383486e51f9e232adf523f12cb05a63d2b15
Alison Hill <34223923+alsnhll@users.noreply.github.com>
2021-08-23 10:58:12 -0400

    a few little fixes
    -hosp data plotting
    -plotting from multiple parallel slots

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a22169aff4f72cca185ca329996141a6e2826ca574017b289b8a0e3abc4ade9
-size 30848
+oid sha256:91beba610eb1b14687b15293cc1101cb5439130b770da7fcc5b426fdae40bfae
+size 30851
