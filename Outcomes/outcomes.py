7147b5753bbebe9712c4da5a864bcd3ab8e480a0
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2021-09-01 23:57:03 +0200

    existing test passes

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff33b4a521b771db2360734393045b7e329b28937f33f5a972b0a5d4a8a9cd20
+oid sha256:26ccb0fc775b25e0bde9fd0a42bdfa5d2b40c9615a52ba41716d11058a194e91
 size 29391
