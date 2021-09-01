575c975157791facb48301ccefe93156b7b58d7c
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2021-09-02 00:03:47 +0200

    test template, failing until config is specified

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26ccb0fc775b25e0bde9fd0a42bdfa5d2b40c9615a52ba41716d11058a194e91
-size 29391
+oid sha256:42402c816bdd88065a21989d1e6f557d793f1dd3135477478936a83cd6fdb6e8
+size 29418
