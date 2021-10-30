ca9ff5b4835fbdfc4d0da1948c0815f078b2e9d6
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2021-10-30 15:55:41 +0200

    fix for the script to delete the right s3 bucket

--- scripts/clean_s3.py
+++ scripts/clean_s3.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4bff3681ffbd060310ea7544aba6398d03f1f8720a42b82ef7a23492d23390b3
-size 1494
+oid sha256:63c890768c067d07adb586e11010de3e3c4331c907a2b5e3353dd5b22f31fce3
+size 1491
