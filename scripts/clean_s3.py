ef3f080ba38748ec7d9fea46e4198ddafb1f05ee
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2021-11-02 17:24:24 +0100

    last update of script

--- scripts/clean_s3.py
+++ scripts/clean_s3.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:454455c039c71a6175d59db2d56dddc0f7f5db883531d0b0528c6874c64da4b7
-size 1751
+oid sha256:867a1ba5adeb7a1844e04fb1a0e5b0b97fd716fa3054792e0787d66a81c940b7
+size 2603
