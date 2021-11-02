2354d039d0a8c71bfd7c32e226e5c98597401cf3
csmith701 <clairesmith6995@gmail.com>
2021-11-02 08:58:55 -0400

    Update get_ground_truch_from_source() to include option to pull hospital data

--- R/pkgs/covidcommon/R/DataUtils.R
+++ R/pkgs/covidcommon/R/DataUtils.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f19bde734557619efdbf5cd7f6a6b67a34af43acdaddd3b9cbeab5d86096c9ec
-size 44817
+oid sha256:582c1c8ef4c6e008ef935cf2834a6a968e07fe3bcfb2fe78206e3ef0c7109298
+size 45033
