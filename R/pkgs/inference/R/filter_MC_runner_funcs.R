e416b05a775d347fc57dfeee57f4887f1ec9e4a9
Alison Hill <34223923+alsnhll@users.noreply.github.com>
2021-11-08 23:17:35 -0500

    removed adaptive mcmc features and included example mcmc diagnostic report
    adaptive MCMC features are now in the mcmc_adapt branch.

--- R/pkgs/inference/R/filter_MC_runner_funcs.R
+++ R/pkgs/inference/R/filter_MC_runner_funcs.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c12e9466452050fe6b67643dd2d747b52cbc14e7918bfb409d088b4ad08d96a9
-size 29156
+oid sha256:eebbdd1ac1f828ba3ef2e5047058f5b881bc5fe559fc9dd4321c01b52baeac1e
+size 27748
--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccaf8da302fd127343d56e4e161a421682cba2773c0aaf30923f7f26ef4e3858
-size 31052
+oid sha256:20f486e01a6c53306b7fc0d97f2f48b9cfb7f3755c5ce89a755e7c3aacdff268
+size 29821
