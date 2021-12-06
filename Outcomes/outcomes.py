372526f4bb04b81c7758dba2486c6bb6a5da507c
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2021-12-06 19:03:30 +0100

    compute new outcomes

--- Outcomes/outcomes.py
+++ Outcomes/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9b13edb0d7a5e9b8bdb774c5b98549befaf0e5b8dc4619abc695c92fb7e9e98
-size 29361
+oid sha256:03aaf6e3476d6b0f8661f6abe76a4a185b4f48a913c8fc2841199f4caa456584
+size 29465
