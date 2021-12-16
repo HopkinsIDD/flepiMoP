63068e2ebbff155e8cb41e09eb6bdd50b10ec8bf
shauntruelove <shauntruelove@jhu.edu>
2021-12-16 11:00:16 -0500

    add environmental value for run id

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41c73447ee70f98cc5a50a4a3a56408423e150a0456d272f0eafce50e38bca17
-size 17395
+oid sha256:4a0550a858af032f3b8c5f9668bbd199804b4085b29b4a43eb8854e38d634c35
+size 17522
