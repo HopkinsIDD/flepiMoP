1ee784b36dc8214853c6e585f5d6ad4e90e37871
shauntruelove <shauntruelove@jhu.edu>
2021-12-22 00:26:57 -0500

    omicron seeding

--- R/scripts/create_seeding.R
+++ R/scripts/create_seeding.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3410fd8b34a2df507c89fe9c4c00c38c58a4fabb96faa517356e3dbe10f6ae6d
-size 12184
+oid sha256:53b9a25eb4e60b5bf2e91335b03414640083c050f073fe5d9c42bcd04465d257
+size 12241
