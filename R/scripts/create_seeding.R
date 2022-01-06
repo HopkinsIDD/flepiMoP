df8e0011bc442e93979f15c69d58635c6549bc2d
shauntruelove <shauntruelove@jhu.edu>
2022-01-05 22:46:00 -0500

    remove R11 temporary modifications

--- R/scripts/create_seeding.R
+++ R/scripts/create_seeding.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1aa1781c0d34ecadc71dca687c13237fc8494403d66414622ac160e2edd5a18e
-size 12362
+oid sha256:6cac8c08fd752b1fb4292e724bde8dc64e502a58726206cf037184723f24baea
+size 11736
