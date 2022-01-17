de6369458b00c0696e1f52aca528004302832f05
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-01-17 16:22:20 +0100

    fix for resume: param_name for NPIs is always taken from config

--- SEIR/NPI/ReduceIntervention.py
+++ SEIR/NPI/ReduceIntervention.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:beeb8c836788d6013a409387b32682c193ee3b61205f8ce58e2c111af3cba942
-size 9517
+oid sha256:ba810396f40275f5cebf1cd84af7d3e59032aff5cc88dff23968625adb97811d
+size 9734
