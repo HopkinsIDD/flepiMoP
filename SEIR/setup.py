db965f75f14bb49c887524a21901f59ad4570356
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-02-04 17:44:55 +0100

    automatically cast dt to float, something needed since a long time

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa425f34e20462c3528dcefb819db80519b4f37164fbb2f83f8f8364dd365e46
-size 11462
+oid sha256:0c7f65d863c619f05d0aca58e78aa679a643005cb170e8386904560b0c5e5e17
+size 11469
