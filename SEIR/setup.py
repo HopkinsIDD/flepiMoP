52890da87630036b706376943976a76f4ab95ef4
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-02-08 11:55:51 +0100

    renamed compartments config as seir config, because that what it is

--- SEIR/setup.py
+++ SEIR/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c7f65d863c619f05d0aca58e78aa679a643005cb170e8386904560b0c5e5e17
-size 11469
+oid sha256:68a24e6bac4e63cff6e9fb36452f3d3cbf147b3ca845a06a7e0fb7d93ef0cca5
+size 11429
--- minimal_interface.py
+++ minimal_interface.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba5954b1d3b2489c457c477239c3d1f1cf265b06c301eb869df33c0fc40a9730
-size 6722
+oid sha256:e47f083c10ce2ab859dc1e44f5ce74a5347e3255931073a455ee0545a183366e
+size 6714
