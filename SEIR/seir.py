39813ba83e0a066b3cafb1102b65882ebde837e0
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-02-08 11:53:21 +0100

    cleanup benchmark helpers

--- SEIR/seir.py
+++ SEIR/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95ed0f21b7af053e6aea7feaf09631851f60e950164a089fcec03fc2eab8ea19
-size 15115
+oid sha256:b452ff6500d15eb365516dea454c38ec9e7fff9e6513f0513c669d0e74244729
+size 15572
