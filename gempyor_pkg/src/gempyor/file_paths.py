5971d6b56093a74ba7bcd0efdda05d830f6c0e9d
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-02-16 10:48:35 +0100

    type is a built-in python construct, changing to ftype

--- gempyor_pkg/src/gempyor/file_paths.py
+++ gempyor_pkg/src/gempyor/file_paths.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:456441ef7d9a325b05244022507e78810e5fcd625ae63d209991bfd656367f09
-size 936
+oid sha256:ede49a8d1b5c96e06ef923f38331f45644d0799fc0a06df9723d45e619a00ae3
+size 944
