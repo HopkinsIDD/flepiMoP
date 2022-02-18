fe3f147b78f9b5f43893e7508f55090cc599c446
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-02-18 17:50:05 +0100

    fix bug save npi, now stuck at assertion error for later

--- gempyor_pkg/src/gempyor/outcomes.py
+++ gempyor_pkg/src/gempyor/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e28d43571171040dbb5cd0c04893fa017f37a9904be31d0303b82b15e2f88cc
+oid sha256:e16cac6a7bdac31bd70ebd2279580d269e1f9a353e4a3b944351a0f5d32f89c6
 size 25828
