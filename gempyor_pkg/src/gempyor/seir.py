5683cd89c2955c25a99c3d64a4e5e2cffc862afb
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-02-21 00:28:40 +0100

    tests in outcome passes, feature parity

--- gempyor_pkg/src/gempyor/seir.py
+++ gempyor_pkg/src/gempyor/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:12de5fa886a9988e1a8cd17b8f1d4932810fae1b737aabb0a37d5346a058f2d0
-size 11566
+oid sha256:57273644eb1b27c7985aa436570104f59da6a4bc5e7471ce94c7c33f6c508a22
+size 11567
--- gempyor_pkg/tests/seir/test_new_seir.py
+++ gempyor_pkg/tests/seir/test_new_seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7056453ed9629ee8bec717e79cc9cee664c73647dfaeea76c6ba8abac4fd45e9
-size 2161
+oid sha256:f79afdc911ebdb6e5b13932ca93d24a0a3f92a271b3aa3b64627dd335e5f803d
+size 2162
