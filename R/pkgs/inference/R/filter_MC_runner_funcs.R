b74c7d71a4d60dfef11582a6eef0401c4d79e0b9
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-02-21 10:51:28 +0100

    unified interface for loadID and normal runs

--- R/pkgs/inference/R/filter_MC_runner_funcs.R
+++ R/pkgs/inference/R/filter_MC_runner_funcs.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6d567c4a02c25911c5e409649eb72d77219f938705ca69c89f50ac261ca42c1
-size 19272
+oid sha256:fe99f4d68735d8e0aae6f6b1c2f392b69d4bac5820d572f3facf87ec374c0d2d
+size 19279
--- gempyor_pkg/src/gempyor/steps_rk4.py
+++ gempyor_pkg/src/gempyor/steps_rk4.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffb7e632a8c173e1c3de655c5e5729ac0db00c1e1c340ec08f84d4ba9fc7a223
-size 10122
+oid sha256:7771c82fd74737e28c186cb9e3ebd344c2d3b1994a22ede10925fd7efd4531e2
+size 9628
