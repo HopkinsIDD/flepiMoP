bfb41beef6328d62da43c851b62576d0671c331a
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-02-21 16:00:41 +0100

    modified the batch script

--- gempyor_pkg/src/gempyor/interface.py
+++ gempyor_pkg/src/gempyor/interface.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:569891a041fb0ccc88946cf8a9b95add9e6dee73112dc304c0cbc4d09e799af5
-size 11187
+oid sha256:28947d996f23a8378047db1b698299b5eba0ace2e44b17ac314a936281773b16
+size 11323
