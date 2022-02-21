d6ce4d04f58f9a92f94d7beb5c2020eda15f4147
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-02-21 16:00:41 +0100

    modified the batch script

--- batch/inference_runner.sh
+++ batch/inference_runner.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:464f6c6a92022ccd650975a464676dd4ddada38f52f59077b1634e7720a62622
-size 6434
+oid sha256:2a1794949a9f4e7ab39500ae9cfd128504df3bdc2c78bd27c63a815ae6019da2
+size 6231
