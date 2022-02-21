70e68425e0b4257fad0975a818c3f7edb9a8e224
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-02-21 14:25:07 +0100

    // running and pre-running of categories

--- gempyor_pkg/src/gempyor/NPI/Stacked.py
+++ gempyor_pkg/src/gempyor/NPI/Stacked.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10e7db82439a30853bc9d146432f3f81b5dd3d2964026d0c67ca02fefc427112
-size 5598
+oid sha256:bbb2d2513fcd58fa7a7fccbade4d433fbd8fa09d55a1fd6dce001325655d0e3e
+size 5459
--- gempyor_pkg/src/gempyor/outcomes.py
+++ gempyor_pkg/src/gempyor/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ecdea11bfccdfdcb70f27232d3a453805874eb20122afe8e8fafa027b680da6e
-size 26635
+oid sha256:111cf5be7868a7e1571c815b4e7da324ad303aa46cd1cf458313d72f49707b4b
+size 26658
