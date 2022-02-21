467b9c83219f47d475581a9eae862951d5521fc9
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-02-21 13:31:16 +0100

    pnames was not used because not passed here git add src/gempyor/NPI/base.py,s that's the second bugfix involving overlap operations

--- gempyor_pkg/src/gempyor/NPI/base.py
+++ gempyor_pkg/src/gempyor/NPI/base.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64d2f2ae9f64196b584813b619aacd0537e5a6e8ce1bb2d3fc41a58d9ee54a3f
-size 1031
+oid sha256:504d686021db4ee417fdddee019c3c7e9f00a53b40a08ef9d20a3a3c09bc4230
+size 1101
