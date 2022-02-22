fecd30a41ff82ae0af5b92df861d25f2d31f5cf2
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-02-22 15:55:01 +0100

    allow outcomes from prevalence

--- gempyor_pkg/src/gempyor/NPI/base.py
+++ gempyor_pkg/src/gempyor/NPI/base.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:504d686021db4ee417fdddee019c3c7e9f00a53b40a08ef9d20a3a3c09bc4230
-size 1101
+oid sha256:4127a1680b8611f63189f9cceb33f886832f6b2cea9672696ed470dd242bd941
+size 1102
--- gempyor_pkg/src/gempyor/outcomes.py
+++ gempyor_pkg/src/gempyor/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:111cf5be7868a7e1571c815b4e7da324ad303aa46cd1cf458313d72f49707b4b
-size 26658
+oid sha256:4952b89a6f0462ff35f0427b9745916d5d46ba7549c9727c0d5bb1b451e7d027
+size 27466
