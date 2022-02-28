a97243128505c71738e14aea1ed3233e66ebe63d
jkamins7 <jkaminsky@jhu.edu>
2022-02-28 09:32:59 -0500

    Revert "local install update"
    This reverts commit 7216553b3da75eb887a1ea4b001aac49c425c4d7.

--- local_install.R
+++ local_install.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b02600845f1e687cf3e5275748d821f01b0c03965e865a81b1f06b953034df0
-size 377
+oid sha256:1a7d10b2b0b77adc54fcfb7d6fed552a19605636d52184d77ad3e0684e6409d9
+size 326
