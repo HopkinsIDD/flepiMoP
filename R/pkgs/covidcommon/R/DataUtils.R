122af1997e45e86da540aee8ffaf6d47012b4e19
jkamins7 <jkaminsky@jhu.edu>
2022-02-28 09:33:13 -0500

    Revert "update covidcommon"
    This reverts commit 86c4dfb35c85bb679b1082825407ec5c2a51f77f.

--- R/pkgs/covidcommon/R/DataUtils.R
+++ R/pkgs/covidcommon/R/DataUtils.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a0245394f21be17c8d6a2d95d90c4332a33f678fbf431745ce18686c9b977ca
-size 66334
+oid sha256:d6123ca40977f067b8cb5453e7b529d1ec13a9625b1de9f020ca6a79e54e547c
+size 66096
