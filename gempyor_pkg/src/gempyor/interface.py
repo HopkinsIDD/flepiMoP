ea22684f39d8188a914277b055988ff25f9a1e90
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-03-03 17:43:17 +0100

    ability of plotting graphs from compartment

--- gempyor_pkg/src/gempyor/interface.py
+++ gempyor_pkg/src/gempyor/interface.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8c6f57ea71b47ffc40166684bec4b826d5de9cd0035f392115e7883a13e65609
-size 11734
+oid sha256:d95a7ac2e8b2f8b3ea3a531707b5ca70eb44894078d90b9a0147ddb9b949592f
+size 11978
