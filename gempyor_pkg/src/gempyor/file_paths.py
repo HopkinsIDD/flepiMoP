4f466ab1d4bd8d9df703ca1bd17fb02cde7f73b6
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-03-03 18:26:19 +0100

    simplifed fn pipeline

--- gempyor_pkg/src/gempyor/file_paths.py
+++ gempyor_pkg/src/gempyor/file_paths.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72c7cc975ed096231369f0064ad9ebfdc33bbf485c41886b4398af4399bef12d
-size 946
+oid sha256:becd2d24b230ad38d6da0c1a8e3c38d3f85db69bc740f084ed3fb61388c1bb61
+size 970
