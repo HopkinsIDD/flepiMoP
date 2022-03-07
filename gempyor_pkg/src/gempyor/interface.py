b6a61b61288cf1c672dcad5294ee6373a085328f
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-03-07 21:42:32 +0100

    styling

--- gempyor_pkg/src/gempyor/interface.py
+++ gempyor_pkg/src/gempyor/interface.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:854347cb4d8da62d617ec7ba865dbaf6e58891cd958382fb21e5e2025b909aaa
-size 14776
+oid sha256:70b0bb8d27fc5004801755cb6c2440edaaac560a8286c0036eb881e8976a5d3e
+size 14768
