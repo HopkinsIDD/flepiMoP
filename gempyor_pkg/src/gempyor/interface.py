4f4b4f6d90be346d5a49b43a6afb81c2045818db
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-03-08 12:10:26 +0100

    profiling

--- gempyor_pkg/src/gempyor/interface.py
+++ gempyor_pkg/src/gempyor/interface.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70b0bb8d27fc5004801755cb6c2440edaaac560a8286c0036eb881e8976a5d3e
-size 14768
+oid sha256:3120c94db5c30137ebdbb10e031d3a0facc3c651043f0bd8acf48e8dcf5301e7
+size 14777
