1fad594328373fc69044da70184da0aac4baa892
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-03-08 12:11:49 +0100

    unused variables

--- gempyor_pkg/src/gempyor/interface.py
+++ gempyor_pkg/src/gempyor/interface.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3120c94db5c30137ebdbb10e031d3a0facc3c651043f0bd8acf48e8dcf5301e7
-size 14777
+oid sha256:1f0be0236a9f0cb285e9829130eea4b468a74e12da983a241fe46286d5d070a9
+size 14778
