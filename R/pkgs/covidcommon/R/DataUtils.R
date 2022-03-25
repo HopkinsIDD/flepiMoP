306a1bfb3a55b60fc747622650282a2677b5d4f6
jkamins7 <jkaminsky@jhu.edu>
2022-03-25 10:54:34 -0400

    Fixing rows with lots of NAs in groundtruth by not being stupid about it

--- R/pkgs/covidcommon/R/DataUtils.R
+++ R/pkgs/covidcommon/R/DataUtils.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e2895184ca67c86b57553cc24408065172d74728d1bc6ee125a2af4a2db92bf
-size 55236
+oid sha256:8600c613adf342feea0584571364ca9602f512f31cf82bb6124cce0f68813e9b
+size 54894
