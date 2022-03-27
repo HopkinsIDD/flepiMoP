30be2aaf05b439d96c857d03c8605d4fb534e330
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-03-28 00:34:37 +0200

    aptempt to prevent values bellow zero

--- gempyor_pkg/src/gempyor/steps_rk4.py
+++ gempyor_pkg/src/gempyor/steps_rk4.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e1293ba3b15937094f58dde16d407aafb0ddc895731f6ef384a5646a2dfd2d5
-size 11941
+oid sha256:873941fb3925880f45a2732b4f2d9fbbd829e2fc7bd9d12cebd148acf13a8181
+size 12340
