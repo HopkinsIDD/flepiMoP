9f121c904c2d9522df2ec65ea3ddefcbd210a143
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-03-28 00:34:01 +0200

    format of NPI checkes

--- gempyor_pkg/src/gempyor/NPI/ReduceIntervention.py
+++ gempyor_pkg/src/gempyor/NPI/ReduceIntervention.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83d8478936770c8f72fab2cdfc76a01214195dba6881f07342df4937a50c74ff
-size 10508
+oid sha256:a8c09e3403b2c8f3057ae4ebeb5cd602c92051fc20642f54b419ce3d1a57aa50
+size 10509
--- gempyor_pkg/src/gempyor/NPI/Stacked.py
+++ gempyor_pkg/src/gempyor/NPI/Stacked.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f26c3e3a734c77a2d21e392c5fb08b55985b5000dc60e4ff63a481c4dd407e8
-size 6037
+oid sha256:a94678dd7a078614a781176b8e26098e090be187fa533d50a462ce38557a0cdb
+size 6067
