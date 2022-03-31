1fa1cef8929921c373027b0b322ad0187998137f
jkamins7 <jkaminsky@jhu.edu>
2022-03-31 12:16:14 -0400

    Added file pruning to filter_MC

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d3e148cf13cd47e8e173ba6042543728f62f83526cfa08fc95db8271b7d2fb3
-size 21630
+oid sha256:2ad6503feb3fa1946ffc83e6230da3abac9c03057fc4465caabdfdccf01e0195
+size 21945
