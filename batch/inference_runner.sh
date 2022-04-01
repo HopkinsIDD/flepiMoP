8ae31bf0d100ffa4bd2339e2bb22c85364893531
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-04-01 18:05:37 +0200

    make so we keep some runs even in case of failure

--- batch/inference_runner.sh
+++ batch/inference_runner.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5f870b7524b92b8ddaea5f47eee42ca2343069b79bb6bde485daba51521a4ff
-size 6302
+oid sha256:2b244520cdcab81ebd209d8a8ed7b2209c3a70fca0a32082d1a4191fa8e41c6c
+size 6303
