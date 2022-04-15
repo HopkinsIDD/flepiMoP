fd73db48c6df8f9d5d98314dff27f564f5800846
jkamins7 <jkaminsky@jhu.edu>
2022-04-15 07:04:02 -0400

    Updated submission script

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:768744ec9ffd52e2dd69f8f2bcf2d36c282a7575e09bf164f0a788d3ef27eef6
-size 20237
+oid sha256:8560799acb602b1d378209b55f38cc44034f2949d8db181bb9e2170b7575663e
+size 20238
