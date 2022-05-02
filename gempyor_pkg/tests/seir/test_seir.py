566773e22942b040bb9dec401f23c15b8ef3e846
jkamins7 <jkaminsky@jhu.edu>
2022-05-02 09:09:14 -0400

    Upated config to include seir config

--- gempyor_pkg/tests/seir/test_seir.py
+++ gempyor_pkg/tests/seir/test_seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:259a937609c9f5b5a0fa67852dd2a135db6f6741f8a463248c975ef259b9f9b6
-size 27599
+oid sha256:39ba96f03f9e927512f9f714179faa27eb37fdad1d0e0c6d7151b197113b19ae
+size 27635
