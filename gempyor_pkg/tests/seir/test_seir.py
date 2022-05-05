f40ad46c7973f14a34414d9840e7edc1119015b7
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-05-05 11:23:59 +0200

    fix legacy integration

--- gempyor_pkg/tests/seir/test_seir.py
+++ gempyor_pkg/tests/seir/test_seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92dd036530db80b5843d848ef8618c3a48ae073648dff9749dc8e66016979480
-size 27690
+oid sha256:fd4eb35ebba58ae7353938cf182997ea2a85d772a523bb7e54f2fabc6d99f4fa
+size 27682
