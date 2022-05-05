7b4d8393ed68ba10913e635c3160fcdbecdd8a5b
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-05-05 10:33:38 +0200

    setup default flag is stoch_traj_flag=False for internal consistency

--- gempyor_pkg/src/gempyor/setup.py
+++ gempyor_pkg/src/gempyor/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3bd6f709fe2c30b0dfa7d86164486c64710371d17b97efb5939a0b251e4f643
-size 12959
+oid sha256:8256f2dd1ff59e4521da597fcbdbb9469668b4d348eb0be9531bc042b420d3ea
+size 12960
