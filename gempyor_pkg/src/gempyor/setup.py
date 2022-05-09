e91cf46a3eb33cfdaf862d6a766dc0501ca80648
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-05-09 19:28:15 +0200

    repaired gempyor tests

--- gempyor_pkg/src/gempyor/setup.py
+++ gempyor_pkg/src/gempyor/setup.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01b1d90698467721bf22f5c5c91e9b6d39764a2f0f187d5b4b8e21b21acdda85
-size 12976
+oid sha256:867e9e780f2dbce573899e9432112668a36888b7ac336d13ab04a769f0acce01
+size 12999
--- gempyor_pkg/tests/seir/test_seir.py
+++ gempyor_pkg/tests/seir/test_seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fd4eb35ebba58ae7353938cf182997ea2a85d772a523bb7e54f2fabc6d99f4fa
-size 27682
+oid sha256:12c95a5478f5e6d565536f7f76a7f7cb884b9d802770447866b3a50e731599b1
+size 27757
