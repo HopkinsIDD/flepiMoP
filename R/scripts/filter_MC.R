607bf80a1b2898a7822c01d6b95628e9e220fd8b
jkamins7 <jkaminsky@jhu.edu>
2022-05-13 10:45:07 -0400

    Setting stochastic to FALSE by default

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1d55e67ba8af26d8a510551960f029bfad9e538ce71de9db0b58c918718de06
-size 29197
+oid sha256:09ad985f4242a2fb3884a71467f611c126395a3cf51cb760c61b244ab0980ad2
+size 29198
