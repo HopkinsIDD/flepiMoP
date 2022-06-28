eb4bd41b67d719cbbedce026da1bc3ef9c31b59e
jkamins7 <jkaminsky@jhu.edu>
2022-06-28 08:21:09 -0400

    Used the error handler to handle errors instead of something jank

--- batch/inference_runner.sh
+++ batch/inference_runner.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ca78a895ee032eb94489f7373a5cbaad29ea05008d4351fbc564f31b45a01fc
-size 6375
+oid sha256:56e83ec0a51c21ccf9e31b102b7fab29737cb637bad8608fdff237b8e299cff2
+size 6410
