26153c894b3944f5a6752e2ebc1ff098a6a2f5aa
jkamins7 <jkaminsky@jhu.edu>
2022-07-06 11:49:16 -0400

    Logging for seeding discard

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ea6e71a50e452daf56d6a07233bde7a0adc3d4d7ba9dda15fccb5d74603c13c
-size 20238
+oid sha256:8564647b73b6a3b1580845191973bb24558da25f92874fac209c0572f786fcc9
+size 20337
