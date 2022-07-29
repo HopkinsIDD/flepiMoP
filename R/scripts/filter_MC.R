1883339f41f1b8a8a177d609f3f54abac913b6af
jkamins7 <jkaminsky@jhu.edu>
2022-07-29 14:49:47 -0400

    Updating full_filter to have good defaults for stochastic

--- R/scripts/filter_MC.R
+++ R/scripts/filter_MC.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c32c047279b31fa9a635f5476b7898cb87060b3512f87114e055fda8d9cf13b2
-size 30332
+oid sha256:1a5c3d5bfca5d8a5395b75a84ba668bffccf32a26826a8eb8a3832954edcb3f8
+size 30333
