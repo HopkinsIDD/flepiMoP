4b47ffc2c2280e0be7f056c8eecc2903cf2abe18
Shaun Truelove <satruelove@gmail.com>
2022-09-06 09:37:19 -0400

    add "historical" type to gt_source options for pulling manual flu data file

--- R/scripts/create_seeding.R
+++ R/scripts/create_seeding.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57475bde86ea55e28e029629dd0014d052e504e39afd06dacd41fe7dcdeaa99e
-size 12131
+oid sha256:e6775e90534715047c12cd33977babb40a774d4e315ae86524114a20993c1cb8
+size 13443
