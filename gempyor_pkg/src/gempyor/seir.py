e05baf7bb176b4ce74a670a05d049d544636de31
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-09-12 17:43:20 -0400

    Test passes, forgot to push these changes

--- gempyor_pkg/src/gempyor/seir.py
+++ gempyor_pkg/src/gempyor/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f65e7b6fdc2d471d8ac3181134b363cb5be7313fe8397b5beca34df4e693575
-size 12286
+oid sha256:51f588ffbb4d4062c6bdfe493376c58bc08abc217f73b111ebb9653571d97da0
+size 12282
