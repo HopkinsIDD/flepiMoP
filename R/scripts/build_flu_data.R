5c4396471b39ea8bf2f0c402fda792c120926166
Shaun Truelove <satruelove@gmail.com>
2022-10-21 12:35:02 -0400

    fix seeding before start date issue

--- R/scripts/build_flu_data.R
+++ R/scripts/build_flu_data.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e81306918ee9a50bc5d744b1a822dc6e6c530fc4b38f91663bc36fdf76edcef
-size 6962
+oid sha256:349683596f4fe7ae13001931183f22a7b0695a846412be0a0d947be7c5ca98e2
+size 7241
