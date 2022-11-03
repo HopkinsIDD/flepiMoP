012701324665ae4a2ee2db2c8d3f35477b5c521a
csmith701 <clairesmith6995@gmail.com>
2022-11-03 14:14:43 -0400

    Fix date column name

--- R/scripts/build_covid_data.R
+++ R/scripts/build_covid_data.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b19834c4d998381a745788fd72ae27641e2d32a260a416deb27442f3d79343e2
-size 4064
+oid sha256:3ede9c3e64aecd81336a58872fccc8f4286046c35c9dcb8b75090130a6d5e0ae
+size 4094
