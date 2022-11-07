2650ede39c68e78d9aecbf2e4a304c19fba6f73a
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-11-07 09:46:10 +0100

    this yield an error if the two ts are not the same lenght hence occulting the important part of the error message

--- gempyor_pkg/src/gempyor/parameters.py
+++ gempyor_pkg/src/gempyor/parameters.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a49f0083ebf369b6bbba5a081de9200495866e740c94acdd0c36fcdb10408b4
-size 13198
+oid sha256:b03dcf20f7e6944ce5296cf3296f470140b5f6db1f519f47e10afa0619fd05c2
+size 13070
