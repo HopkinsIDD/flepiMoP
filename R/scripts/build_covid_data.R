fdb0ccd31426ba2ce8f203c5c781e53a8fa89e5d
Shaun Truelove <satruelove@gmail.com>
2022-11-10 09:21:43 -0500

    fix covid build script

--- R/scripts/build_covid_data.R
+++ R/scripts/build_covid_data.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b19834c4d998381a745788fd72ae27641e2d32a260a416deb27442f3d79343e2
-size 4064
+oid sha256:ba17e9b5fe50ece736ccea3da881c1a98b1d8a1c36eb5dceb5f94207890b17d3
+size 4279
