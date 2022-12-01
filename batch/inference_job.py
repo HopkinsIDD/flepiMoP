889190bd0e2939da88536a31f9a285bc56c219fe
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-12-01 22:56:48 +0100

    adding the option to specify the CSP directory

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6add3a28b7d76ae188d0dc986951ce099e98386408c3ac5b734182eae9c74db8
-size 20470
+oid sha256:94e2a1c13c0e0fe736adaa9c26263d90be1944dd994ed4e3bd4af25fede5941a
+size 20932
