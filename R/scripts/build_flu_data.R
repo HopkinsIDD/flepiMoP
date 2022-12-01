b4b1428b13a4b9aaaca17f4180cd42fce037ae46
saraloo <45245630+saraloo@users.noreply.github.com>
2022-12-01 11:48:42 -0500

    variant data edits

--- R/scripts/build_flu_data.R
+++ R/scripts/build_flu_data.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c658722c577143e54c93f5b502b5dc3bb4d21f587dacf2827e4e541d7c208588
-size 7488
+oid sha256:524ab216fee687abd7c082537491f5f5db1fa825d60c05431b337671c9b7abbb
+size 7691
