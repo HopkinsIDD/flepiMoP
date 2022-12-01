76c22284f337f536c99fd4f0f6f56ac25c9de7a8
Shaun Truelove <satruelove@gmail.com>
2022-12-01 14:08:47 -0500

    fixed variant

--- R/scripts/build_flu_data.R
+++ R/scripts/build_flu_data.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:392de37c6585830444413db51ec31867709d578eb2802e8ace488bdfab2936a3
-size 7881
+oid sha256:6c387346439b1a46cc79812c13f40af97014036848a6c200bec5fcf6a2964bbc
+size 7883
