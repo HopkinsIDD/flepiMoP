63452b0946addc1c6f71063863ad93b97c7c5926
Shaun Truelove <satruelove@gmail.com>
2022-12-02 13:21:12 -0500

    local_install update

--- local_install.R
+++ local_install.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:27a8a1d0ebfd45341c93d8aa1087a7ac640e13863c5820597debdec581a6b4c3
-size 1242
+oid sha256:e8277c78cef44ca2eb3609885a81d3b6d5ce2b93c05a8a2c68575a47bf513529
+size 1243
