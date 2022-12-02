e1f8be2b5dfa6570485780fb29601b201225ed4c
Shaun Truelove <satruelove@gmail.com>
2022-12-02 14:50:20 -0500

    remove cdcfluview package and variant pull

--- R/scripts/build_flu_data.R
+++ R/scripts/build_flu_data.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c387346439b1a46cc79812c13f40af97014036848a6c200bec5fcf6a2964bbc
-size 7883
+oid sha256:efd3653d8c6aa33b690b33f5549b0a2e1d5961df5749b68422d12417f7a4a09c
+size 8079
