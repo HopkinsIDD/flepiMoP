829c9e1e336c9017c1d37394dcfeb9bea135d5ca
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-12-09 00:27:20 +0100

    unifying the aws & slurm batch process

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:da6129f3656507e103edcd43259482e002102ad69a569da17618bded7d58c089
-size 23570
+oid sha256:b550dafa252d298a4632d5b20b50985e25d6be59ef0f92dd3873045103d1eb49
+size 25011
--- batch/postprocess_runner.sh
+++ batch/postprocess_runner.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4df34d68d0073f2278e2036042a7792f10a8be2a5f1ac7e214f0e3eb7ce8052
-size 4370
+oid sha256:9a22e1af003c0f7468fcf854a657afba67bce05ff99cdfda51c4b3a94e835f98
+size 4355
