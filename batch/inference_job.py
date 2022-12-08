aff6c652246c555ac2fad59b56d2d900e7c4cd5f
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-12-08 16:16:44 +0100

    bugfix + working on slurm

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24363c3a2865877a635cf417a6688471ad0847e6b7fed108dd4070d3b6f0b194
-size 21017
+oid sha256:d321fab0c747bcbc7aee71c391c1315c3687e6a54dfd83c07738d8b434e79ae1
+size 21019
