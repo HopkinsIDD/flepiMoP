dd32340c989fb47aa60af0cc6da20f62e287124b
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-12-09 15:52:58 +0100

    single script submission for both slurm & batch, with some bells (need the whistles later)

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b550dafa252d298a4632d5b20b50985e25d6be59ef0f92dd3873045103d1eb49
-size 25011
+oid sha256:9fe7c71c9e29ea81f4a304aa22d8e4eb35f9cc8685cd231f577ed1bf300286ac
+size 26489
