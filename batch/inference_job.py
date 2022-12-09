6461a0d8157bc83f6076cbd0878c5d8585442c7b
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-12-09 16:56:28 +0100

    datapath for filtering

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4bc9edeb903480a53be05490f3289138c75c0fc851481ac318af13323dfa406
-size 26532
+oid sha256:392cc7dc9e62ac3d8f59526dd21b9c3fcae5366b474c81fafedb0aaeba8ece7d
+size 26633
