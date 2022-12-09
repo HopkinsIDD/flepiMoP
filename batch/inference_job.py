c63cb160bb8ea6e37b97ddc9d89a1ea54f050b16
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-12-09 16:51:00 +0100

    datapath for geoid

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fe7c71c9e29ea81f4a304aa22d8e4eb35f9cc8685cd231f577ed1bf300286ac
-size 26489
+oid sha256:a4bc9edeb903480a53be05490f3289138c75c0fc851481ac318af13323dfa406
+size 26532
