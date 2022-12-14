8fd79ad9ca62151adde04616e5f303cd6270a0f8
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-12-14 17:48:46 +0100

    branches & cleanup

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4fa4ed838f5a0505b692626e7cf2a9b87b3cf44e2f1c93b766f717c63e5c5dbf
-size 27951
+oid sha256:313ad4be37c634d40e1fec28ffcf6770a845936f20bd76b259eb00551bafa5f9
+size 29658
--- batch/inference_runner.sh
+++ batch/inference_runner.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45bda22a96ffd1b3edacd0a0f43a0f86c18983f80057ebbee1120eec2bde09f5
-size 7348
+oid sha256:8af2ae23dc01fe3dfc901ea16243faa500260a041a7e78f01ed6786aea3b1772
+size 7368
