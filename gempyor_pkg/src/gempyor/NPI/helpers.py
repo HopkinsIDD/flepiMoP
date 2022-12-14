a80e3a0504733a8bc692bac5f5e8dff1c1ba90c4
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2022-12-14 18:29:53 +0100

    black --line-length 120 . --exclude renv*

--- gempyor_pkg/src/gempyor/NPI/helpers.py
+++ gempyor_pkg/src/gempyor/NPI/helpers.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f55db7fca38e1bc95f61d336e71bc8efba27f674bb95e9ea6d451d556eb0911
-size 719
+oid sha256:d9aa9c76cb362bed1058b3635dadcff48c9afa5890d0693c80820c0143ebb1e6
+size 695
