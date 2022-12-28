b2a472bb017b48f081ed446f1036a40b25783a83
Shaun Truelove <satruelove@gmail.com>
2022-12-27 21:50:05 -0500

    package update

--- R/pkgs/covidcommon/R/DataUtils.R
+++ R/pkgs/covidcommon/R/DataUtils.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b85cdcf6279a2602768153683c3cfbc4968aac66bbec72d60d2e6644ff2c2b2
-size 55208
+oid sha256:4c948857b4e076c5be93f5feba0951bacaf19ec201b8394d4dfe88d95b3a4f03
+size 56430
