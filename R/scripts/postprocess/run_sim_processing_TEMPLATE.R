e5b16a3f7c3b394da29b9cfee46a50a3663960cc
Shaun Truelove <satruelove@gmail.com>
2023-01-19 08:58:41 -0500

    remove "drop"

--- R/scripts/postprocess/run_sim_processing_TEMPLATE.R
+++ R/scripts/postprocess/run_sim_processing_TEMPLATE.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87eedbdde0016c84cd93d095455fcc6c693b81ced223d6cadb529d0c1993f18d
-size 24800
+oid sha256:f0aad7fac05bd69c8f126170e2710e9758210d6e20bb22d6adb9afe76609147f
+size 24444
