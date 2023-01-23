67da8d0b8c61fdbc151ca6b5001b1385fc0a83ce
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2023-01-23 23:21:30 +0100

    black

--- gempyor_pkg/src/gempyor/seir.py
+++ gempyor_pkg/src/gempyor/seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f69c8c634b52cd7b626b3707a1a6e8f70c1a2b4d46fb5ce6b18038d70099663
-size 11962
+oid sha256:b3f8cfac5cc562ed11cb2afa88b29df6f65a68291e2b0733095419f1c304c86a
+size 11963
--- gempyor_pkg/src/gempyor/steps_rk4.py
+++ gempyor_pkg/src/gempyor/steps_rk4.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:305ac40b60b1760a73e06a5a6893ab79fa43786db2ef8cdb436df6d9c8ca86e7
-size 15498
+oid sha256:c483498c4d214849bdd248fcb0e776074501a208dfea419cf0700b6fc292dd9e
+size 15538
