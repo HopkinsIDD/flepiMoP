de0555ae35d23db57b81b0cc847a6583bb08d88f
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2023-01-24 00:14:54 +0100

    fixes all over the place

--- batch/AWS_inference_runner.sh
+++ batch/AWS_inference_runner.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:289f0e150e54ae1a0873527cbada62d61fe1768e3454d9a205be7c8f0c963f78
-size 7392
+oid sha256:d53c2aa667d5c041daca4b1cb97d384d63d5ec42d778768a058d7aedd40b34e2
+size 7404
