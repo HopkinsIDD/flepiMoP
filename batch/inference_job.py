8c2cfc6bd2862ec47e860f948f1bf36277e0b1bf
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2023-01-23 19:49:49 +0100

    fix

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4331e5fbefe724c4e8cd87b89c9d6184dbac8ad60adf1c83f428a23c820f4ce6
-size 29731
+oid sha256:af1f4248ea6d35e069bcaf5f8fffe694079cbf5c232264d3ecc99828b8b08207
+size 29732
