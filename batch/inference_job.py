ccb85a8411b6eff99a33288a17011bfc6de692ce
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2023-01-23 02:21:06 +0100

    every worker was writing the same file at the same time

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db262278f40147a65e3d6536204591f923509f8cf6ab908bbb506278f4a8f565
-size 29685
+oid sha256:579df70dfee383937fa633c5bbbe29a9aa5fe51c32a8f1fa02057b2bd3ae4574
+size 29686
