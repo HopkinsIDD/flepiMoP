24a3f0d83876d848bc92eb58294f5d2c845935cd
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2023-01-24 00:14:54 +0100

    fixes all over the place

--- batch/AWS_postprocess_runner.sh
+++ batch/AWS_postprocess_runner.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa1e81e747aefd9950bae6595f87ae73bc4c41a58fa216e9364fdd81565419c1
-size 5370
+oid sha256:7ab66faa1c408e8f1f3725292cfc212d9b5524ddf44d177a7743968b54add92d
+size 5382
