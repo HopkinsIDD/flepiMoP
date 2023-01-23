44c3447e2246920cbf4fc0ec7212b5b6ecddf939
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2023-01-23 23:19:56 +0100

    clarity in filenames

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:af1f4248ea6d35e069bcaf5f8fffe694079cbf5c232264d3ecc99828b8b08207
-size 29732
+oid sha256:a010f7b4762d5f4543e3a1f23359041c56ca3dda6d7ea51f07d2dcaf46247628
+size 29752
