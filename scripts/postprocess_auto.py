345e4c89f6c8767b925e58cb3ed83fe9e0251228
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2023-01-31 01:31:25 +0100

    to prevent erros

--- scripts/postprocess_auto.py
+++ scripts/postprocess_auto.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40f027c190baec13c3f01264fb676b7ff6f9f37e426288f1da6024a50afcf6a2
-size 8954
+oid sha256:0ca808827b9569f7918e93e9b19d805ccc63eadfdf5fa3c38feb5c8c2e85892a
+size 8945
