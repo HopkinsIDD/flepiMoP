c234f0051c09d502d96ef87ca9f29da5038d1b54
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2023-01-31 01:12:10 +0100

    accomodating postprocess and sending to slack

--- scripts/postprocess_auto.py
+++ scripts/postprocess_auto.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a15e12c82f715e7cca0eeb341b73f4344e09c1ca107ea5e579193bf9b0534996
-size 8827
+oid sha256:84e28209abb10a84feb003e9e5c8c61dbcdb6c19c00a74f301484ef998e65b9e
+size 8834
