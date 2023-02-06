b394a4cd158ccd02e2f1af503a49c905f72f2617
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2023-02-07 00:48:35 +0100

    increased default due to run failing with error 137: not enough memory

--- batch/inference_job.py
+++ batch/inference_job.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a2dfa65d9e37163542e13ad8c3fab0da43b8ba59ee2d5e2980db0b4a70c363d
-size 30036
+oid sha256:ee31c257a25c24365629af48e9928bd2ebe066b182bbf962e24565dad8575d95
+size 30037
