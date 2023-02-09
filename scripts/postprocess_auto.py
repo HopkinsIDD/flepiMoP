2918c99e88b8ff9e99e446a6d9f27cbdd0f9f884
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2023-02-09 14:51:01 +0100

    fix flepibot channel

--- scripts/postprocess_auto.py
+++ scripts/postprocess_auto.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:186cf6c5c31c3ec6ec64c72f8abea92dbbd620b10201a4d6099415875227b543
-size 10437
+oid sha256:2446de063d62023380c7fe910ddb703f46c1db04473eb1668e3c3fbba3e5b1e1
+size 10445
