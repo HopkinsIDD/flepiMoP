726809ad15ba24f8d9283d820424c77350f04eac
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2023-02-13 23:46:07 +0100

    removed log polluting warning and fix soon to be deprecated pandas functions

--- gempyor_pkg/src/gempyor/outcomes.py
+++ gempyor_pkg/src/gempyor/outcomes.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2d79750cfe7cb252cc981b3bab08d6052817c7e986d876a658b7ead481766e8
-size 25316
+oid sha256:e245df14d7671b7ed1dde316be176237e6e51176833f40bf2b77a139f9d0501b
+size 25333
