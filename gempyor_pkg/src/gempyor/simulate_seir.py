6136cfd030d7d0772ef6b4d3419b8ada2f608664
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2023-02-13 20:45:40 +0100

    fix (#496): we can have one node setup without specifying mobility nor fake second node

--- gempyor_pkg/src/gempyor/simulate_seir.py
+++ gempyor_pkg/src/gempyor/simulate_seir.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f06076a41bd70ce179170ae181a45e45e40fbf192e830650b15cf2e119b3003d
-size 7795
+oid sha256:d1dbca6e5db7c6b04627277f1eabbe180bbad242a409aca3ceb7b152c9f8f24b
+size 7860
