63b7ebee8d3b8163549f8304f42053c23ddc23f6
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2023-02-20 13:12:27 +0100

    postprocessin scripts fix

--- batch/postprocessing-scripts.sh
+++ batch/postprocessing-scripts.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ed830a6fed51b0ce3050bee1be3800a6abc5c691f1f610fb2d61b7c820e944c6
-size 405
+oid sha256:833ebc29bc1a454474f3cf643fa956eb29b8cec437bbffcda08cd407aaa6f582
+size 533
