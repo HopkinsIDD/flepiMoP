1f251e1d5d39e3459f198dc8baa3ed5c16f8cd64
kjsato <koz.sato@gmail.com>
2023-03-01 15:02:49 -0500

    fixing -  Cannot rewrite branches: Your index contains uncommitted changes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:62c6843fa620de82842e1a602de543a7577740be750238ebbca4e1ea68a26113
-size 3083
+oid sha256:2e1a58e8d6459de26dbbd4b2be06d74a08310c444d2a967abd27748d8454da30
+size 3367
--- build/local_install.R
+++ build/local_install.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbd21b19906a65244f8745bce93de7f6b5c4802a9dc3f347059224b134638051
-size 1368
+oid sha256:8e49ac58f0999bbb2e07518d0cae53199a475b8b9bd85feadaacd3abd97b74e1
+size 1387
