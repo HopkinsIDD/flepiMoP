90809ba6aadfa377711a63432041ef65e20899a4
kjsato <koz.sato@gmail.com>
2023-08-08 16:12:11 -0400

    deleted unnecessary lines in tests/seir/test_seir.py::test_check_values()

--- flepimop/gempyor_pkg/tests/seir/test_seir.py
+++ flepimop/gempyor_pkg/tests/seir/test_seir.py
@@ -35,8 +35,8 @@ def test_check_values():
 
         seeding[0, 0] = 1
 
-        if np.all(seeding == 0):
-            warnings.warn("provided seeding has only value 0", UserWarning)
+        #if np.all(seeding == 0):
+        #    warnings.warn("provided seeding has only value 0", UserWarning)
 
         if np.all(modinf.mobility.data < 1):
             warnings.warn("highest mobility value is less than 1", UserWarning)
