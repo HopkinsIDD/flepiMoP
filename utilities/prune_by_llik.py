4247514c7a21830ee42a5b9ab14a9972b77e4018
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2023-09-08 23:30:35 +0200

    fix

--- utilities/prune_by_llik.py
+++ utilities/prune_by_llik.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
-import glob, os, sys
+import glob, os, sys, re
 from pathlib import Path
 
 import pyarrow.parquet as pq
