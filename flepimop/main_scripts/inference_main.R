a6cae8c43f7bb60a65619a22cc711ba6ab64a5fd
Shaun Truelove <shauntruelove@users.noreply.github.com>
2023-10-11 13:51:53 -0400

    update inference to the new modifiers structure

--- flepimop/main_scripts/inference_main.R
+++ flepimop/main_scripts/inference_main.R
@@ -43,7 +43,11 @@ config <- flepicommon::load_config(opt$config)
 ##If outcome scenarios are specified check their existence
 outcome_modifiers_scenarios <- opt$outcome_modifiers_scenarios
 if (all(outcome_modifiers_scenarios == "all")) {
-  outcome_modifiers_scenarios <- config$outcomes_modifiers$scenarios
+    if (!is.null(config$outcomes_modifiers$scenarios)){
+        outcome_modifiers_scenarios <- config$outcomes_modifiers$scenarios
+    } else {
+        outcome_modifiers_scenarios <- "all"
+    }
 } else if (!(outcome_modifiers_scenarios %in% config$outcomes_modifiers$scenarios)){
   message(paste("Invalid outcome scenario argument:[",paste(setdiff(outcome_modifiers_scenarios, config$outcome$scenarios)), "] did not match any of the named args in", paste(config$outcomes_modifiers$scenarios, collapse = ", "), "\n"))
   quit("yes", status=1)
