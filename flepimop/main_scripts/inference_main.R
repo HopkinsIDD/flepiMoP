9e5f74ab7ceca4064cc7efd6c214e5be48c1c078
Shaun Truelove <shauntruelove@users.noreply.github.com>
2023-10-11 23:30:45 -0400

    Revert "update inference to the new modifiers structure"
    This reverts commit a6cae8c43f7bb60a65619a22cc711ba6ab64a5fd.

--- flepimop/main_scripts/inference_main.R
+++ flepimop/main_scripts/inference_main.R
@@ -43,11 +43,7 @@ config <- flepicommon::load_config(opt$config)
 ##If outcome scenarios are specified check their existence
 outcome_modifiers_scenarios <- opt$outcome_modifiers_scenarios
 if (all(outcome_modifiers_scenarios == "all")) {
-    if (!is.null(config$outcomes_modifiers$scenarios)){
-        outcome_modifiers_scenarios <- config$outcomes_modifiers$scenarios
-    } else {
-        outcome_modifiers_scenarios <- "all"
-    }
+  outcome_modifiers_scenarios <- config$outcomes_modifiers$scenarios
 } else if (!(outcome_modifiers_scenarios %in% config$outcomes_modifiers$scenarios)){
   message(paste("Invalid outcome scenario argument:[",paste(setdiff(outcome_modifiers_scenarios, config$outcome$scenarios)), "] did not match any of the named args in", paste(config$outcomes_modifiers$scenarios, collapse = ", "), "\n"))
   quit("yes", status=1)
