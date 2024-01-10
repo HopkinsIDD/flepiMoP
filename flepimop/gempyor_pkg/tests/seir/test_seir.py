b2f93b5643824c5973300608d6270a05fade8dc5
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2024-01-10 17:31:16 +0100

    fix test to respect the overlap operation

--- flepimop/gempyor_pkg/tests/seir/test_seir.py
+++ flepimop/gempyor_pkg/tests/seir/test_seir.py
@@ -741,6 +741,10 @@ def test_parallel_compartments_no_vacc():
         modinf=modinf,
         modifiers_library=modinf.seir_modifiers_library,
         subpops=modinf.subpop_struct.subpop_names,
+        pnames_overlap_operation_sum=modinf.parameters.stacked_modifier_method["sum"],
+        pnames_overlap_operation_reductionprod=modinf.parameters.stacked_modifier_method["reduction_product"],
+           
+        
     )
 
     params = modinf.parameters.parameters_quick_draw(modinf.n_days, modinf.nsubpops)
