ea36079860c68bb809be56f4faad9d642997b684
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2024-04-10 14:17:34 +0200

    logloss

--- flepimop/gempyor_pkg/src/gempyor/logloss.py
+++ flepimop/gempyor_pkg/src/gempyor/logloss.py
@@ -26,9 +26,18 @@ class Statistic:
         else:
             return data
 
-    def compute_loss(self, model_data, gt_data):
+    def compute_logloss(self, model_data, gt_data, skip_resampling=False):
+        if not skip_resampling:
+            model_data = self.apply_resampling(model_data)
+            gt_data = self.apply_resampling(gt_data)
+
+        model_data = model_data[self.sim_var]
+        gt_data = gt_data[self.data_var]
+
+        assert model_data.shape == gt_data.shape
+
         if self.loss_function == "rmse":
-            return np.sqrt(np.mean((model_data - gt_data) ** 2))
+            return -np.sqrt(np.mean((model_data - gt_data) ** 2))
         elif self.loss_function == "poisson_log_loss":
             epsilon = 1e-10  # to avoid log(0)
             return -np.mean(model_data - gt_data * np.log(model_data + epsilon))
@@ -38,36 +47,38 @@ class Statistic:
 
 class LogLoss:
     def __init__(self, inference_config: confuse.ConfigView, data_dir:str = "."):
-        self.gt = pd.read_csv(f"{data_dir}/"inference_config["gt_data_path"].get())
-        
+        self.gt = pd.read_csv(f"{data_dir}/{inference_config['gt_data_path'].get()}")
+        self.statistics = {}
+        for key, value in inference_config["statistics"].items():
+            self.statistics[key] = Statistic(key, value)
 
-
-def log_loss(model_df, gt, modinf, statistics):
-    """
-        model_df, gt: dataframe indexed by date
+    def compute_logloss(self, model_df, modinf):
+        """
+        Compute logloss for all statistics
+        model_df: DataFrame indexed by date
+        modinf: model information
         TODO: support kwargs for emcee, and this looks very slow
-    """
-    
-    log_loss = xr.DataArray(0, dims=["statistic", "subpop"],  
+        """
+        logloss = xr.DataArray(0, dims=["statistic", "subpop"],  
                 coords={
-                "statistic":statistics.key(),
+                "statistic":self.statistics.key(),
                 "subpop":modinf.subpop_struct.subpop_names})
 
-    for subpop in modinf.subpop_struct.subpop_names:    
-        # essential to sort by index (date here)
-        gt_s = gt[gt["subpop"]==subpop].sort_index()
-        model_df_s = model_df[model_df["subpop"]==subpop].sort_index()
+        for subpop in modinf.subpop_struct.subpop_names:
+            # essential to sort by index (date here)
+            gt_s = self.gt[self.gt["subpop"] == subpop].sort_index()
+            model_df_s = model_df[model_df["subpop"] == subpop].sort_index()
 
-        # places where data and model overlap
-        first_date = max(gt_s.index.min(), model_df_s.index.min())
-        last_date = min(gt_s.index.max(), model_df_s.index.max())
+            # Places where data and model overlap
+            first_date = max(gt_s.index.min(), model_df_s.index.min())
+            last_date = min(gt_s.index.max(), model_df_s.index.max())
 
-        gt_s = gt_s.loc[first_date:last_date].drop(["subpop"],axis=1).resample("W-SAT").agg(pd.Series.sum, skipna=False) # if one NA in the interval, skip the itnerval  (see https://stackoverflow.com/questions/54252106/while-resampling-put-nan-in-the-resulting-value-if-there-are-some-nan-values-in)
-        model_df_s = model_df_s.drop(["subpop","time"],axis=1).loc[first_date:last_date].resample("W-SAT").agg(pd.Series.sum, skipna=False) # todo sub subpop here
-        
-        for key, value in statistics.items():
-            assert model_df_s[key].shape == gt_s[value].shape
+            gt_s = gt_s.loc[first_date:last_date].drop(["subpop"], axis=1)
+            model_df_s = model_df_s.drop(["subpop", "time"], axis=1).loc[first_date:last_date]
 
-            log_loss.loc[dict(statistics=key, subpop=subpop)] += np.nansum((model_df_s[key]-gt_s[value])**2)
+            for key, stat in self.statistics.items():
+                logloss.loc[dict(statistics=key, subpop=subpop)] += stat.compute_logloss(model_df, gt_s)
 
-    return -log_loss,
+        return logloss
+
+
