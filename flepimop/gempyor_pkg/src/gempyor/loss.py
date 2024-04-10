5f142ceb34fb94b0739a3be60aedc84684ec872b
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2024-04-10 13:58:49 +0200

    progressing on stat

--- flepimop/gempyor_pkg/src/gempyor/loss.py
+++ flepimop/gempyor_pkg/src/gempyor/loss.py
@@ -6,12 +6,34 @@ import confuse
 ## https://docs.xarray.dev/en/stable/user-guide/indexing.html#assigning-values-with-indexing
 ##
 
+
 class Statistic:
     def __init__(self, name, statistic_config: confuse.ConfigView):
         self.sim_var = statistic_config["sim_var"].as_str()
         self.data_var = statistic_config["data_var"].as_str()
         self.name = name
-        self.preprocess = 
+        self.resample_config = statistic_config.get("resample_config", None)
+        self.loss_function = statistic_config.get("loss_function", "rmse")
+
+    def apply_resampling(self, data):
+        if self.resample_config:
+            freq = self.resample_config.get("freq", "W-SAT")
+            agg_func = getattr(pd.Series, self.resample_config.get("agg_func", "sum"))
+            skipna = self.resample_config.get("skipna", False)
+
+            data_resampled = data.resample(freq).agg(agg_func, skipna=skipna)
+            return data_resampled
+        else:
+            return data
+
+    def compute_loss(self, model_data, gt_data):
+        if self.loss_function == "rmse":
+            return np.sqrt(np.mean((model_data - gt_data) ** 2))
+        elif self.loss_function == "poisson_log_loss":
+            epsilon = 1e-10  # to avoid log(0)
+            return -np.mean(model_data - gt_data * np.log(model_data + epsilon))
+        else:
+            raise ValueError("Unsupported loss function")
 
 
 class LogLoss:
