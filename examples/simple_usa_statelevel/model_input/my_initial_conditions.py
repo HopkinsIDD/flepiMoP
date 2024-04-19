55351138548c21be99fe9db6b1aac0f925359e32
Joseph Lemaitre <jo.lemaitresamra@gmail.com>
2024-04-20 01:06:55 +0200

    fix benchmark, and add the posibility to avoid 0

--- examples/simple_usa_statelevel/model_input/my_initial_conditions.py
+++ examples/simple_usa_statelevel/model_input/my_initial_conditions.py
@@ -4,10 +4,12 @@ import numpy as np
 
 class InitialConditions(gempyor.initial_conditions.InitialConditions):
     def get_from_config(self, sim_id: int, modinf) -> np.ndarray:
+
         y0 = np.zeros((modinf.compartments.compartments.shape[0], modinf.nsubpops))
         S_idx = modinf.compartments.get_comp_idx({"infection_stage": "S"})
         I_idx = modinf.compartments.get_comp_idx({"infection_stage": "I"})
-        prop_inf = np.random.uniform(low=0, high=0.01, size=modinf.nsubpops)
+        prop_inf = .005 #np.random.uniform(low=0, high=0.01, size=modinf.nsubpops)
+        # TODO cause this is another example
         y0[S_idx, :] = modinf.subpop_pop * (1 - prop_inf)
         y0[I_idx, :] = modinf.subpop_pop * prop_inf
 
