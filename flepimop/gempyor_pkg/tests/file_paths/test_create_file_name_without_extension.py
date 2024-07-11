8035dcc8a5d8553c590c3f50789df2cd92d8d274
Timothy Willard <timothywillard22@gmail.com>
2024-07-11 11:55:12 -0400

    Unit tests for `create_file_name_without_extension`
    Added unit tests for
    `gempyor.file_paths.create_file_name_without_extension`. Used a general
    purpose `change_directory_to_temp_directory` pytest fixture to avoid
    test paths in git repo, but should be moved to a better home at some
    point.

---
+++ flepimop/gempyor_pkg/tests/file_paths/test_create_file_name_without_extension.py
@@ -0,0 +1,79 @@
+from collections.abc import Generator
+import os
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import pytest
+
+from gempyor.file_paths import create_file_name_without_extension
+
+
+@pytest.fixture
+def change_directory_to_temp_directory() -> Generator[None, None, None]:
+    current_dir = os.getcwd()
+    temp_dir = TemporaryDirectory()
+    os.chdir(temp_dir.name)
+    yield
+    os.chdir(current_dir)
+    temp_dir.cleanup()
+
+
+class TestCreateFileNameWithoutExtension:
+    """
+    Unit tests for the `gempyor.file_paths.create_file_name_without_extension` function.
+    """
+
+    @pytest.mark.usefixtures("change_directory_to_temp_directory")
+    @pytest.mark.parametrize(
+        (
+            "run_id",
+            "prefix",
+            "index",
+            "ftype",
+            "inference_filepath_suffix",
+            "inference_filename_prefix",
+            "create_directory",
+        ),
+        [
+            ("abc", "def", "ghi", "jkl", "mno", "pqr", False),
+            ("abc", "def", "ghi", "jkl", "mno", "pqr", True),
+            ("abc", "def", 123, "jkl", "mno", "pqr", False),
+            ("abc", "def", 123, "jkl", "mno", "pqr", True),
+        ],
+    )
+    def test_create_file_name(
+        self,
+        run_id: str,
+        prefix: str,
+        index: str | int,
+        ftype: str,
+        inference_filepath_suffix: str,
+        inference_filename_prefix: str,
+        create_directory: bool,
+    ) -> None:
+        # Setup
+        path = create_file_name_without_extension(
+            run_id=run_id,
+            prefix=prefix,
+            index=index,
+            ftype=ftype,
+            inference_filepath_suffix=inference_filepath_suffix,
+            inference_filename_prefix=inference_filename_prefix,
+            create_directory=create_directory,
+        )
+        expected_path = Path(
+            "model_output",
+            prefix,
+            ftype,
+            inference_filepath_suffix,
+            f"{inference_filename_prefix}{index:>09}.{run_id}.{ftype}",
+        )
+        
+        # Assertions
+        assert isinstance(path, Path)
+        assert path.exists() == False
+        assert path == expected_path
+        if create_directory:
+            assert path.parent.exists()
+        else:
+            assert not path.parent.exists()
