7f163a56b12a38c97f50fa3bbcbc1d50747fb1e2
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2024-07-12 08:58:10 -0400

    Finished `file_paths` documentation, unit tests
    * Added/expanded examples for `create_file_name`,
      `create_file_name_without_extension`, and `run_id` in
      `gempyor.file_paths`.
    * Added unit tests for user provided timestamps to `run_id`.
    * Applied formatting.

--- flepimop/gempyor_pkg/tests/file_paths/test_run_id.py
+++ flepimop/gempyor_pkg/tests/file_paths/test_run_id.py
@@ -1,5 +1,7 @@
-from datetime import datetime
+from datetime import datetime, timezone
 import re
+
+import pytest
 
 from gempyor.file_paths import run_id
 
@@ -7,7 +9,9 @@ from gempyor.file_paths import run_id
 class TestRunId:
     """Unit tests for the `gempyor.file_paths.run_id` function."""
 
-    def test_get_run_id(self) -> None:
+    run_id_regex = re.compile(r"(?i)^[0-9]{8}_[0-9]{6}([a-z]+)?$")
+
+    def test_get_run_id_default_timestamp(self) -> None:
         # Setup
         before = datetime.now()
         rid = run_id()
@@ -15,14 +19,30 @@ class TestRunId:
 
         # Basic assertions
         assert isinstance(rid, str)
-        assert re.match(r"^[0-9]{8}_[0-9]{6}$", rid)
+        assert self.run_id_regex.match(rid)
 
         # Run id is between before/after
         before = before.replace(microsecond=0)
         after = after.replace(microsecond=0, second=after.second + 1)
+        run_time = datetime.strptime(rid, "%Y%m%d_%H%M%S")
+        assert run_time >= before
+        assert run_time <= after
+
+    @pytest.mark.parametrize(
+        "timestamp",
+        [(None), (datetime.now()), (datetime(2024, 1, 1, tzinfo=timezone.utc))],
+    )
+    def test_get_run_id_user_provided_timestamp(
+        self, timestamp: None | datetime
+    ) -> None:
+        # Setup
+        rid = run_id(timestamp=timestamp)
+
+        # Assertions
+        assert isinstance(rid, str)
+        assert self.run_id_regex.match(rid)
         try:
             run_time = datetime.strptime(rid, "%Y%m%d_%H%M%S%Z")
         except:
             run_time = datetime.strptime(rid, "%Y%m%d_%H%M%S")
-        assert run_time >= before
-        assert run_time <= after
+        assert isinstance(run_time, datetime)
