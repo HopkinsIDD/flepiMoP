b914481db0d70a5c8056657a9c8ed89ccd43642a
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2024-07-31 13:00:35 -0400

    Create unit testing utils for `confuse` package
    Wrote, documented, and unit tested `create_confuse_subview_from_dict`
    and `create_confuse_rootview_from_dict` functions to easily create
    confuse package objects from dicts for use in parametrized unit tests.

--- flepimop/gempyor_pkg/src/gempyor/testing.py
+++ flepimop/gempyor_pkg/src/gempyor/testing.py
@@ -5,10 +5,18 @@ This module contains unit testing utilities, mostly pytest fixtures. To use this
 the optional test dependencies must be installed.
 """
 
+__all__ = [
+    "change_directory_to_temp_directory",
+    "create_confuse_rootview_from_dict",
+    "create_confuse_subview_from_dict",
+]
+
 from collections.abc import Generator
 import os
 from tempfile import TemporaryDirectory
+from typing import Any
 
+import confuse
 import pytest
 
 
@@ -30,3 +38,71 @@ def change_directory_to_temp_directory() -> Generator[None, None, None]:
     yield
     os.chdir(current_dir)
     temp_dir.cleanup()
+
+
+def create_confuse_rootview_from_dict(data: dict[str, Any]) -> confuse.RootView:
+    """
+    Create a RootView from a dictionary for unit testing confuse parameters.
+
+    Args:
+        data: The data to populate the confuse root view with.
+
+    Returns:
+        A confuse root view.
+
+    Examples:
+        >>> data = {
+        ...     "foo": "bar",
+        ...     "fizz": 123,
+        ...     "alphabet": ["a", "b", "c"],
+        ...     "mapping": {"x": 1, "y": 2},
+        ... }
+        >>> rv = create_confuse_rootview_from_dict(data)
+        >>> rv
+        <RootView: root>
+        >>> rv.keys()
+        ['foo', 'fizz', 'alphabet', 'mapping']
+        >>> rv.get()
+        {'foo': 'bar', 'fizz': 123, 'alphabet': ['a', 'b', 'c'], 'mapping': {'x': 1, 'y': 2}}
+        >>> rv == rv.root()
+        True
+        >>> rv.name
+        'root'
+    """
+    return confuse.RootView([confuse.ConfigSource.of(data)])
+
+
+def create_confuse_subview_from_dict(
+    name: str, data: dict[str, Any]
+) -> confuse.Subview:
+    """
+    Create a Subview from a dictionary for unit testing confuse parameters.
+
+    Args:
+        name: The name of the subview being created.
+        data: The data to populate the confuse subview with.
+
+    Returns:
+        A confuse subview.
+
+    Examples:
+        >>> data = {
+        ...     "foo": "bar",
+        ...     "fizz": 123,
+        ...     "alphabet": ["a", "b", "c"],
+        ...     "mapping": {"x": 1, "y": 2},
+        ... }
+        >>> sv = create_confuse_subview_from_dict("params", data)
+        >>> sv
+        <Subview: params>
+        >>> sv.keys()
+        ['foo', 'fizz', 'alphabet', 'mapping']
+        >>> sv.get()
+        {'foo': 'bar', 'fizz': 123, 'alphabet': ['a', 'b', 'c'], 'mapping': {'x': 1, 'y': 2}}
+        >>> sv == sv.root()
+        False
+        >>> sv.name
+        'params'
+    """
+    root_view = create_confuse_rootview_from_dict({name: data})
+    return root_view[name]
