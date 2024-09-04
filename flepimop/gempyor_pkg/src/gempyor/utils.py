2fbda88473052db90e66dcd1b29d2e3c2c2e9bcd
Emily Przykucki <emily.przykucki@gmail.com>
2024-09-04 14:10:38 -0400

    Documenting utils.py
    Adding examples to function documentation (and further refining documentation) in the gempyor.utils module.

--- flepimop/gempyor_pkg/src/gempyor/utils.py
+++ flepimop/gempyor_pkg/src/gempyor/utils.py
@@ -99,20 +99,17 @@ def read_df(
     )
 
 
-def command_safe_run(command: str, command_name: str="mycommand", fail_on_fail: bool=True):
+def command_safe_run(command: str, command_name: str="mycommand", fail_on_fail: bool=True) -> tuple[int, str, str]:
     """
     Runs a shell command and prints diagnostics if command fails.
 
     Args:
         command: The CLI command to be given.
-        command_name: The reference name for you command (str). Default value is "mycommand".
+        command_name: The reference name for you command. Default value is "mycommand".
         fail_on_fail: If True, an exception will be thrown if the command fails (default is True)
 
     Returns:
-        returncode: The returncode message from running yourcommand.
-        stdout: Standard output.
-        stderr: Standard error stream.
-
+        As a tuple; the return code, the standard output, and standard error from running the command.
     Raises:
         Exception: If fail_on_fail=True and the command fails, an exception will be thrown.
     """
@@ -141,7 +138,7 @@ def command_safe_run(command: str, command_name: str="mycommand", fail_on_fail: 
 
 def add_method(cls):
     """
-    Decorator to add a method to a class.
+    A function which adds a function to a class.
 
     Args:
         cls: The class you want to add a method to.
@@ -161,7 +158,7 @@ def add_method(cls):
     return decorator
 
 
-def search_and_import_plugins_class(plugin_file_path: str, path_prefix: str, class_name: str, **kwargs):
+def search_and_import_plugins_class(plugin_file_path: str, path_prefix: str, class_name: str, **kwargs: dict[str, any]) -> any:
     """
     Function serving to create a class that finds and imports the necessary modules.
 
@@ -169,10 +166,23 @@ def search_and_import_plugins_class(plugin_file_path: str, path_prefix: str, cla
         plugin_file_path: Pathway to the module.
         path_prefix: Pathway prefix to the module.
         class_name: Name of the class.
-        kwargs: Further arguments passed to initilization of the class.
+        **kwargs: Further arguments passed to initilization of the class.
 
     Returns:
-        The classs_name attribute of module class.
+        The instance of the class that was instantiated with provided **kwargs.
+
+    Example:
+        Suppose there is a module called `my_plugin.py with a class `MyClass` located at `/path/to/plugin/`.
+        
+        Dynamically import and instantiate the class:
+        ```
+        instance = search_and_import_plugins_class('/path/to/plugin', path_prefix, 'MyClass', **params)
+        ```
+
+        View the instance:
+        ```
+        print(instance.display())
+        ```
     """
     # Look for all possible plugins and import them
     # https://stackoverflow.com/questions/67631/how-can-i-import-a-module-dynamically-given-the-full-path
@@ -195,31 +205,40 @@ import pstats
 from functools import wraps
 
 
-def profile(output_file=None, sort_by="cumulative", lines_to_print=None, strip_dirs: bool=False):
-    """A time profiler decorator.
+def profile(output_file: str = None, sort_by: str = "cumulative", lines_to_print: int = None, strip_dirs: bool = False):
+    """
+    A time profiler decorator.
     Inspired by and modified the profile decorator of Giampaolo Rodola:
     http://code.activestate.com/recipes/577817-profile-decorator/
 
     Args:
-        output_file: str or None. Default is None
+        output_file: 
             Path of the output file. If only name of the file is given, it's
             saved in the current directory.
             If it's None, the name of the decorated function is used.
-        sort_by: str or SortKey enum or tuple/list of str/SortKey enum
+        sort_by: 
             Sorting criteria for the Stats object.
             For a list of valid string and SortKey refer to:
             https://docs.python.org/3/library/profile.html#pstats.Stats.sort_stats
-        lines_to_print: int or None
+        lines_to_print: 
             Number of lines to print. Default (None) is for all the lines.
             This is useful in reducing the size of the printout, especially
             that sorting by 'cumulative', the time consuming operations
             are printed toward the top of the file.
-        strip_dirs: bool
-            Whether to remove the leading path info from file names.
-            This is also useful in reducing the size of the printout.
+        strip_dirs: 
+            Whether to remove the leading path info from file names. 
 
     Returns:
         Profile of the decorated function.
+
+    Example:
+        ```
+        @profile(output_file="my_function.prof")
+        def my_function():
+            # Function body content
+            pass
+        my_function()
+        ```
     """
 
     def inner(func):
@@ -238,7 +257,7 @@ def profile(output_file=None, sort_by="cumulative", lines_to_print=None, strip_d
     return inner
 
 
-def as_list(thing) -> list:
+def as_list(thing: any) -> list[any]:
     """
     Returns argument passed as a list.
 
@@ -246,7 +265,7 @@ def as_list(thing) -> list:
         thing: The object that you would like to be converted to a list.
 
     Returns:
-        thing: The object converted to a list.
+        he object converted to a list.
     """
     if type(thing) == list:
         return thing
@@ -276,11 +295,18 @@ class Timer(object):
 class ISO8601Date(confuse.Template):
     """
     Reads in config dates into datetimes.dates.
-
-    Attributes:
-        value: Date value.
     """
-    def convert(self, value, view):
+    def convert(self, value: any, view: confuse.View):
+        """
+        Converts the given value to a datetime.date object.
+
+        Args:
+            value: The value to be converted. Can be datetime.date object or ISO8601 string.
+            view:  A view object from confuse, to be used for error reporting.
+
+        Raises:
+            confuse.TemplateError: If `value` is neither a datetime.date nor an ISO8601Date string.
+        """
         if isinstance(value, datetime.date):
             return value
         elif isinstance(value, str):
@@ -296,9 +322,6 @@ def as_date(self) -> datetime.date:
 
     Returns:
         A datetime.date data type of the date associated with the object.
-
-    Raises:
-        ValueError: On parsing errors.
     """
 
     return self.get(ISO8601Date())
@@ -348,8 +371,13 @@ def get_truncated_normal(
         b: The upper bound of the truncated normal distribution. Defaults to 10.
 
     Returns:
-        rv_frozen: A frozen instance of the truncated normal distribution with the
-        specified parameters.
+        rv_frozen: A frozen instance of the truncated normal distribution with the specified parameters.
+    
+    Example:
+        Create a truncated normal distribution with specified parameters (truncated between 1 and 10):
+        ```
+        truncated_normal_dist = get_truncated_normal(mean=5, sd=2, a=1, b=10)
+        ```
     """
     lower = (a - mean) / sd
     upper = (b - mean) / sd
@@ -360,7 +388,8 @@ def get_log_normal(
     meanlog: float | int,
     sdlog: float | int,
 ) -> scipy.stats._distn_infrastructure.rv_frozen:
-    """Returns a log normal distribution.
+    """
+    Returns a log normal distribution.
 
     This function constructs a log normal distribution with the specified
     log mean and log standard deviation.
@@ -372,6 +401,12 @@ def get_log_normal(
     Returns:
         rv_frozen: A frozen instance of the log normal distribution with the
         specified parameters.
+    
+    Example:
+        Create a log-normal distribution with specified parameters:
+        ```
+        log_normal_dist = get_log_normal(meanlog=1, sdlog=0.5)
+        ```
     """
     return scipy.stats.lognorm(s=sdlog, scale=np.exp(meanlog), loc=0)
 
@@ -390,6 +425,28 @@ def as_random_distribution(self):
     Raises: 
         ValueError: When values are out of range.
         NotImplementedError: If an unknown distribution is found.
+
+    Example:
+        Say that `config` is a `confuse.ConfigView` instance.
+
+        To create a uniform distribution between 1 and 10:
+        ```
+        dist_function = config.as_random_distribution()
+        sample = dist_function()
+        ```
+
+        To use a truncated normal distribution:
+        ```
+        config_truncnorm = confuse.ConfigView({
+            "distribution": "truncnorm",
+            "mean": 0
+            "sd": 1,
+            "a": -1, 
+            "b": 1
+            })
+        truncnorm_dist_function = config_truncnorm.as_random_distribution()
+        truncnorm_sample = truncnorm_dist_function()
+        ```
     """
 
     if isinstance(self.get(), dict):
@@ -445,10 +502,12 @@ def list_filenames(
     in the filters will be returned.
 
     Args:
-        folder: The directory to search for files. Defaults to the current directory.
-        filters: A string or a list of strings to filter filenames. Only files
-        containing all the provided substrings will be returned. Defaults to an
-        empty list.
+        folder: 
+            The directory to search for files. Defaults to the current directory.
+        filters: 
+            A string or a list of strings to filter filenames. Only files
+            containing all the provided substrings will be returned. Defaults to an
+            empty list.
 
     Returns:
         A list of strings representing the paths to the files that match the filters.
@@ -499,7 +558,7 @@ def rolling_mean_pad(
     Examples:
         Below is a brief set of examples showcasing how to smooth a metric, like
         hospitalizations, using this function.
-
+        ```
         >>> import numpy as np
         >>> from gempyor.utils import rolling_mean_pad
         >>> hospitalizations = np.arange(1., 29.).reshape((7, 4))
@@ -519,6 +578,7 @@ def rolling_mean_pad(
             [17. , 18. , 19. , 20. ],
             [20.2, 21.2, 22.2, 23.2],
             [22.6, 23.6, 24.6, 25.6]])
+        ```
     """
     weights = (1. / window) * np.ones(window)
     output = scipy.ndimage.convolve1d(data, weights, axis=0, mode="nearest")
@@ -537,13 +597,22 @@ def rolling_mean_pad(
 
 def print_disk_diagnosis():
     """
-    Reads and prints AWS diagnoses. Includes total bytes, used bytes, free bytes.
+    Reads and prints AWS disk diagnostic information.
     """
     import os
     from os import path
     from shutil import disk_usage
 
-    def bash(command):
+    def bash(command: str) -> str:
+        """
+        Executes a shell command and returns its output.
+
+        Args:
+            command: The shell command to be executed.
+
+        Returns:
+            The output of the shell command.
+        """
         output = os.popen(command).read()
         return output
 
@@ -570,15 +639,32 @@ def create_resume_out_filename(
     Compiles run output information.
 
     Args:
-        flepi_run_index: Index of the run (str).
-        flepi_prefix: File prefix (str).
-        flepi_slot_index: Index of the slot (str).
-        flepi_block_index: Index of the block (str).
-        filetype: File type (str).
-        liketype: Chimeric or global (str).
+        flepi_run_index: Index of the run.
+        flepi_prefix: File prefix.
+        flepi_slot_index: Index of the slot.
+        flepi_block_index: Index of the block.
+        filetype: File type.
+        liketype: Chimeric or global.
     
     Returns:
         The path to a corresponding output file.
+    
+    Example:
+        Generate an output file with specified parameters:
+        ```
+        filename = create_resume_out_filename(
+            flepi_run_index="test_run",
+            flepi_prefix="model_output/run_id/",
+            flepi_slot_index="1",
+            flepi_block_index="2",
+            filetype="seed",
+            liketype="chimeric"
+            )
+        ```
+        Example output:
+        ```
+        "experiment/001/normal/intermediate/000000123.000000000.1.parquet"
+        ```
     """
     prefix = f"{flepi_prefix}/{flepi_run_index}"
     inference_filepath_suffix = f"{liketype}/intermediate"
@@ -613,6 +699,22 @@ def create_resume_input_filename(
     
     Returns:
         The path to the a corresponding input file.
+    
+    Example:
+        Generate an input file with specified parameters:
+        ```
+        filename = create_resume_input_filename(
+            resume_run_index="2",
+            flepi_prefix="model_output/run_id/",
+            flepi_slot_index="1",
+            filetype="seed",
+            liketype="chimeric"
+            )
+        ```
+        Example output:
+        ```
+        "experiment/002/normal/final/789.csv"
+        ```
     """
     prefix = f"{flepi_prefix}/{resume_run_index}"
     inference_filepath_suffix = f"{liketype}/final"
@@ -633,7 +735,8 @@ def create_resume_input_filename(
 def get_filetype_for_resume(resume_discard_seeding: str, flepi_block_index: str) -> list[str]:
     """
     Retrieves a list of parquet file types that are relevant for resuming a process based on
-    specific environment variable settings. This function dynamically determines the list
+    specific environment variable settings. 
+    This function dynamically determines the list
     based on the current operational context given by the environment.
 
     Args:
@@ -642,6 +745,25 @@ def get_filetype_for_resume(resume_discard_seeding: str, flepi_block_index: str)
 
     Returns:
         List of file types.
+    
+    Example:
+        Determine file types for block index 1 with seeding data NOT discarded:
+        ```
+        filetypes = get_filetype_for_resume(resume_discard_seeding="false", flepi_block_index="1")
+        ```
+        Output of `print(filetypes)`:
+        ```
+        ["seed", "spar", "snpi", "hpar", "hnpi", "init"]
+        ```
+
+        Determine file types for block index 2 with seeding data discarded:
+        ```
+        filtypes = get_filetype_for_resume(resume_discard_seeding="true", flepi_block_index="2")
+        ```
+        Output of `print(filetypes)`:
+        ```
+        ["seed", "spar", "snpi", "hpar", "hnpi", "host", "llik", "init"]
+        ```
     """
     if flepi_block_index == "1":
         if resume_discard_seeding == "true":
@@ -660,7 +782,7 @@ def create_resume_file_names_map(
     flepi_slot_index,
     flepi_run_index,
     last_job_output,
-) -> Dict[str, str]:
+) -> dict:
     """
     Generates a mapping of input file names to output file names for a resume process based on
     parquet file types and environmental conditions. The function adjusts the file name mappings
@@ -676,8 +798,9 @@ def create_resume_file_names_map(
         last_job_output: Adjusts the keys in the mapping to be prefixed with this path.
 
     Return:
-        Dict[str, str]: A dictionary where keys are input file paths and values are corresponding
-                        output file paths. 
+        A dictionary where keys are input file paths and values are corresponding
+        output file paths. 
+
     The mappings depend on:
     - Parquet file types appropriate for resuming a process, as determined by the environment.
     - Whether the files are for 'global' or 'chimeric' types, as these liketypes influence the
@@ -692,6 +815,19 @@ def create_resume_file_names_map(
         functions and environment variables which if improperly configured could lead to unexpected
         behavior.
     
+    Example:
+        Generate a mapping of file names for a given resume process:
+        ```
+        file_names_map = create_resume_file_names_map(
+            resume_discard_seeding="false",
+            flepi_block_index="1",
+            resume_run_index="1",
+            flepi_prefix="model_output/run_id/",
+            flepi_slot_index="1",
+            flepi_run_index="test_run",
+            last_job_output="s3://bucket/path/")
+        ```
+
     Notes:
     - The paths may be modified by the 'LAST_JOB_OUTPUT' if it is set and points to an S3 location.
     """
@@ -729,7 +865,7 @@ def create_resume_file_names_map(
     return resume_file_name_mapping
 
 
-def download_file_from_s3(name_map: Dict[str, str]) -> None:
+def download_file_from_s3(name_map: dict) -> None:
     """
     Downloads files from AWS S3 based on a mapping of S3 URIs to local file paths. The function
     checks if the directory for the first output file exists and creates it if necessary. It
@@ -737,13 +873,14 @@ def download_file_from_s3(name_map: Dict[str, str]) -> None:
     local path, and handles errors if the S3 URI format is incorrect or if the download fails.
 
     Args:
-        name_map (Dict[str, str]): A dictionary where keys are S3 URIs (strings) and values
-                                   are the local file paths (strings) where the files should
-                                   be saved.
+        name_map: 
+            A dictionary where keys are S3 URIs (strings) and values
+            are the local file paths (strings) where the files should
+            be saved.
 
     Returns:
-        None: This function does not return a value; its primary effect is the side effect of
-              downloading files and potentially creating directories.
+        This function does not return a value; its primary effect is the side effect of
+        downloading files and potentially creating directories.
 
     Raises:
         ValueError: If an S3 URI does not start with 's3://', indicating an invalid format.
@@ -802,9 +939,6 @@ def move_file_at_local(name_map: Dict[str, str]) -> None:
 
     Args:
         name_map (Dict[str, str]): A dictionary mapping source file paths to destination file paths.
-
-    Returns:
-        None
     """
     for src, dst in name_map.items():
         os.path.makedirs(os.path.dirname(dst), exist_ok=True)
