5fbb8fbce07864bb78ba6b059450259c22bc218d
Carl A. B. Pearson <carl.ab.pearson@gmail.com>
2024-09-26 11:33:07 -0400

    further install scripts fixes

--- flepimop/R_packages/inference/R/install_cli.R
+++ flepimop/R_packages/inference/R/install_cli.R
@@ -10,13 +10,12 @@
 #' 
 #' @export
 install_cli <- function(
-  path = if (.Platform$OS.type == "unix") file.path("usr", "local", "bin") else stop("Unsupported OS"),
-  overwrite = TRUE
+  path = if (.Platform$OS.type == "unix") normalizePath(file.path("/usr", "local", "bin")) else stop("Unsupported OS")
 ) {
   scriptfiles <- list.files(
     system.file("scripts", package = utils::packageName()), pattern = "flepimop-.*", full.names = TRUE
   )
   from <- scriptfiles
-  to <- file.path(path, gsub("\.R$", "", basename(scriptfiles)))
-  file.copy(from, to, overwrite = overwrite)
+  to <- file.path(path, gsub("\\.R$", "", basename(scriptfiles)))
+  file.symlink(from, to)
 }
