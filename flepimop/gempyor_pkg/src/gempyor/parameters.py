2066ee43776706fcb4ca2055415eb74a324cf810
Emily Przykucki <emily.przykucki@gmail.com>
2024-10-02 16:45:19 -0400

    Update parameters.py

--- flepimop/gempyor_pkg/src/gempyor/parameters.py
+++ flepimop/gempyor_pkg/src/gempyor/parameters.py
@@ -136,7 +136,7 @@ class Parameters:
                         f"ERROR loading file {fn_name} for parameter {pn}: " 
                         f"the 'date' entries of the provided file do not include all the days specified to be modeled by " 
                         f"the config. the provided file includes {len(df.index)} days between {str(df.index[0])} to {str(df.index[-1])}, " 
-                        f"while there are {len(pd.date_range(ti, tf))} days in the config time span of {ti}->{tf}. The file must contain entries for the "
+                        f"while there are {len(pd.date_range(ti, tf))} days in the config time span of {ti}->{tf}. The file must contain entries for "
                         f"the exact start and end dates from the config."
                     )
                 if not (pd.date_range(ti, tf) == df.index).all():
@@ -145,8 +145,8 @@ class Parameters:
                     raise ValueError(
                         f"ERROR loading file {fn_name} for parameter {pn}: "
                         f"the 'date' entries of the provided file do not include all the days specified to be modeled by "
-                        f" the config. the provided file includes {len(df.index)} days between {str(df.index[0])} to {str(df.index[-1])}, "
-                        f"while there are {len(pd.date_range(ti, tf))} days in the config time span of {ti}->{tf}. The file must contain entries for the "
+                        f"the config. the provided file includes {len(df.index)} days between {str(df.index[0])} to {str(df.index[-1])}, "
+                        f"while there are {len(pd.date_range(ti, tf))} days in the config time span of {ti}->{tf}. The file must contain entries for "
                         f"the exact start and end dates from the config."
                     )
 
