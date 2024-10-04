6d69186a6a85b2bf14a39b65fe9a1e91009d5df0
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2024-10-04 16:56:12 -0400

    Use `devtools::install` in `setup.R`
    Use `devtools::install` for `install.packages` for better handling of
    source package installs.

--- build/setup.R
+++ build/setup.R
@@ -8,15 +8,15 @@ if (length(.args) != 1) {
 
 # TODO sniff for upgrade mode
 
-if (!require(remotes)) {
-  install.packages("remotes", repos = c(getOption("repos"), "http://cran.r-project.org"));
-  stopifnot("Could not load `remotes` package." = require(remotes))
+if (!require(devtools)) {
+  install.packages("devtools", repos = c(getOption("repos"), "http://cran.r-project.org"));
+  stopifnot("Could not load `devtools` package." = require(devtools))
 }
 
 rpkgs <- list.files(file.path(.args[1], "flepimop", "R_packages"), full.names = TRUE)
 
 for (pkg in rpkgs) {
-  install.packages(pkg, repos = NULL, type = "source")
+  devtools::install(pkg, reload=FALSE, force=TRUE)
 }
 
 # other dependencies for analysis scripts
@@ -30,4 +30,7 @@ for (pkg in c("ggfortify", "flextable", "optparse", "cowplot")) {
 }
 
 # install the R scripts as executables
+if (!require(inference)) {
+  stop("Could not load `inference` package.")
+}
 inference::install_cli()
