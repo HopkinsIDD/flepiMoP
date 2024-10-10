873ab59ee29015443109fa57fbd493a4a3f99041
Carl A. B. Pearson <carl.ab.pearson@gmail.com>
2024-10-10 14:32:39 -0400

    update tutorial testing

--- examples/test_cli.py
+++ examples/test_cli.py
@@ -9,7 +9,7 @@ import os
 def test_config_sample_2pop():
   os.chdir(os.path.dirname(__file__) + "/tutorials")
   runner = CliRunner()
-  result = runner.invoke(simulate, ['-c', 'config_sample_2pop.yml'])
+  result = runner.invoke(simulate, ['config_sample_2pop.yml'])
   print(result.output) # useful for debug
   print(result.exit_code) # useful for debug
   print(result.exception) # useful for debug
@@ -20,7 +20,7 @@ def test_config_sample_2pop():
 def test_sample_2pop_modifiers():
   os.chdir(os.path.dirname(__file__) + "/tutorials")
   runner = CliRunner()
-  result = runner.invoke(simulate, ['-c', 'config_sample_2pop_modifiers.yml'])
+  result = runner.invoke(simulate, ['config_sample_2pop.yml', 'config_sample_2pop_outcomes.yml', 'config_sample_2pop_modifiers.yml'])
   print(result.output) # useful for debug
   print(result.exit_code) # useful for debug
   print(result.exception) # useful for debug
