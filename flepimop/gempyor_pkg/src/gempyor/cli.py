6c18229dd326f88f3161aa76fce204dac9016348
Carl A. B. Pearson <carl.ab.pearson@gmail.com>
2024-10-10 14:25:48 -0400

    expand cli coverage

--- flepimop/gempyor_pkg/src/gempyor/cli.py
+++ flepimop/gempyor_pkg/src/gempyor/cli.py
@@ -1,6 +1,10 @@
-import click
 from gempyor.shared_cli import argument_config_files, option_config_files, parse_config_files, cli
 from gempyor.utils import config
+
+# Guidance for extending the CLI:
+# - to add a new small command to the CLI, can just add a new function with the @cli.command() decorator here (e.g. patch below)
+# - to add something with lots of module logic in it, should define that in the module (e.g. compartments for a group command, or simulate for a single command)
+# - ... and then import that module here to add it to the CLI
 
 @cli.command()
 @argument_config_files
@@ -9,6 +13,9 @@ def patch(**kwargs):
     """Merge configuration files"""
     parse_config_files(**kwargs)
     print(config.dump())
+
+from .compartments import compartments
+from .simulate import simulate
 
 if __name__ == "__main__":
     cli()
