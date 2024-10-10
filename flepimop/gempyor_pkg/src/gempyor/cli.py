1f85233895e58115434e2a8208371aae5e652a65
Carl A. B. Pearson <carl.ab.pearson@gmail.com>
2024-10-10 15:00:47 -0400

    adding internal comments to cli.py

--- flepimop/gempyor_pkg/src/gempyor/cli.py
+++ flepimop/gempyor_pkg/src/gempyor/cli.py
@@ -6,6 +6,7 @@ from gempyor.utils import config
 # - to add something with lots of module logic in it, should define that in the module (e.g. compartments for a group command, or simulate for a single command)
 # - ... and then import that module here to add it to the CLI
 
+# add some basic commands to the CLI
 @cli.command()
 @argument_config_files
 @option_config_files
@@ -14,6 +15,7 @@ def patch(**kwargs):
     parse_config_files(**kwargs)
     print(config.dump())
 
+# register the commands from the other modules
 from .compartments import compartments
 from .simulate import simulate
 
