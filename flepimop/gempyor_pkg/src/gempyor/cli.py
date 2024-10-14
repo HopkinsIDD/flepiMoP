382547e47e06bc1bb3321cfb93057ad383908a25
Carl A. B. Pearson <carl.ab.pearson@gmail.com>
2024-10-14 12:43:36 -0400

    addressing some tw comments

--- flepimop/gempyor_pkg/src/gempyor/cli.py
+++ flepimop/gempyor_pkg/src/gempyor/cli.py
@@ -1,23 +1,30 @@
-from gempyor.shared_cli import argument_config_files, option_config_files, parse_config_files, cli
-from gempyor.utils import config
+from .shared_cli import (
+    argument_config_files,
+    option_config_files,
+    parse_config_files,
+    cli,
+)
+from .utils import config
+
+# register the commands from the other modules
+from .compartments import compartments
+from .simulate import simulate
 
 # Guidance for extending the CLI:
-# - to add a new small command to the CLI, can just add a new function with the @cli.command() decorator here (e.g. patch below)
-# - to add something with lots of module logic in it, should define that in the module (e.g. compartments for a group command, or simulate for a single command)
-# - ... and then import that module here to add it to the CLI
+# - to add a new small command to the CLI, add a new function with the @cli.command() decorator here (e.g. patch below)
+# - to add something with lots of module logic in it, define that in the module (e.g. .compartments, .simulate above)
+# - ... and then import that module above to add it to the CLI
+
 
 # add some basic commands to the CLI
 @cli.command()
 @argument_config_files
 @option_config_files
-def patch(**kwargs):
+def patch(**kwargs) -> None:
     """Merge configuration files"""
     parse_config_files(**kwargs)
     print(config.dump())
 
-# register the commands from the other modules
-from .compartments import compartments
-from .simulate import simulate
 
 if __name__ == "__main__":
     cli()
