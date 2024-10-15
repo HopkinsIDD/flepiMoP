e98396e1ad70381774fb69853d20e869f7a8cc6f
Carl A. B. Pearson <carl.ab.pearson@gmail.com>
2024-10-15 09:27:47 -0400

    update examples and cli testing

--- examples/test_cli.py
+++ examples/test_cli.py
@@ -10,50 +10,86 @@ from gempyor.simulate import simulate
 
 
 def test_config_sample_2pop():
-  os.chdir(os.path.dirname(__file__) + "/tutorials")
-  runner = CliRunner()
-  result = runner.invoke(simulate, ['config_sample_2pop.yml'])
-  print(result.output) # useful for debug
-  print(result.exit_code) # useful for debug
-  print(result.exception) # useful for debug
-  assert result.exit_code == 0
-  assert 'completed in' in result.output
+    os.chdir(os.path.dirname(__file__) + "/tutorials")
+    runner = CliRunner()
+    result = runner.invoke(simulate, ["config_sample_2pop.yml"])
+    print(result.output)  # useful for debug
+    print(result.exit_code)  # useful for debug
+    print(result.exception)  # useful for debug
+    assert result.exit_code == 0
+    assert "completed in" in result.output
+
+
+def test_config_sample_2pop():
+    os.chdir(os.path.dirname(__file__) + "/tutorials")
+    runner = CliRunner()
+    result = runner.invoke(simulate, ["-c", "config_sample_2pop.yml"])
+    print(result.output)  # useful for debug
+    print(result.exit_code)  # useful for debug
+    print(result.exception)  # useful for debug
+    assert result.exit_code == 0
+    assert "completed in" in result.output
 
 
 def test_sample_2pop_modifiers():
-  os.chdir(os.path.dirname(__file__) + "/tutorials")
-  runner = CliRunner()
-  result = runner.invoke(simulate, ['config_sample_2pop.yml', 'config_sample_2pop_outcomes.part', 'config_sample_2pop_modifiers.part'])
-  print(result.output) # useful for debug
-  print(result.exit_code) # useful for debug
-  print(result.exception) # useful for debug
-  assert result.exit_code == 0
-  assert 'completed in' in result.output
+    os.chdir(os.path.dirname(__file__) + "/tutorials")
+    runner = CliRunner()
+    result = runner.invoke(
+        simulate,
+        [
+            "config_sample_2pop.yml",
+            "config_sample_2pop_outcomes_part.yml",
+            "config_sample_2pop_modifiers_part.yml",
+        ],
+    )
+    print(result.output)  # useful for debug
+    print(result.exit_code)  # useful for debug
+    print(result.exception)  # useful for debug
+    assert result.exit_code == 0
+    assert "completed in" in result.output
+
 
 def test_sample_2pop_modifiers_combined():
-  os.chdir(os.path.dirname(__file__) + "/tutorials")
-  runner = CliRunner()
-  result = runner.invoke(simulate, ['config_sample_2pop_modifiers.yml'])
-  print(result.output) # useful for debug
-  print(result.exit_code) # useful for debug
-  print(result.exception) # useful for debug
-  assert result.exit_code == 0
-  assert 'completed in' in result.output
+    os.chdir(os.path.dirname(__file__) + "/tutorials")
+    runner = CliRunner()
+    result = runner.invoke(simulate, ["config_sample_2pop_modifiers.yml"])
+    print(result.output)  # useful for debug
+    print(result.exit_code)  # useful for debug
+    print(result.exception)  # useful for debug
+    assert result.exit_code == 0
+    assert "completed in" in result.output
+
+
+def test_sample_2pop_modifiers_combined():
+    os.chdir(os.path.dirname(__file__) + "/tutorials")
+    runner = CliRunner()
+    result = runner.invoke(simulate, ["-c", "config_sample_2pop_modifiers.yml"])
+    print(result.output)  # useful for debug
+    print(result.exit_code)  # useful for debug
+    print(result.exception)  # useful for debug
+    assert result.exit_code == 0
+    assert "completed in" in result.output
+
 
 def test_simple_usa_statelevel():
-  os.chdir(os.path.dirname(__file__) + "/simple_usa_statelevel")
-  runner = CliRunner()
-  result = runner.invoke(simulate, ['-n', '1', '-c', 'simple_usa_statelevel.yml'])
-  print(result.output) # useful for debug
-  print(result.exit_code) # useful for debug
-  print(result.exception) # useful for debug
-  assert result.exit_code == 0
-  assert 'completed in' in result.output
+    os.chdir(os.path.dirname(__file__) + "/simple_usa_statelevel")
+    runner = CliRunner()
+    result = runner.invoke(simulate, ["-n", "1", "-c", "simple_usa_statelevel.yml"])
+    print(result.output)  # useful for debug
+    print(result.exit_code)  # useful for debug
+    print(result.exception)  # useful for debug
+    assert result.exit_code == 0
+    assert "completed in" in result.output
+
 
 def test_simple_usa_statelevel_deprecated():
-  os.chdir(os.path.dirname(__file__) + "/simple_usa_statelevel")
-  result = subprocess.run(["gempyor-simulate", "-n", "1", "-c", "simple_usa_statelevel.yml"], capture_output=True, text=True)
-  print(result.stdout) # useful for debug
-  print(result.stderr) # useful for debug
-  print(result.returncode) # useful for debug
-  assert result.returncode == 0
+    os.chdir(os.path.dirname(__file__) + "/simple_usa_statelevel")
+    result = subprocess.run(
+        ["gempyor-simulate", "-n", "1", "-c", "simple_usa_statelevel.yml"],
+        capture_output=True,
+        text=True,
+    )
+    print(result.stdout)  # useful for debug
+    print(result.stderr)  # useful for debug
+    print(result.returncode)  # useful for debug
+    assert result.returncode == 0
