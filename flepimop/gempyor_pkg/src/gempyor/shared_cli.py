b42b391aede44ae14e072455d23a63070481caa5
Carl A. B. Pearson <carl.ab.pearson@gmail.com>
2024-10-15 10:36:29 -0400

    prune deprecated, some shared_cli reorg

--- flepimop/gempyor_pkg/src/gempyor/shared_cli.py
+++ flepimop/gempyor_pkg/src/gempyor/shared_cli.py
@@ -1,14 +1,16 @@
-import multiprocessing
-import warnings
+"""
+An internal module to share common CLI elements. Defines the overall cli group,
+supported options for config file overrides, and custom click decorators.
+"""
+
 from functools import reduce
+import multiprocessing
+import pathlib
+import warnings
 
 import click
 
 from .utils import config, as_list
-
-"""
-An internal module to share common CLI elements.
-"""
 
 __all__ = []
 
@@ -17,24 +19,42 @@ def cli():
     """Flexible Epidemic Modeling Platform (FlepiMoP) Command Line Interface"""
     pass
 
-
+# click decorator to handle configuration file(s) as arguments
+# use as `@argument_config_files` before a cli command definition
 argument_config_files = click.argument(
     "config_files", nargs=-1, type=click.Path(exists=True)
 )
-""" `click` Argument decorator to handle configuration file(s) """
 
+# List of `click` options that will be applied by `option_config_files`
+# n.b., the help for these options will be presented in the order defined here
 config_file_options = [
     click.option(
-        "--write-csv/--no-write-csv",
-        default=False,
-        show_default=True,
-        help="write csv output?",
+        "-c",
+        "--config",
+        "config_filepath",
+        envvar="CONFIG_PATH",
+        type=click.Path(exists=True),
+        required=False,  # deprecated = ["-c", "--config"],
+        # preferred = "CONFIG_FILES...",
+        help="Deprecated: configuration file for this simulation",
     ),
     click.option(
-        "--write-parquet/--no-write-parquet",
-        default=True,
-        show_default=True,
-        help="write parquet output?",
+        "-s",
+        "--seir_modifiers_scenarios",
+        envvar="FLEPI_SEIR_SCENARIO",
+        type=str,
+        default=[],
+        multiple=True,
+        help="override/select the transmission scenario(s) to run",
+    ),
+    click.option(
+        "-d",
+        "--outcome_modifiers_scenarios",
+        envvar="FLEPI_OUTCOME_SCENARIO",
+        type=str,
+        default=[],
+        multiple=True,
+        help="override/select the outcome scenario(s) to run",
     ),
     click.option(
         "-j",
@@ -94,59 +114,46 @@ config_file_options = [
         help="Run stochastic simulations?",
     ),
     click.option(
-        "-s",
-        "--seir_modifiers_scenarios",
-        envvar="FLEPI_SEIR_SCENARIO",
-        type=str,
-        default=[],
-        multiple=True,
-        help="override/select the transmission scenario(s) to run",
+        "--write-csv/--no-write-csv",
+        default=False,
+        show_default=True,
+        help="write csv output?",
     ),
     click.option(
-        "-d",
-        "--outcome_modifiers_scenarios",
-        envvar="FLEPI_OUTCOME_SCENARIO",
-        type=str,
-        default=[],
-        multiple=True,
-        help="override/select the outcome scenario(s) to run",
-    ),
-    click.option(
-        "-c",
-        "--config",
-        "config_filepath",
-        envvar="CONFIG_PATH",
-        type=click.Path(exists=True),
-        required=False,  # deprecated = ["-c", "--config"],
-        # preferred = "CONFIG_FILES...",
-        help="Deprecated: configuration file for this simulation",
+        "--write-parquet/--no-write-parquet",
+        default=True,
+        show_default=True,
+        help="write parquet output?",
     ),
 ]
-""" List of `click` options that will be applied by `option_config_files` """
-
 
 def option_config_files(function: callable) -> callable:
-    """`click` Option decorator to apply handlers for all options"""
-    reduce(lambda f, option: option(f), config_file_options, function)
+    """
+    `click` Option decorator to apply handlers for all options
+    use as `@option_config_files` before a cli command definition
+    """
+    reduce(lambda f, option: option(f), reversed(config_file_options), function)
     return function
 
 
 def parse_config_files(
-    config_files: list[str],
-    config_filepath: str,
-    in_run_id: str,
-    out_run_id: str,
-    seir_modifiers_scenarios: list[str],
-    outcome_modifiers_scenarios: list[str],
-    in_prefix: str,
-    nslots: int,
-    jobs: int,
-    write_csv: bool,
-    write_parquet: bool,
-    first_sim_index: int,
-    stoch_traj_flag: bool,
+    config_files: list[pathlib.Path],
+    config_filepath: pathlib.Path = None,
+    seir_modifiers_scenarios: list[str] = [],
+    outcome_modifiers_scenarios: list[str] = [],
+    in_run_id: str = None,
+    out_run_id: str = None,
+    in_prefix: str = None,
+    nslots: int = None,
+    jobs: int = None,
+    write_csv: bool = False,
+    write_parquet: bool = True,
+    first_sim_index: int = 1,
+    stoch_traj_flag: bool = False,
 ) -> None:
-    """Parse the configuration file(s) and override with command line arguments"""
+    """
+    Parse configuration file(s) and override with command line arguments
+    """
     config.clear()
     if config_filepath:
         if not len(config_files):
