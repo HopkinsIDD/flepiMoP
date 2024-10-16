07d93cdf8b3a540a04c01e225c95235f827813d6
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2024-10-16 07:56:51 -0400

    Add empty space to invoke flepicommon CI

--- flepimop/R_packages/flepicommon/R/config.R
+++ flepimop/R_packages/flepicommon/R/config.R
@@ -159,6 +159,7 @@ check_within_bounds <- function(value, obj) {
   }
 }
 
+
 #' @name prettyprint_optlist
 #' @description Print a list of options such that it does not take the whole screen
 #' Display `name : value` \n for all elements.
