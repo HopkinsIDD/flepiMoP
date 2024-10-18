88e310db1fc7bfd34a28496779a6015ca4445a45
Emily Przykucki <100221052+emprzy@users.noreply.github.com>
2024-10-18 12:49:22 -0400

    Update flepimop/gempyor_pkg/src/gempyor/inference.py
    Co-authored-by: Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>

--- flepimop/gempyor_pkg/src/gempyor/inference.py
+++ flepimop/gempyor_pkg/src/gempyor/inference.py
@@ -268,8 +268,8 @@ def autodetect_scenarios(config):
 
     if len(seir_modifiers_scenarios) != 1 or len(outcome_modifiers_scenarios) != 1:
         raise ValueError(
-            f"Inference only supports configuration files with one scenario, recieved:\n"
-            f"SEIR modifiers: '{seir_modifiers_scenarios}'."
+            f"Inference only supports configuration files with one scenario, recieved "
+            f"SEIR modifiers: '{seir_modifiers_scenarios}', and "
             f"Outcomes modifiers: '{outcome_modifiers_scenarios}'."
         )
 
