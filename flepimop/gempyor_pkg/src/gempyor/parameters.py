99b4aec107a13180cf565b3313b98dd4dba65b43
Emily Przykucki <emily.przykucki@gmail.com>
2024-11-06 13:52:42 -0500

    Update parameters.py
    Updating an error message to be more streamlined, and also match the RegEx in test_parameters_class.py

--- flepimop/gempyor_pkg/src/gempyor/parameters.py
+++ flepimop/gempyor_pkg/src/gempyor/parameters.py
@@ -128,20 +128,16 @@ class Parameters:
                     print("loaded dates:", df.index)
                     raise ValueError(
                         f"Issue loading file '{fn_name}' for parameter '{pn}': "
-                        f"the 'date' entries of the provided file do not include all the days specified to be modeled by "
-                        f"the config. The provided file includes '{len(df.index)}' days between '{str(df.index[0])}' and '{str(df.index[-1])}', " 
-                        f"while there are '{len(pd.date_range(ti, tf))}' days in the config time span of '{ti}' to '{tf}'. "
-                        f"The file must contain entries for the exact start and end dates from the config."
+                        f"Provided file dates span '{str(df.index[0])}' to '{str(df.index[-1])}', "
+                        f"but the config dates span '{ti}' to '{tf}'."
                     )
                 if not (pd.date_range(ti, tf) == df.index).all():
                     print("config dates:", pd.date_range(ti, tf))
                     print("loaded dates:", df.index)
                     raise ValueError(
                         f"Issue loading file '{fn_name}' for parameter '{pn}': "
-                        f"the 'date' entries of the provided file do not include all the days specified to be modeled by "
-                        f"the config. The provided file includes '{len(df.index)}' days between '{str(df.index[0])}' and '{str(df.index[-1])}', " 
-                        f"while there are '{len(pd.date_range(ti, tf))}' days in the config time span of '{ti}' to '{tf}'. "
-                        f"The file must contain entries for the exact start and end dates from the config."
+                        f"Provided file dates span '{str(df.index[0])}' to '{str(df.index[-1])}', "
+                        f"but the config dates span '{ti}' to '{tf}'."
                     )
 
                 self.pdata[pn]["ts"] = df
