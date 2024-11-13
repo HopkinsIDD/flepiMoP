d844ced1d806e8047f4ad3a2b94d294c4186b00f
Emily Przykucki <emily.przykucki@gmail.com>
2024-11-13 12:24:53 -0500

    Regex update

--- flepimop/gempyor_pkg/tests/seir/test_model_info.py
+++ flepimop/gempyor_pkg/tests/seir/test_model_info.py
@@ -46,7 +46,7 @@ class TestModelInfo:
         config["start_date"] = "2022-01-02"
         with pytest.raises(
             ValueError,
-            match=r"Time to finish \('\{self\.tf\}'\) is less than or equal to time to start \('\{self\.ti\}'\)\.",
+            match=r"Time to finish.*is less than or equal to time to start.*",
         ):
             s = ModelInfo(
                 config=config,
