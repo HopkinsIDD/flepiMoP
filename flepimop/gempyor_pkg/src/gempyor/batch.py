049089544dafa4abe2e37d9a4f6a41588d00c397
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2024-11-14 12:01:58 -0500

    Override nodes for legacy inference on slurm

--- flepimop/gempyor_pkg/src/gempyor/batch.py
+++ flepimop/gempyor_pkg/src/gempyor/batch.py
@@ -816,6 +816,7 @@ def _submit_scenario_job(
             options["mail-type"] = "BEGIN,END"
             options["mail-user"] = kwargs["email"]
         if inference_method is None:
+            options["nodes"] = "1"
             options["array"] = f"1-{job_size.jobs}"
         _sbatch_template(
             "inference_slurm.sbatch.j2",
