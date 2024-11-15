0fa1f9b71cca8d9a4a75023bd8a3c392bb1beb31
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2024-11-15 09:23:02 -0500

    Document info/shared_cli, MemoryParamType int
    * Fleshed out documentation for the `gempyor.info/shared_cli` modules.
    * Added the ability to optionally return the converted memory as an int
      instead of as a float to `MemoryParamType`.
    * Removed pre-instantiated custom click param types.

--- flepimop/gempyor_pkg/src/gempyor/batch.py
+++ flepimop/gempyor_pkg/src/gempyor/batch.py
@@ -31,11 +31,11 @@ from .info import get_cluster_info
 from .logging import get_script_logger
 from .utils import _format_cli_options, _git_checkout, _git_head, _shutil_which, config
 from .shared_cli import (
-    MEMORY_MB,
-    NONNEGATIVE_DURATION,
+    DurationParamType,
+    MemoryParamType,
     cli,
-    config_files_argument,
     config_file_options,
+    config_files_argument,
     log_cli_inputs,
     mock_context,
     parse_config_files,
@@ -903,13 +903,13 @@ def _submit_scenario_job(
         ),
         click.Option(
             param_decls=["--simulation-time", "simulation_time"],
-            type=NONNEGATIVE_DURATION,
+            type=DurationParamType(True),
             default="3min",
             help="The time limit per a simulation.",
         ),
         click.Option(
             param_decls=["--initial-time", "initial_time"],
-            type=NONNEGATIVE_DURATION,
+            type=DurationParamType(True),
             default="20min",
             help="The initialization time limit.",
         ),
@@ -971,7 +971,7 @@ def _submit_scenario_job(
         ),
         click.Option(
             param_decls=["--memory", "memory"],
-            type=MEMORY_MB,
+            type=MemoryParamType("mb", as_int=True),
             default=None,
             help="Override for the amount of memory per node to use in MB.",
         ),
@@ -1130,19 +1130,12 @@ def _click_submit(ctx: click.Context = mock_context, **kwargs: Any) -> None:
     logger.info("Setting a total job time limit of %s minutes", job_time_limit.format())
 
     # Job resources
-    memory = None if kwargs["memory"] is None else math.ceil(kwargs["memory"])
-    if memory != kwargs["memory"]:
-        logger.warning(
-            "The requested memory of %.3fMB has been rounded up to %uMB for submission",
-            kwargs["memory"],
-            memory,
-        )
     job_resources = JobResources.from_presets(
         job_size,
         inference_method,
         nodes=kwargs["nodes"],
         cpus=kwargs["cpus"],
-        memory=memory,
+        memory=kwargs["memory"],
     )
     logger.info("Requesting the resources %s for this job.", job_resources)
 
