45f8406fbbdf27ed8dfdf8e96a1757adac34b029
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-01-08 11:19:48 -0500

    Document `gempyor.info` module
    Fleshed out documentation for the `gempyor.info` module. Including
    module level docs and docstrings for the individual classes. Fleshed out
    examples.

--- flepimop/gempyor_pkg/src/gempyor/info.py
+++ flepimop/gempyor_pkg/src/gempyor/info.py
@@ -1,3 +1,31 @@
+"""
+Retrieving static information from developer managed yaml files.
+
+Currently, it includes utilities for handling cluster-specific information, but it can 
+be extended to other categories as needed.
+
+Classes:
+    Module: Represents a software module with a name and optional version.
+    PathExport: Represents a path export with a path, prepend flag, and error handling.
+    Cluster: Represents a cluster with a name, list of modules, and list of path 
+        exports.
+
+Functions:
+    get_cluster_info: Retrieves cluster-specific information.
+
+Examples:
+    >>> from pprint import pprint
+    >>> from gempyor.info import get_cluster_info
+    >>> cluster_info = get_cluster_info("longleaf")
+    >>> cluster_info.name
+    'longleaf'
+    >>> pprint(cluster_info.modules)
+    [Module(name='gcc', version='9.1.0'),
+    Module(name='anaconda', version='2023.03'),
+    Module(name='git', version=None),
+    Module(name='aws', version=None)]
+"""
+
 __all__ = ["Cluster", "Module", "PathExport", "get_cluster_info"]
 
 
@@ -12,23 +40,55 @@ import yaml
 
 
 class Module(BaseModel):
+    """
+    A model representing a module to load.
+
+    Attributes:
+        name: The name of the module to load.
+        version: The specific version of the module to load if there is one.
+
+    See Also:
+        [Lmod](https://lmod.readthedocs.io/en/latest/)
+    """
+
     name: str
     version: str | None = None
 
 
 class PathExport(BaseModel):
+    """
+    A model representing the export path configuration.
+
+    Attributes:
+        path: The file system path of the path to add to the `$PATH` environment
+            variable.
+        prepend: A flag indicating whether to prepend additional information to the
+            `$PATH` environment variable.
+        error_if_missing: A flag indicating whether to raise an error if the path is
+            missing.
+    """
+
     path: Path
     prepend: bool = True
     error_if_missing: bool = False
 
 
 class Cluster(BaseModel):
+    """
+    A model representing a cluster configuration.
+
+    Attributes:
+        name: The name of the cluster.
+        modules: A list of modules associated with the cluster.
+        path_exports: A list of path exports for the cluster.
+    """
+
     name: str
     modules: list[Module] = []
     path_exports: list[PathExport] = []
 
 
-T = TypeVar("T", bound=BaseModel)
+_BASE_MODEL_TYPE = TypeVar("T", bound=BaseModel)
 
 
 _CLUSTER_FQDN_REGEXES: tuple[tuple[str, Pattern], ...] = (
@@ -38,8 +98,8 @@ _CLUSTER_FQDN_REGEXES: tuple[tuple[str, Pattern], ...] = (
 
 
 def _get_info(
-    category: str, name: str, model: type[T], flepi_path: os.PathLike | None
-) -> T:
+    category: str, name: str, model: type[_BASE_MODEL_TYPE], flepi_path: os.PathLike | None
+) -> _BASE_MODEL_TYPE:
     """
     Get and parse an information yaml file.
 
@@ -79,8 +139,14 @@ def get_cluster_info(name: str | None, flepi_path: os.PathLike | None = None) ->
         flepi_path: Either a path like determine the directory to look for the info
             directory in or `None` to use the `FLEPI_PATH` environment variable.
 
-    Returns
+    Returns:
         An object containing the information about the `name` cluster.
+
+    Examples:
+        >>> from gempyor.info import get_cluster_info
+        >>> cluster_info = get_cluster_info("longleaf")
+        >>> cluster_info.name
+        'longleaf'
     """
     name = _infer_cluster_from_fqdn() if name is None else name
     return _get_info("cluster", name, Cluster, flepi_path)
