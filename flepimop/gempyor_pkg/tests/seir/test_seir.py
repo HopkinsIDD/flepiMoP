7951d119e1ec69bacd78139fd5d4ad763b1249fe
Emily Przykucki <emily.przykucki@gmail.com>
2025-01-17 13:27:02 -0500

    Regex update

--- flepimop/gempyor_pkg/tests/seir/test_seir.py
+++ flepimop/gempyor_pkg/tests/seir/test_seir.py
@@ -72,7 +72,7 @@ def test_check_parameter_positivity():
 
     with pytest.raises(
         ValueError,
-        match=(rf"There are negative parameter errors in subpops \{test_2_neg_subpops}\.*"),
+        match=(rf"There are negative parameter errors in subpops \\{test_2_neg_subpops}\\.*"),
     ):
         seir.check_parameter_positivity(
             test_array2, parameter_names, dates, subpop_names
@@ -98,7 +98,7 @@ def test_check_parameter_positivity():
 
     with pytest.raises(
         ValueError,
-        match=(rf"There are negative parameter errors in subpops \{test_3_neg_subpops}\.*"),
+        match=(rf"There are negative parameter errors in subpops \\{test_3_neg_subpops}\\.*"),
     ):
         seir.check_parameter_positivity(
             test_array3, parameter_names, dates, subpop_names
