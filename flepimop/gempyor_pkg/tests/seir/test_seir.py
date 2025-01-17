44f3fc21933a633e228f9dfa7856a56ee49ea179
Emily Przykucki <emily.przykucki@gmail.com>
2025-01-17 14:24:20 -0500

    Fixing data type error

--- flepimop/gempyor_pkg/tests/seir/test_seir.py
+++ flepimop/gempyor_pkg/tests/seir/test_seir.py
@@ -68,7 +68,7 @@ def test_check_parameter_positivity():
     test_2_negative_index_parameters = np.argwhere(test_array2 < 0)
     test_2_neg_params = []
     test_2_neg_subpops = []
-    test_2_first_neg_date = dates[0].date
+    test_2_first_neg_date = dates[0].date()
     for param_idx, _, sp_idx in test_2_negative_index_parameters:
         test_2_neg_subpops.append(subpop_names[sp_idx])
         test_2_neg_params.append(parameter_names[param_idx])
@@ -99,7 +99,7 @@ def test_check_parameter_positivity():
     test_3_negative_index_parameters = np.argwhere(test_array2 < 0)
     test_3_neg_params = []
     test_3_neg_subpops = []
-    test_3_first_neg_date = dates[0].date
+    test_3_first_neg_date = dates[0].date()
     for param_idx, _, sp_idx in test_3_negative_index_parameters:
         test_3_neg_subpops.append(subpop_names[sp_idx])
         test_3_neg_params.append(parameter_names[param_idx])
