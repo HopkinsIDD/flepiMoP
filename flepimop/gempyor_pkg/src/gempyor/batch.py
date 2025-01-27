933ac63a16d9d2680faae6f6172571e75384958b
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-01-27 13:58:31 -0500

    Add `Self` shim for python 3.10

--- flepimop/gempyor_pkg/src/gempyor/batch.py
+++ flepimop/gempyor_pkg/src/gempyor/batch.py
@@ -12,7 +12,14 @@ from dataclasses import dataclass
 from datetime import timedelta
 from enum import Enum, auto
 import math
-from typing import Literal, Self
+import sys
+from typing import Any, Literal
+
+
+if sys.version_info >= (3, 11):
+    from typing import Self
+else:
+    Self = Any
 
 
 class BatchSystem(Enum):
