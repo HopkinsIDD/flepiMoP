502ac885218ca7775bdc2fef4c8840a5867586ea
Carl A. B. Pearson <carl.ab.pearson@gmail.com>
2025-02-04 09:22:39 -0500

    register flepisync

--- flepimop/gempyor_pkg/src/gempyor/cli.py
+++ flepimop/gempyor_pkg/src/gempyor/cli.py
@@ -13,6 +13,7 @@ from .utils import _dump_formatted_yaml, config
 # register the commands from the other modules
 from . import compartments, simulate
 from .batch._cli import *
+from . import compartments, simulate, flepisync
 from .NPI import base
 
 # Guidance for extending the CLI:
