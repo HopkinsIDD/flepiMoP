6cf81e011c333798ccdc94d2b2d5a3a3e4b22b89
Carl A. B. Pearson <carl.ab.pearson@gmail.com>
2025-02-13 15:48:49 -0500

    WIP

--- flepimop/gempyor_pkg/src/gempyor/sync/sync.py
+++ flepimop/gempyor_pkg/src/gempyor/sync/sync.py
@@ -1,9 +1,5 @@
-import os
-from abc import ABC, abstractmethod
-from typing import Literal, Annotated, Union, Dict
-from pathlib import Path
-from subprocess import run, CompletedProcess
 
+import yaml
 import click
 
 from ..shared_cli import (
@@ -15,27 +11,39 @@ from ..shared_cli import (
 from ..utils import config
 from ..file_paths import create_file_name_for_push
 
+from ._sync import sync_from_yaml
+
 sync_options = {
     "protocol": click.Option(
-        ["-p", "--protocol"],
+        ["-p", "--protocol", "protocol_name"],
         type=click.STRING,
         help="sync protocol to use from configuration file",
     ),
     "source": click.Option(
-        ["-s", "--source"],
+        ["-s", "--source", "source_override"],
         type=click.Path(),
         help="source directory to 'push' changes from",
     ),
     "target": click.Option(
-        ["-t", "--target"],
+        ["-t", "--target", "target_override"],
         type=click.Path(),
         help="target directory to 'push' changes to",
     ),
     "filter": click.Option(
-        ["-f", "--filter"],
+        ["-f", "--filter", "filter_override"],
         type=click.STRING,
         multiple=True,
         help="filter to apply to files; see `man rsync` for details",
+    ),
+    "reverse": click.Option(
+        ["--reverse"],
+        is_flag=True,
+        help="reverse the source and target directories",
+    ),
+    "dryrun": click.Option(
+        ["-n", "--dry-run"],
+        is_flag=True,
+        help="perform a dry run of the sync operation",
     ),
 }
 
@@ -47,7 +55,11 @@ sync_options = {
 @click.pass_context
 def sync(ctx: click.Context = mock_context, **kwargs) -> int:
     """Sync flepimop files between local and remote locations."""
+
     config_files = kwargs.pop("config_files")
     if not config_files:
         ctx.fail("No configuration files provided." + "\n" + ctx.get_help())
-    print("invoking bare sync -- assuming default value from config file")
+    else:
+        with syncdef := sync_from_yaml(config_files):
+            res = syncdef.sync(**kwargs)
+        return res.returncode
