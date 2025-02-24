649fdd572f13329a0c207bf8a42ae3dee9e1775d
Carl A. B. Pearson <carl.ab.pearson@gmail.com>
2025-02-24 16:38:04 -0500

    WIP testing evolution

--- flepimop/gempyor_pkg/tests/sync/test__sync_filter.py
+++ flepimop/gempyor_pkg/tests/sync/test__sync_filter.py
@@ -3,18 +3,16 @@ from typing import Literal
 import pytest
 from pydantic import ValidationError, TypeAdapter
 
-from gempyor.sync._sync_filter import *
+from gempyor.sync._sync_filter import WithFilters, FilterParts
 
-# include vs exclude filters successfully parsed, including default include
-
-# convert convert single filters to list
-
-# handle single include filter expanding to add an exclude all
+class MockFilters(WithFilters):
+    def _formatter(self, f: FilterParts) -> list[str]:
+        return [f[0], f[1]]
 
 # valid vs invalid filters successfully parsed
 @pytest.mark.parametrize(
     "filter", [
-        "somefilter",
+        ["somefilter"],
         ["somefilter", "another"],
         [],
     ],
@@ -24,15 +22,15 @@ def test_valid_filters(filter: list[str] | str):
     A string that doesn't start with a space or a plus/minus without a
     subsequent space is a valid filter
     """
-    _ = TypeAdapter(ListSyncFilter).validate_python(filter)
+    _ = MockFilters(filters = filter)
 
 @pytest.mark.parametrize(
-    "filter", (
+    "filter", [
         " somefilter",
         "-something",
         "+something",
         1,
-    ),
+    ],
 )
 def test_invalid_filters(filter: list[str] | str):
     """
@@ -40,4 +38,40 @@ def test_invalid_filters(filter: list[str] | str):
     is an invalid filter
     """
     with pytest.raises(ValidationError):
-        _ = TypeAdapter(ListSyncFilter).validate_python(filter)
+        _ = MockFilters(filters = filter)
+
+# ensure automatic list promotion
+@pytest.mark.parametrize(
+    "filter", [
+        "somefilter",
+    ],
+)
+def test_valid_filters(filter: list[str] | str):
+    """
+    A string that doesn't start with a space or a plus/minus without a
+    subsequent space is a valid filter
+    """
+    mf = MockFilters(filters = filter)
+    assert mf.filters.__len__() == 1
+
+# include vs exclude filters successfully parsed, including default include, including mixtures
+@pytest.mark.parametrize(
+    "filter,mode", [
+        (["somefilter"], ["+"]),
+        (["somefilter", "- another"], ["+", "-"]),
+        (["- somefilter", "+ another"], ["-", "+"]),
+    ],
+)
+def test_filter_modes(filter: list[str] | str, mode: list[Literal["+", "-"]]):
+    """
+    a string that starts with a space or a plus/minus without a subsequent space
+    is an invalid filter
+    """
+    obj = MockFilters(filters = filter)
+    lfs = obj.list_filters()
+    for i, m in enumerate(mode):
+        assert lfs[i][0] == m
+
+# convert convert single filters to list
+
+# handle single include filter expanding to add an exclude all
