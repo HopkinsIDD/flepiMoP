135435ffa9f16b5580716cec1287303c54a22edc
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-02-25 12:02:21 -0500

    Unit tests and docs for `_pydantic_ext`
    Added unit tests for
    `gempyor._pydantic_ext._ensure_list/_overrride_or_val` as well as added
    documentation.

--- flepimop/gempyor_pkg/src/gempyor/_pydantic_ext.py
+++ flepimop/gempyor_pkg/src/gempyor/_pydantic_ext.py
@@ -1,22 +1,82 @@
-from typing import Any
+"""
+Gempyor specific Pydantic extensions.
 
-__all__ = ["_ensure_list", "_override_or_val"]
+This module contains functions that are useful for working with and creating Pydantic
+models.
+"""
+
+__all__ = ()
 
 
-# once 3.12, use type parametrization
-# def _ensure_list[T](value: T | list[T] | tuple) -> list[T]:
-def _ensure_list(value: Any) -> list | None:
+from typing import TypeVar, overload
+
+
+T = TypeVar("T")
+U = TypeVar("U")
+
+
+def _ensure_list(value: list[T] | tuple[T] | T | None) -> list[T] | None:
+    """
+    Ensure that a list, tuple, or single value is returned as a list.
+    
+    Args:
+        value: A value to ensure is a list.
+    
+    Returns:
+        A list of the value(s), if the `value` is not None.
+
+    Examples:
+        >>> from gempyor._pydantic_ext import _ensure_list
+        >>> _ensure_list(None) is None
+        True
+        >>> _ensure_list("abc")
+        ['abc']
+        >>> _ensure_list(123)
+        [123]
+        >>> _ensure_list([True, False, True])
+        [True, False, True]
+        >>> _ensure_list((True, False, True))
+        [True, False, True]
+        >>> _ensure_list((1, 2, 3, 4))
+        [1, 2, 3, 4]
+    """
     if value is None:
         return None
-    elif isinstance(value, list):
+    if isinstance(value, list):
         return value
-    elif isinstance(value, tuple):
+    if isinstance(value, tuple):
         return list(value)
-    else:
-        return [value]
+    return [value]
 
 
-# once 3.12, use type parametrization
-# def _override_or_val[T](override : T | None, value : T) -> T:
-def _override_or_val(override: Any, value: Any) -> Any:
+@overload
+def _override_or_val(override: T, value: U) -> T: ...
+
+
+@overload
+def _override_or_val(override: None, value: U) -> U: ...
+
+
+def _override_or_val(override: T | None, value: U) -> T | U:
+    """
+    Return the override value if it is not None, otherwise return the value.
+    
+    Args:
+        override: Optional override value.
+        value: The value to return if the override is None.
+    
+    Returns:
+        The `override` value if it is not None, otherwise `value`.
+    
+    Examples:
+        >>> from gempyor._pydantic_ext import _override_or_val
+        >>> _override_or_val(None, 1)
+        1
+        >>> _override_or_val(None, "abc")
+        'abc'
+        >>> _override_or_val(1, "abc")
+        1
+        >>> _override_or_val("", "foo")
+        ''
+    """
     return value if override is None else override
