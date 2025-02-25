b686f20ce9b5d6659f6a87e919bb6751d384a3c8
Carl A. B. Pearson <carl.ab.pearson@gmail.com>
2025-02-25 10:55:15 -0500

    WIP testing; setup items for test_syncoptions

---
+++ flepimop/gempyor_pkg/tests/sync/test_syncoptions.py
@@ -0,0 +1,29 @@
+from typing import Literal
+
+import pytest
+from pydantic import ValidationError
+
+from gempyor.sync._sync import SyncOptions, SyncProtocols
+from gempyor._pydantic_ext import _ensure_list
+
+# test filter validation / construction for overrides, prefixes, and suffixes (see test__sync_filter.py)
+
+# ensure that None vs [] is handled correctly for override
+
+# ensure that all the other options properly bind / fail when given garbage
+
+@pytest.mark.parametrize(
+    "opts",
+    [
+        {"filter_override": "somefilter"},
+        {"filter_override": ["somefilter", "another"]},
+        {"filter_override": []},
+    ],
+)
+def test_sync_opts_filters(opts: dict):
+    """
+    Ensures SyncOptions can instantiate valid objects w.r.t to filters
+    """
+    sp = SyncOptions(**opts)
+    assert sp.filter_override == _ensure_list(opts["filter_override"])
+
