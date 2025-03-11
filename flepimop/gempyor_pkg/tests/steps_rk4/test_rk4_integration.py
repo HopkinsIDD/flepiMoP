143a175605ff92f2558a653f7f4c97418dab7a33
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-03-11 11:43:12 -0400

    Add `rk4_integration` test for stochastic sim
    Added a unit test for `gempyor.steps_rk4.rk4_integration` to demonstrate
    that stochastic simulations fail with legacy integration method despite
    what documentation advertises.

---
+++ flepimop/gempyor_pkg/tests/steps_rk4/test_rk4_integration.py
@@ -0,0 +1,47 @@
+"""Unit tests for `gempyor.steps_rk4.rk4_integration`."""
+
+import numba as nb
+import numpy as np
+
+from gempyor.steps_rk4 import rk4_integration
+
+
+def test_stochastic_simulation_works_with_legacy_integration_method() -> None:
+    """Test that the stochastic simulation works with the legacy integration method."""
+    # These inputs are modeled after those produced by `config_sample_2pop.yml`.
+    seeding_data = nb.typed.Dict.empty(
+        key_type=nb.types.unicode_type,
+        value_type=nb.types.int64[:],
+    )
+    seeding_data["seeding_sources"] = np.zeros(0, dtype=np.int64)
+    seeding_data["seeding_destinations"] = np.zeros(0, dtype=np.int64)
+    seeding_data["seeding_subpops"] = np.zeros(0, dtype=np.int64)
+    seeding_data["day_start_idx"] = np.zeros(214, dtype=np.int64)
+    result = rk4_integration(
+        ncompartments=4,
+        nspatial_nodes=2,
+        ndays=213,
+        parameters=np.ones((4, 213, 2)),
+        dt=0.25,
+        transitions=np.array([[0, 1, 2], [1, 2, 3], [1, 2, 3], [0, 2, 3], [2, 3, 4]]),
+        proportion_info=np.array([[0, 1, 2, 3], [1, 2, 3, 4], [0, 0, 0, 0]]),
+        transition_sum_compartments=np.array([0, 2, 1, 2]),
+        initial_conditions=np.array(
+            [
+                [8.995e03, 1.000e03],
+                [5.000e00, 0.000e00],
+                [0.000e00, 0.000e00],
+                [0.000e00, 0.000e00],
+            ]
+        ),
+        seeding_data=seeding_data,
+        seeding_amounts=np.array([], dtype=np.float64),
+        mobility_data=np.array([100.0, 20.0]),
+        mobility_row_indices=np.array([1, 0], dtype=np.int32),
+        mobility_data_indices=np.array([0, 1, 2], dtype=np.int32),
+        population=np.array([9000, 1000]),
+        stochastic_p=True,
+        method="legacy",
+        silent=True,
+    )
+    assert isinstance(result, tuple)
