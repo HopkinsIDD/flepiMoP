4d2a46c19afc7086477d5b9cda1b72af0d55b50f
Carl A. B. Pearson <pearsonca@users.noreply.github.com>
2025-03-24 18:07:37 -0400

    Update flepimop/gempyor_pkg/src/gempyor/shared_cli.py
    Co-authored-by: Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>

--- flepimop/gempyor_pkg/src/gempyor/shared_cli.py
+++ flepimop/gempyor_pkg/src/gempyor/shared_cli.py
@@ -333,7 +333,7 @@ def log_cli_inputs(kwargs: dict[str, Any], verbosity: int | None = None) -> None
         >>> log_cli_inputs(kwargs)
         2024-11-05 09:29:21,666:DEBUG:gempyor.shared_cli> CLI was given 4 arguments:
         2024-11-05 09:29:21,667:DEBUG:gempyor.shared_cli> input_file = /Users/twillard/Desktop/GitHub/HopkinsIDD/flepiMoP/flepimop/gempyor_pkg/config.in.
-        2024-11-05 09:29:21,667:DEBUG:gempyor.shared_cli> stochastic = stochastic.
+        2024-11-05 09:29:21,667:DEBUG:gempyor.shared_cli> method = stochastic.
         2024-11-05 09:29:21,668:DEBUG:gempyor.shared_cli> cluster    = longleaf.
         2024-11-05 09:29:21,668:DEBUG:gempyor.shared_cli> verbosity  = 3.
     """
