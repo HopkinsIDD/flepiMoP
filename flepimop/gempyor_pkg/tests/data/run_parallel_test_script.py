f9c1420986aaecbd57775246c47527c2f7570028
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-04-09 14:43:10 -0400

    Add documentation to parallel aux script
    Added documentation to the `run_parallel_test_script.py` parallel
    auxillary testing script.

--- flepimop/gempyor_pkg/tests/data/run_parallel_test_script.py
+++ flepimop/gempyor_pkg/tests/data/run_parallel_test_script.py
@@ -1,3 +1,37 @@
+"""
+Auxiliary runner for pared down version of `gempyor.simulate.simulate` in a new process.
+
+This script is used to test the parallelization of the `run_parallel_SEIR` and
+`run_parallel_outcomes` functions by multiprocessing start method. The challenge is that
+the start method used by multiprocessing has an impact on the random sampling in these
+two functions but setting the start method via `multiprocessing.set_start_method` can
+only be done once per a process. To get around this limitation when testing, this script
+is used to run a pared down version of `gempyor.simulate.simulate` in a new process with
+the desired start method. The script takes the following arguments:
+
+1. `setup_sample_2pop_vaccine_scenarios`: Path to the setup directory.
+2. `start_method`: The start method to use for multiprocessing (e.g., 'spawn', 'fork').
+3. `n_jobs`: Number of jobs to run in parallel.
+4. `do_outcomes`: Whether to run outcomes or not (True/False).
+
+The script sets the working directory to the setup directory, sets the start method for
+multiprocessing, and then calls the `main` function with the provided arguments.
+The `main` function does the following:
+
+1. Parses the configuration files in the setup directory.
+2. Creates a `ModelInfo` object with the parsed configuration.
+3. Calls the `run_parallel_SEIR` function with the `ModelInfo` object and the number of
+   jobs.
+4. If `do_outcomes` is True, calls the `run_parallel_outcomes` function with the
+   `ModelInfo` object, the number of jobs, and the simulation index to write.
+5. The script is designed to be run as a standalone script, and the `main` function is
+   called only if the script is run directly. This allows for easy testing and
+   debugging of the script without having to run the entire `gempyor.simulate.simulate`
+   function.
+
+This script is part of the `gempyor` package and is used for testing purposes only.
+"""
+
 import multiprocessing as mp
 import os
 from pathlib import Path
@@ -9,7 +43,19 @@ from gempyor.seir import run_parallel_SEIR
 from gempyor.shared_cli import parse_config_files
 
 
-def main(setup_sample_2pop_vaccine_scenarios, n_jobs, do_outcomes):
+def main(setup_sample_2pop_vaccine_scenarios: Path, n_jobs: int, do_outcomes: bool) -> None:
+    """
+    Run a pared down version of `gempyor.simulate.simulate` in a new process.
+
+    This function will do as minimal setup as required to call the
+    `gempyor.seir.run_parallel_SEIR` function and, optionally, the
+    `gempyor.outcomes.run_parallel_outcomes` function.
+
+    Args:
+        setup_sample_2pop_vaccine_scenarios: Path to the setup directory.
+        n_jobs: Number of jobs to run in parallel.
+        do_outcomes: Whether to run outcomes or not.
+    """
     cfg = parse_config_files(
         config_filepath=setup_sample_2pop_vaccine_scenarios
         / "config_sample_2pop_vaccine_scenarios.yml",
@@ -58,6 +104,13 @@ def main(setup_sample_2pop_vaccine_scenarios, n_jobs, do_outcomes):
 
 
 if __name__ == "__main__":
+    """
+    USAGE: run_parallel_test_script.py <setup_sample_2pop_vaccine_scenarios> <start_method> <n_jobs> <do_outcomes>
+    <setup_sample_2pop_vaccine_scenarios>: Path to the setup directory.
+    <start_method>: The start method to use for multiprocessing (e.g., 'spawn', 'fork').
+    <n_jobs>: Number of jobs to run in parallel.
+    <do_outcomes>: Whether to run outcomes or not (True/False).
+    """
     setup_sample_2pop_vaccine_scenarios = Path(sys.argv[1])
     start_method = sys.argv[2]
     n_jobs = int(sys.argv[3])
