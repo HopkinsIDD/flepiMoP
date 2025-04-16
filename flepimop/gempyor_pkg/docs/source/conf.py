493c615b15900d77af5b5af629519a5663a60e06
Emily Przykucki <emily.przykucki@gmail.com>
2025-04-16 10:47:48 -0400

    Correcting `sphinx` documentation generation

--- flepimop/gempyor_pkg/docs/source/conf.py
+++ flepimop/gempyor_pkg/docs/source/conf.py
@@ -8,30 +8,29 @@
 
 import os
 import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../src')))
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../src")))
-
-project = "gempyor"
-copyright = "2025, ACCIDDA"
-author = "ACCIDDA"
+project = 'gempyor'
+copyright = '2025, ACCIDDA'
+author = 'ACCIDDA'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "sphinx.ext.apidoc",
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "sphinx.ext.autosummary",
 ]
 
-templates_path = ["_templates"]
+templates_path = ['_templates']
 exclude_patterns = []
+
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
-html_static_path = ["_static"]
+html_theme = 'alabaster'
+html_static_path = ['_static']
--- flepimop/gempyor_pkg/docs/source/index.rst
+++ flepimop/gempyor_pkg/docs/source/index.rst
@@ -3,30 +3,24 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to `gempyor`'s documentation!
-=====================================
-
-`gempyor` is the core python package for `flepiMoP`.
-
-Modules
 =======
+Welcome to gempyor's documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 4
+   :caption: Contents:
 
 .. rubric:: Modules
 
 .. autosummary::
    :toctree: generated
 
-   gempyor
+   gempyor.compartments
+   gempyor.model_info
+   gempyor.inference
+   gempyor.parameters
 
-
-API Reference
-=============
-
-.. toctree::
-   :maxdepth: 4
-   :caption: Contents:
-
-   api/gempyor
 
 Indices and tables
 ==================
