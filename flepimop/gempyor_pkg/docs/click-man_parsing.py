354784a3cab00cfdfae931c073a6f80a68094408
Emily Przykucki <emily.przykucki@gmail.com>
2025-04-21 16:38:15 -0400

    new CLI documentation framework with `click-man`

---
+++ flepimop/gempyor_pkg/docs/click-man_parsing.py
@@ -0,0 +1,37 @@
+"""
+A script to iteratively convert flepiMoP CLI man pages into markdown files.
+"""
+
+import subprocess
+from pathlib import Path
+
+
+def main():
+    """
+    Main execution function.
+    """
+
+    # Path to find man pages, path to output files
+    script_dir = Path(__file__).resolve().parent
+    folder_path = script_dir / 'cli_man'
+    output_dir = folder_path / 'markdown-files'
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    for file in folder_path.rglob('*.1'):
+        if file.is_file():
+            # Create file name
+            output_md = output_dir / (file.stem + '.md')
+        
+            # Construct pandoc command and run
+            command = [
+                'pandoc',
+                str(file),
+                '-f', 'man',
+                '-t', 'markdown',
+                '-o', str(output_md)
+            ]
+            subprocess.run(command, check=True)
+
+
+if __name__ == "__main__":
+    main()
