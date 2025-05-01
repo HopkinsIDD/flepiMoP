c6e0ee11d3dfde7b8875f6ef1c85e777be328489
Emily Przykucki <emily.przykucki@gmail.com>
2025-05-01 17:27:32 -0400

    Update shared_cli.py

--- flepimop/gempyor_pkg/src/gempyor/shared_cli.py
+++ flepimop/gempyor_pkg/src/gempyor/shared_cli.py
@@ -242,13 +242,7 @@ def parse_config_files(
     # (only if they are identical)
     cfp1 = kwargs.get("config_files")
     cfp2 = kwargs.get("config_filepath")
-    if (
-        cfp1
-        and cfp2
-        and isinstance(cfp1, (list, tuple))
-        and isinstance(cfp2, (list, tuple))
-        and set(cfp1) == set(cfp2)
-    ):
+    if cfp1 and cfp2 and set(cfp1) == set(cfp2):
         del kwargs["config_filepath"]  # delete option-set config path
 
     # initialize the config, including handling missing / double-specified config files
