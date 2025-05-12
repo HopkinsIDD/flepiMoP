696a5410d928ed8a5065c8f0a1a257ef299cde5a
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-05-12 15:25:07 -0400

    Extract rsync ensure path util, string paths
    * Extracted `RsyncModel._ensure_path` to standalone `_rsync_ensure_path`
      for unit testing,
    * Added unit tests for `_rsync_ensure_path` and `_RSYNC_HOST_REGEX`,
    * Changed paths for `SyncOptions` to be strings to accommodate a range
      of input options along with corresponding changes.

--- flepimop/gempyor_pkg/tests/sync/_sync/test_sync_options_class.py
+++ flepimop/gempyor_pkg/tests/sync/_sync/test_sync_options_class.py
@@ -1,23 +1,19 @@
 """Unit tests for the `gempyor.sync._sync.SyncOptions` class."""
 
-from pathlib import Path
-
 import pytest
 
 from gempyor.sync._sync import SyncOptions
 
 
-@pytest.mark.parametrize(
-    "original", [Path("/a/b/c"), Path("/foo/bar"), Path("s3://fizz/buzz")]
-)
-@pytest.mark.parametrize("override", [None, Path("/x/y/z"), Path("s3://bizz/bazz")])
-@pytest.mark.parametrize("append", [None, Path("g/h/i"), Path("new")])
+@pytest.mark.parametrize("original", ["/a/b/c", "/foo/bar", "s3://fizz/buzz"])
+@pytest.mark.parametrize("override", [None, "/x/y/z", "s3://bizz/bazz"])
+@pytest.mark.parametrize("append", [None, "g/h/i", "new"])
 def test__true_path_output_validation(
-    original: Path, override: Path | None, append: Path | None
+    original: str, override: str | None, append: str | None
 ) -> None:
     """Test the `_true_path` method of the `SyncOptions` class."""
-    path = SyncOptions._true_path(original, override, append)
-    assert isinstance(path, Path)
+    path = SyncOptions()._true_path(original, override, append)
+    assert isinstance(path, str)
     assert str(path).startswith(str(original) if override is None else str(override))
     if append is not None:
         assert str(path).endswith(str(append))
