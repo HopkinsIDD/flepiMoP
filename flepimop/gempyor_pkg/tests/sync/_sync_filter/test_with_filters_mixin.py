12142a7f9f7790d7c025d6f8389172cbcbbb2d99
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-05-21 16:22:50 -0400

    Incorporate substring filter mode into tests

--- flepimop/gempyor_pkg/tests/sync/_sync_filter/test_with_filters_mixin.py
+++ flepimop/gempyor_pkg/tests/sync/_sync_filter/test_with_filters_mixin.py
@@ -15,7 +15,8 @@ class ExampleWithFilters(WithFilters):
 
 
 @pytest.mark.parametrize(
-    "filters", [[], ["- /foo/bar.txt"], ["+ /fizz/buzz.txt"], ["/a/b/c.txt"]]
+    "filters",
+    [[], ["- /foo/bar.txt"], ["+ /fizz/buzz.txt"], ["/a/b/c.txt"], ["s /x/y/z.txt"]],
 )
 @pytest.mark.parametrize(
     "overrides",
@@ -26,6 +27,7 @@ class ExampleWithFilters(WithFilters):
         ["+ /x/y/z.txt"],
         ["/x/y/z.txt"],
         ["+ /qrs.txt", "- /tuv.txt"],
+        ["s /abc.txt"],
     ],
 )
 @pytest.mark.parametrize(
@@ -37,6 +39,7 @@ class ExampleWithFilters(WithFilters):
         ["+ /ghi.txt"],
         ["/jkl.txt"],
         ["+ /mno.txt", "- /pqr.txt"],
+        ["s /stu.txt"],
     ],
 )
 @pytest.mark.parametrize(
@@ -48,6 +51,7 @@ class ExampleWithFilters(WithFilters):
         ["+ /ihg.txt"],
         ["/lkj.txt"],
         ["+ /omn.txt", "- /rqp.txt"],
+        ["s /uts.txt"],
     ],
 )
 @pytest.mark.parametrize("reverse", [False, True])
@@ -68,10 +72,13 @@ def test_list_filters(
         and all(isinstance(f, tuple) for f in parsed_filters)
         and all(isinstance(p, str) for f in parsed_filters for p in f)
     )
-    any_minus = (
-        any(f.startswith("- ") for f in (filters if overrides is None else overrides))
-        or any(f.startswith("- ") for f in (prefix or []))
-        or any(f.startswith("- ") for f in (suffix or []))
+    any_minus_or_substring = (
+        any(
+            f.startswith("- ") or f.startswith("s ")
+            for f in (filters if overrides is None else overrides)
+        )
+        or any(f.startswith("- ") or f.startswith("s ") for f in (prefix or []))
+        or any(f.startswith("- ") or f.startswith("s ") for f in (suffix or []))
     )
     parsed_count = sum(
         (
@@ -80,14 +87,16 @@ def test_list_filters(
             len(suffix or []),
         )
     )
-    assert len(parsed_filters) == parsed_count + int(not any_minus) * min(parsed_count, 1)
+    assert len(parsed_filters) == parsed_count + int(not any_minus_or_substring) * min(
+        parsed_count, 1
+    )
     if reverse:
         # It's easier to undo reverse and assert than conditionally assert
         parsed_filters.reverse()
     if prefix:
         assert [rf for _, _, rf in parsed_filters[: len(prefix)]] == prefix
     if suffix:
-        if any_minus:
+        if any_minus_or_substring:
             assert [rf for _, _, rf in parsed_filters[-len(suffix) :]] == suffix
         else:
             assert [rf for _, _, rf in parsed_filters[-(len(suffix) + 1) : -1]] == suffix
