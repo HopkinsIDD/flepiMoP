e17e3a0125f79f11968a0fffe4f19ded17aa9ed7
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-06-04 09:56:50 -0400

    Address pylint for `gempyor.batch._submit`
    Consolidated duplicate args into the template data.

--- flepimop/gempyor_pkg/src/gempyor/batch/_submit.py
+++ flepimop/gempyor_pkg/src/gempyor/batch/_submit.py
@@ -1,9 +1,11 @@
+"""Internal helpers for submitting scenario jobs to a batch system."""
+
 __all__ = ()
 
 
 from collections.abc import Iterable
 from datetime import timedelta
-from typing import Any, Literal, overload
+from typing import Any
 
 from ..logging import get_script_logger
 from ._inference import _create_inference_command
@@ -11,95 +13,51 @@ from .systems import BatchSystem
 from .types import JobResources, JobSize, JobSubmission
 
 
-@overload
 def _submit_scenario_job(
-    name: str,
-    job_name: str,
-    inference: Literal["emcee", "r"],
     job_size: JobSize,
     batch_system: BatchSystem,
-    outcome_modifiers_scenario: str,
-    seir_modifiers_scenario: str,
-    options: None,
-    template_data: dict[str, Any],
-    verbosity: int,
-    dry_run: Literal[False],
-) -> JobSubmission: ...
-
-
-@overload
-def _submit_scenario_job(
-    name: str,
-    job_name: str,
-    inference: Literal["emcee", "r"],
-    job_size: JobSize,
-    batch_system: BatchSystem,
-    outcome_modifiers_scenario: str,
-    seir_modifiers_scenario: str,
-    options: None,
-    template_data: dict[str, Any],
-    verbosity: int,
-    dry_run: Literal[True],
-) -> JobSubmission: ...
-
-
-def _submit_scenario_job(
-    name: str,
-    job_name: str,
-    inference: Literal["emcee", "r"],
-    job_size: JobSize,
-    batch_system: BatchSystem,
-    outcome_modifiers_scenario: str,
-    seir_modifiers_scenario: str,
     options: dict[str, str | Iterable[str]] | None,
     template_data: dict[str, Any],
-    verbosity: int,
-    dry_run: bool,
 ) -> JobSubmission | None:
     """
     Submit a job for a scenario.
 
     Args:
-        name: The name of the config file used as a prefix for the job name.
-        job_name: The name of the job to submit.
-        inference: The inference method to use.
         job_size: The size of the job to submit.
         batch_system: The batch system to submit the job to.
-        outcome_modifiers_scenario: The outcome modifiers scenario to use.
-        seir_modifiers_scenario: The SEIR modifiers scenario to use.
         options: The options to use for the job submission.
         template_data: The template data to use for the job submission.
-        verbosity: A integer verbosity level to enable logging or `None` for no logging.
-        dry_run: A boolean indicating if this is a dry run or not, if set to `True` this
-            function will not actually submit/run a job.
 
     Returns:
         A JobSubmission object if `dry_run` is `False`, otherwise `None`.
     """
 
     # Get logger
-    if verbosity is not None:
-        logger = get_script_logger(__name__, verbosity)
-        if outcome_modifiers_scenario == "None":
-            logger.warning(
-                "The outcome modifiers scenario is `None`, may lead to "
-                "unintended consequences in output file/directory names."
-            )
-        if seir_modifiers_scenario == "None":
-            logger.warning(
-                "The SEIR modifiers scenario is `None`, may lead to "
-                "unintended consequences in output file/directory names."
-            )
+    logger = get_script_logger(__name__, template_data["verbosity"])
+    if template_data["outcome_modifiers_scenario"] == "None":
+        logger.warning(
+            "The outcome modifiers scenario is `None`, may lead to "
+            "unintended consequences in output file/directory names."
+        )
+    if template_data["seir_modifiers_scenario"] == "None":
+        logger.warning(
+            "The SEIR modifiers scenario is `None`, may lead to "
+            "unintended consequences in output file/directory names."
+        )
 
     # Modify the job for the given scenario info
-    job_name += f"_{seir_modifiers_scenario}_{outcome_modifiers_scenario}"
-    prefix = f"{name}_{seir_modifiers_scenario}_{outcome_modifiers_scenario}"
-    if verbosity is not None:
+    suffix = (
+        f"{template_data['seir_modifiers_scenario']}_"
+        f"{template_data['outcome_modifiers_scenario']}"
+    )
+    job_name = f"{template_data['job_name']}_{suffix}"
+    prefix = f"{template_data['name']}_{suffix}"
+    if template_data["verbosity"] is not None:
         logger.info(
             "Preparing a job for outcome and SEIR modifiers scenarios "
             "'%s' and '%s', respectively, with job name '%s'.",
-            outcome_modifiers_scenario,
-            seir_modifiers_scenario,
+            template_data["outcome_modifiers_scenario"],
+            template_data["seir_modifiers_scenario"],
             job_name,
         )
 
@@ -108,31 +66,36 @@ def _submit_scenario_job(
         **template_data,
         **{
             "prefix": prefix,
-            "outcome_modifiers_scenario": outcome_modifiers_scenario,
-            "seir_modifiers_scenario": seir_modifiers_scenario,
+            "outcome_modifiers_scenario": template_data["outcome_modifiers_scenario"],
+            "seir_modifiers_scenario": template_data["seir_modifiers_scenario"],
             "job_comment": (
-                f"{name} submitted by {template_data.get('user', 'unknown')} at "
+                f"{template_data['name']} submitted by "
+                f"{template_data.get('user', 'unknown')} at "
                 f"{template_data.get('now', 'unknown')} with outcome and SEIR "
-                f"modifiers scenarios '{outcome_modifiers_scenario}' and "
-                f"'{seir_modifiers_scenario}', respectively."
+                f"modifiers scenarios '{template_data['outcome_modifiers_scenario']}' "
+                f"and '{template_data['seir_modifiers_scenario']}', respectively."
             ),
             "job_name": job_name,
         },
     }
 
     # Get inference command
-    inference_command = _create_inference_command(
-        inference,
+    command = _create_inference_command(
+        template_data["inference_method"],
         job_size,
-        **{k: v for k, v in template_data.items() if k not in {"inference", "job_size"}},
+        **{
+            k: v
+            for k, v in template_data.items()
+            if k not in {"inference", "inference_method", "job_size"}
+        },
     )
 
     # Submit inference job
     submission = batch_system.submit_command(
-        inference_command,
+        command,
         options,
-        verbosity,
-        dry_run,
+        template_data["verbosity"],
+        template_data["dry_run"],
         **{
             k: v
             for k, v in template_data.items()
@@ -198,8 +161,8 @@ def _submit_scenario_job(
         batch_system.submit_command(
             "\n".join(command),
             options,
-            verbosity,
-            dry_run,
+            template_data["verbosity"],
+            template_data["dry_run"],
             **(
                 {
                     k: v
