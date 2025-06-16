e53784d9baadd01006f6188057365b65673c3ca8
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-06-16 11:35:22 -0400

    Draft modular initial conditions
    First pass at modular initial conditions, enabled by the
    `InitialConditionsABC` that defines the interface for an initial
    conditions module. Also implmented `DefaultInitialConditions` as an
    example of how to use said ABC.

--- flepimop/gempyor_pkg/src/gempyor/initial_conditions/__init__.py
+++ flepimop/gempyor_pkg/src/gempyor/initial_conditions/__init__.py
@@ -1,14 +1,17 @@
 """Provides functionality for handling initial conditions."""
 
 __all__ = (
+    "DefaultInitialConditions",
     "InitialConditions",
+    "InitialConditionsABC",
     "check_population",
     "initial_conditions_factory",
     "read_initial_condition_from_seir_output",
     "read_initial_condition_from_tidydataframe",
 )
 
-
+from ._base import InitialConditionsABC
+from ._default import DefaultInitialConditions
 from ._factory import initial_conditions_factory
 from ._initial_conditions import InitialConditions
 from ._utils import check_population
