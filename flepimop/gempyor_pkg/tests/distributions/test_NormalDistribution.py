65093e397b6fa71a03f7da3f229a472e0185f4d1
Emily Przykucki <emily.przykucki@gmail.com>
2025-06-20 16:16:02 -0400

    Add tests for `FixedDistribution`
    Unit tests and validation for the `FixedDistribution` class in `gmepyor_pkg/tests/distributions/`. Also removal of an unused parameter in the `FixedDistribution` `.sample()` method.

--- flepimop/gempyor_pkg/tests/distributions/test_NormalDistribution.py
+++ flepimop/gempyor_pkg/tests/distributions/test_NormalDistribution.py
@@ -1,7 +1,6 @@
 import numpy as np
-from pydantic import ValidationError
-
 import pytest
+from pydantic import ValidationError
 
 from gempyor.distributions import NormalDistribution
 
@@ -11,10 +10,6 @@ from gempyor.distributions import NormalDistribution
     "sigma", [10.0, 1.0, 0.5], ids=["sigma_ten", "sigma_one", "sigma_pointfive"]
 )
 def test_normal_distribution_init(mu: float, sigma: float) -> None:
-    """
-    Test that a  NormalDistribution object can be initialized with given parameters.
-    """
-
     dist = NormalDistribution(mu=mu, sigma=sigma)
     assert dist.mu == mu
     assert dist.sigma == sigma
@@ -27,20 +22,11 @@ def test_normal_distribution_init(mu: float, sigma: float) -> None:
 def test_normal_distribution_sample_raises_error_for_invalid_sigma(
     invalid_sigma: float,
 ) -> None:
-    """
-    Test that initializing a NormalDistribution object with an
-    invalid sigma raises a ValidationError.
-    """
-
     with pytest.raises(ValidationError, match="Input should be greater than 0") as e:
         NormalDistribution(mu=10.0, sigma=invalid_sigma)
 
 
 def test_normal_distribution_sample_rng_reproducibility() -> None:
-    """
-    Test that sampling is reproducible when a seed is given to RNG.
-    """
-
     dist = NormalDistribution(mu=10.0, sigma=1.5)
 
     rng1 = np.random.default_rng(seed=100)
@@ -55,18 +41,14 @@ def test_normal_distribution_sample_rng_reproducibility() -> None:
 @pytest.mark.parametrize(
     "size, expected_shape",
     [
-        ((3, 2), (3, 2)),  # Case 1: size is not provided, should default to a single value
-        (10, (10,)),  # Case 2: size is an integer
-        ((3, 4), (3, 4)),  # Case 3: size is a tuple
+        ((3, 2), (3, 2)),  
+        (10, (10,)),  
+        ((3, 4), (3, 4)),  
     ],
     ids=["tuple_size1", "integer_size", "tuple_size2"],
 )
 @pytest.mark.parametrize("use_rng", [True, False], ids=["with_rng", "without_rng"])
 def test_normal_distribution_sample_properties(size, expected_shape, use_rng) -> None:
-    """
-    Test that .sample() method produces a np.array of expected size wtih float64s.
-    """
-
     dist = NormalDistribution(mu=0.0, sigma=1.0)
     kwargs = {}
     kwargs["size"] = size
