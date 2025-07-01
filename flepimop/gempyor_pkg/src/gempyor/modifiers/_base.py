7fe03b53f04d735674d4ea0c5c9f6e86bae32691
Emily Przykucki <emily.przykucki@gmail.com>
2025-07-01 17:13:14 -0400

    Add `.apply_dataframe()` method to `ModifierABC`
    `.apply_dataframe()` is a wrapper around `.apply()`

--- flepimop/gempyor_pkg/src/gempyor/modifiers/_base.py
+++ flepimop/gempyor_pkg/src/gempyor/modifiers/_base.py
@@ -4,7 +4,6 @@ __all__: tuple[str, ...] = ()
 
 import numpy as np
 import pandas as pd
-import typing
 from abc import ABC, abstractmethod
 
 from pydantic import BaseModel
@@ -27,7 +26,16 @@ class ModifierABC(ABC, BaseModel):
     def apply(
         self,
         parameter: np.ndarray,
-        modification: typing.Union[pd.DataFrame, float],
+        modification: pd.DataFrame | float,
         method: str = "product",
     ) -> np.ndarray:
         pass
+
+    def apply_dataframe(
+        self,
+        parameter: np.ndarray,
+        modification: pd.DataFrame,
+        method: str = "product",
+    ) -> np.ndarray:
+        """A wrapper around the .apply() method for DataFrame modifications."""
+        return self.apply(parameter=parameter, modification=modification, method=method)
