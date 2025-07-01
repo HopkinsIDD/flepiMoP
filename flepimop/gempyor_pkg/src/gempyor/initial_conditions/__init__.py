bce2e66285675414506c0c87cbc3ac7563671229
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-07-01 15:55:20 -0400

    Replace `InitialConditions` with modular plugins
    Replaced the previous `InitialConditions` monolithic class with modular
    classes that subclass `InitialConditionsABC`. These subclasses then
    register themselves as plugins via `register_initial_conditions_plugin`.
    These are then accessible via the `initial_conditions_from_plugin`
    function.
    
    Furthermore, this has been integrated directly into the rest of
    `gempyor`. The `get_initial_conditions_data` method was added to the
    model info object to more easily access initial conditions, similar to
    how seeding is done.

--- flepimop/gempyor_pkg/src/gempyor/initial_conditions/__init__.py
+++ flepimop/gempyor_pkg/src/gempyor/initial_conditions/__init__.py
@@ -3,19 +3,18 @@
 __all__ = (
     "DefaultInitialConditions",
     "FileOrFolderDrawInitialConditions",
-    "InitialConditions",
     "InitialConditionsABC",
     "check_population",
-    "initial_conditions_factory",
+    "initial_conditions_from_plugin",
     "read_initial_condition_from_seir_output",
     "read_initial_condition_from_tidydataframe",
+    "register_initial_conditions_plugin",
 )
 
 from ._base import InitialConditionsABC
 from ._default import DefaultInitialConditions
-from ._factory import initial_conditions_factory
 from ._file_or_folder_draw import FileOrFolderDrawInitialConditions
-from ._initial_conditions import InitialConditions
+from ._plugins import initial_conditions_from_plugin, register_initial_conditions_plugin
 from ._utils import check_population
 from ._readers import (
     read_initial_condition_from_seir_output,
