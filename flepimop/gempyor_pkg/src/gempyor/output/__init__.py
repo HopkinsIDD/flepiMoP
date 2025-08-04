ce5671b475eeb9773c0e4b80e105400dc4ba31f7
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-08-04 16:47:06 -0400

    WIP: Model output abc

---
+++ flepimop/gempyor_pkg/src/gempyor/output/__init__.py
@@ -0,0 +1,6 @@
+"""Model output I/O API."""
+
+__all__ = ("Chains", "ModifierInfo", "ModifierInfoPeriod", "OutputABC")
+
+from ._base import OutputABC
+from ._types import Chains, ModifierInfo, ModifierInfoPeriod
