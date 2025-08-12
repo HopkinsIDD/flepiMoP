87030fd9015b667d3abad24d9f0a1058a7c89439
Timothy Willard <9395586+TimothyWillard@users.noreply.github.com>
2025-08-12 11:53:11 -0400

    Light refactor of `InferenceParameters` class
    * Address suggestions from `pylint` to make the code more readable as
    	well as add documentation/type hints.
    * Added initial, highly targeted unit tests.
    * Consolidated `hit_*` methods into the `check_in_bound` method.
    * Removed the duplicative `*bs` attributes for lower and upper bounds.
    
    WIP: Remove `*bs` attributes

--- flepimop/gempyor_pkg/src/gempyor/inference_parameter.py
+++ flepimop/gempyor_pkg/src/gempyor/inference_parameter.py
@@ -2,40 +2,109 @@
 Managing inference parameters in a vectorized way.
 """
 
+__all__ = ("InferenceParameters",)
+
+
+from collections import Counter
+from typing import Literal
+
 import numpy as np
 import numpy.typing as npt
+import pandas as pd
+from confuse import ConfigView
 
 from . import NPI
-from .distributions import distribution_from_confuse_config
+from .distributions import DistributionABC, distribution_from_confuse_config
 
 
 class InferenceParameters:
     """
-    A class to manage inference parameters, in a vectorized way
-
-    Parameters:
-        global_config (confuse.ConfigView): The global configuration.
-        subpop_names (list): The subpopulation names, in the right order
+    A class to manage vectorized inference parameters.
     """
 
-    def __init__(self, global_config, subpop_names):
-        self.ptypes = []
-        self.pnames = []
-        self.subpops = []
-        self.pdists = []
-        self.ubs = np.array([], dtype=np.float64)
-        self.lbs = np.array([], dtype=np.float64)
-        self.build_from_config(global_config, subpop_names)
-
-    def add_modifier(self, pname, ptype, parameter_config, subpops):
+    def __init__(self, global_config: ConfigView, subpop_names: list[str]) -> None:
         """
-        Adds a modifier parameter to the parameters list.
+        Initializes the `InferenceParameters` instance.
+
+        This constructor sets up the inference parameters based on the provided global
+        configuration and subpopulation names by calling the `build_from_config` method.
 
         Args:
-            pname (str): The parameter name.
-            ptype (str): The parameter type.
-            parameter_config (confuse.ConfigView): The configuration for the parameter.
-            subpops (list): List of subpopulations affected by the modifier.
+            global_config: The global configuration represented as a
+                `confuse.ConfigView`.
+            subpop_names: A list of subpopulation names to be used in
+                the configuration.
+        """
+        self.ptypes: list[Literal["outcome_modifiers", "seir_modifiers"]] = []
+        self.pnames: list[str] = []
+        self.subpops: list[str] = []
+        self.pdists: list[DistributionABC] = []
+        self.build_from_config(global_config, subpop_names)
+
+    def __str__(self) -> str:
+        return f"InferenceParameters: with {len(self)} parameters: \n" + "\n".join(
+            f"    {key}: {value} parameters" for key, value in Counter(self.ptypes).items()
+        )
+
+    def __len__(self) -> int:
+        return len(self.pnames)
+
+    def print_summary(self) -> None:
+        """
+        Prints a summary of the inference parameters.
+
+        Produces a summary of the parameters, including their types, names, bounds and
+        relevant subpopulations. This is useful for debugging and understanding the
+        configuration.
+        """
+        dim = len(self)
+        print(f"There are {dim} parameters in the configuration.")
+        for p_idx in range(dim):
+            lower, upper = self.pdists[p_idx].support
+            print(
+                f"{self.ptypes[p_idx]}::{self.pnames[p_idx]} in "
+                f"[{lower}, {upper}] >> affected subpop: {self.subpops[p_idx]}"
+            )
+
+    def build_from_config(self, global_config: ConfigView, subpop_names: list[str]) -> None:
+        """
+        Constructs the inference parameters from the global configuration.
+
+        Args:
+            global_config: The global configuration represented as a
+                `confuse.ConfigView`.
+            subpop_names: A list of subpopulation names to be used in
+                the configuration.
+        """
+        for config_part in ["seir_modifiers", "outcome_modifiers"]:
+            if global_config[config_part].exists():
+                for npi in global_config[config_part]["modifiers"].get():
+                    if global_config[config_part]["modifiers"][npi][
+                        "perturbation"
+                    ].exists():
+                        self.add_modifier(
+                            pname=npi,
+                            ptype=config_part,
+                            parameter_config=global_config[config_part]["modifiers"][npi],
+                            subpops=subpop_names,
+                        )
+
+    def add_modifier(
+        self,
+        pname: str,
+        ptype: Literal["outcome_modifiers", "seir_modifiers"],
+        parameter_config: ConfigView,
+        subpops: list[str],
+    ) -> None:
+        """
+        Adds a modifier parameter to the inference parameters representation.
+
+        Args:
+            pname: The name of the parameter.
+            ptype: The parameter type, must be one of "outcome_modifiers" or
+                "seir_modifiers".
+            parameter_config: The confuse representation of the parameter configuration.
+            subpops: A list of subpopulations affected by the modifier.
         """
         # identify spatial group
         affected_subpops = set(subpops)
@@ -54,27 +123,21 @@ class InferenceParameters:
             if spatial_groups["ungrouped"]:
                 for sp in spatial_groups["ungrouped"]:
                     dist = distribution_from_confuse_config(parameter_config["value"])
-                    lower, upper = dist.support
                     self.add_single_parameter(
                         ptype=ptype,
                         pname=pname,
                         subpop=sp,
                         pdist=dist,
-                        lb=lower,
-                        ub=upper,
                     )
             # grouped subpop have one parameter per group
             if spatial_groups["grouped"]:
                 for group in spatial_groups["grouped"]:
                     dist = distribution_from_confuse_config(parameter_config["value"])
-                    lower, upper = dist.support
                     self.add_single_parameter(
                         ptype=ptype,
                         pname=pname,
                         subpop=",".join(group),
                         pdist=dist,
-                        lb=lower,
-                        ub=upper,
                     )
         elif parameter_config["method"].get() == "MultiPeriodModifier":
             affected_subpops_grp = []
@@ -99,115 +162,85 @@ class InferenceParameters:
                 if this_spatial_group["ungrouped"]:
                     for sp in this_spatial_group["ungrouped"]:
                         dist = distribution_from_confuse_config(parameter_config["value"])
-                        lower, upper = dist.support
                         self.add_single_parameter(
                             ptype=ptype,
                             pname=pname,
                             subpop=sp,
                             pdist=dist,
-                            lb=lower,
-                            ub=upper,
                         )
                 # grouped subpop have one parameter per group
                 if this_spatial_group["grouped"]:
                     for group in this_spatial_group["grouped"]:
                         dist = distribution_from_confuse_config(parameter_config["value"])
-                        lower, upper = dist.support
                         self.add_single_parameter(
                             ptype=ptype,
                             pname=pname,
                             subpop=",".join(group),
                             pdist=dist,
-                            lb=lower,
-                            ub=upper,
                         )
         else:
             raise ValueError(f"Unknown method {parameter_config['method']}")
 
-    def add_single_parameter(self, ptype, pname, subpop, pdist, lb, ub):
+    def add_single_parameter(
+        self,
+        ptype: Literal["outcome_modifiers", "seir_modifiers"],
+        pname: str,
+        subpop: str,
+        pdist: DistributionABC,
+    ) -> None:
         """
-        Adds a single parameter to the parameters list.
+        Adds a single parameter to the inference parameters representation.
 
         Args:
-            ptype (str): The parameter type.
-            pname (str): The parameter name.
-            subpop (str): The subpopulation affected by the parameter.
+            ptype: The parameter type, must be one of "outcome_modifiers" or
+                "seir_modifiers".
+            pname: The parameter name.
+            subpop: The subpopulation affected by the parameter.
             pdist: The distribution of the parameter.
-            lb: The lower bound of the parameter.
-            ub: The upper bound of the parameter.
         """
         self.ptypes.append(ptype)
         self.pnames.append(pname)
         self.subpops.append(subpop)
         self.pdists.append(pdist)
-        self.ubs = np.append(self.ubs, ub)
-        self.lbs = np.append(self.lbs, lb)
 
-    def build_from_config(self, global_config, subpop_names):
-        for config_part in ["seir_modifiers", "outcome_modifiers"]:
-            if global_config[config_part].exists():
-                for npi in global_config[config_part]["modifiers"].get():
-                    if global_config[config_part]["modifiers"][npi][
-                        "perturbation"
-                    ].exists():
-                        self.add_modifier(
-                            pname=npi,
-                            ptype=config_part,
-                            parameter_config=global_config[config_part]["modifiers"][npi],
-                            subpops=subpop_names,
-                        )
-
-    def print_summary(self):
-        print(f"There are {len(self.pnames)} parameters in the configuration.")
-        for p_idx in range(self.get_dim()):
-            print(
-                f"{self.ptypes[p_idx]}::{self.pnames[p_idx]} in [{self.lbs[p_idx]}, {self.ubs[p_idx]}]"
-                f"   >> affected subpop: {self.subpops[p_idx]}"
-            )
-
-    def __str__(self) -> str:
-        from collections import Counter
-
-        this_str = f"InferenceParameters: with {self.get_dim()} parameters: \n"
-        for key, value in Counter(self.ptypes).items():
-            this_str += f"    {key}: {value} parameters\n"
-
-        return this_str
-
-    def get_dim(self):
-        return len(self.pnames)
-
-    def get_parameters_for_subpop(self, subpop: str) -> list:
-        """Returns the index parameters for a given subpopulation"""
-        parameters = []
-        for i, sp in enumerate(self.subpops):
-            if sp == subpop:
-                parameters.append(i)
-        return parameters
-
-    def __len__(self):
+    def get_dim(self) -> int:
         """
-        so one can use the built-in python len function
-        """
-        return len(self.pnames)
+        Get the dimension of the parameter space.
 
-    def draw_initial(self, n_draw=1):
+        Returns:
+            The dimension of the parameter space, which is a non-negative integer.
+        """
+        return len(self)
+
+    def get_parameters_for_subpop(self, subpop: str) -> list[int]:
+        """
+        Get the indices of parameters relevant for a specific subpopulation.
+
+        Args:
+            subpop: The name of the subpopulation to pull parameters indexes for.
+
+        Returns:
+            A list of indices corresponding to parameters that affect the specified
+            subpopulation.
+        """
+        return [i for i, s in enumerate(self.subpops) if s == subpop]
+
+    def draw_initial(self, n_draw: int = 1) -> npt.NDArray[np.float64]:
         """
         Draws initial parameter values.
 
         Args:
-            n_draw (int): Number of draws, e.g the number of slots or walkers
+            n_draw: Number of draws, e.g the number of slots or walkers.
 
         Returns:
-            np.ndarray: Array of initial parameter values.
+            Array of initial parameter values with shape (`n_draw`, dim).
         """
-        p0 = np.zeros((n_draw, self.get_dim()))
-        for p_idx in range(self.get_dim()):
+        dim = len(self)
+        p0 = np.zeros((n_draw, dim))
+        for p_idx in range(dim):
             p0[:, p_idx] = self.pdists[p_idx].sample(size=n_draw)
         return p0
 
-    # TODO: write a more granular method the return for a
-    # single parameter and correct the proposal like we did
     def check_in_bound(self, proposal: npt.NDArray[np.float64]) -> bool:
         """
         Checks if the proposal is within parameter bounds.
@@ -218,38 +251,18 @@ class InferenceParameters:
         Returns:
             `True` if the proposal is within bounds, `False` otherwise.
         """
-        return np.logical_and(~self.hit_lbs(proposal), ~self.hit_ubs(proposal)).all()
-
-    def hit_lbs(self, proposal: npt.NDArray[np.float64]) -> npt.NDArray[np.bool_]:
-        """
-        Test if the proposal hits the lower bounds.
-
-        Args:
-            proposal: The proposed parameter values.
-
-        Returns:
-            A boolean array indicating which proposed parameters hit the lower bounds.
-        """
-        return np.less(proposal, self.lbs)
-
-    def hit_ubs(self, proposal: npt.NDArray[np.float64]) -> npt.NDArray[np.bool_]:
-        """
-        Test if the proposal hits the upper bounds.
-
-        Args:
-            proposal: The proposed parameter values.
-
-        Returns:
-            A boolean array indicating which proposed parameters hit the upper bounds.
-        """
-        return np.greater(proposal, self.ubs)
+        lower_bounds, upper_bounds = zip(*[dist.support for dist in self.pdists])
+        return np.logical_and(
+            np.greater_equal(proposal, lower_bounds),
+            np.less_equal(proposal, upper_bounds),
+        ).all()
 
     def inject_proposal(
         self,
-        proposal,
-        snpi_df=None,
-        hnpi_df=None,
-    ):
+        proposal: npt.NDArray[np.float64],
+        snpi_df: pd.DataFrame,
+        hnpi_df: pd.DataFrame,
+    ) -> tuple[pd.DataFrame, pd.DataFrame]:
         """
         Injects the proposal into model inputs, at the right place.
 
@@ -259,24 +272,16 @@ class InferenceParameters:
             snpi_df (pd.DataFrame): DataFrame for snpi.
 
         Returns:
-            pd.DataFrame, pd.DataFrame: Modified hnpi_df and snpi_df.
+            A tuple of modified DataFrames, the first one for SEIR modifiers and the
+            second for outcome modifiers.
         """
         snpi_df_mod = snpi_df.copy(deep=True)
         hnpi_df_mod = hnpi_df.copy(deep=True)
-
-        # Ideally this should lie in each submodules, e.g NPI.inject, parameter.inject
-
-        for p_idx in range(self.get_dim()):
-            if self.ptypes[p_idx] == "seir_modifiers":
-                snpi_df_mod.loc[
-                    (snpi_df_mod["modifier_name"] == self.pnames[p_idx])
-                    & (snpi_df_mod["subpop"] == self.subpops[p_idx]),
-                    "value",
-                ] = proposal[p_idx]
-            elif self.ptypes[p_idx] == "outcome_modifiers":
-                hnpi_df_mod.loc[
-                    (hnpi_df_mod["modifier_name"] == self.pnames[p_idx])
-                    & (hnpi_df_mod["subpop"] == self.subpops[p_idx]),
-                    "value",
-                ] = proposal[p_idx]
+        for p_idx in range(len(self)):
+            df = snpi_df_mod if self.ptypes[p_idx] == "seir_modifiers" else hnpi_df_mod
+            df.loc[
+                (df["modifier_name"] == self.pnames[p_idx])
+                & (df["subpop"] == self.subpops[p_idx]),
+                "value",
+            ] = proposal[p_idx]
         return snpi_df_mod, hnpi_df_mod
